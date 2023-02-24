c137509f1a8c7d73af3edca2c2015aaa089364fe
skibidibidop <beltran.mapb@gmail.com>
2023-02-24 10:34:18 +0800

    Add para with red text

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,2 +1,10 @@
 'use strict';
 
+// To avoid using the word new, I'm using 'neo' for variables
+
+const divContainer = document.querySelector('#container');
+const neoPara = document.createElement('p');
+
+divContainer.appendChild(neoPara);
+neoPara.textContent = 'Hey, I\'m Red!';
+neoPara.style.color = 'red';
