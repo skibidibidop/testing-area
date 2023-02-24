7be8db6be7f1dfdfab30201472a3c277c4040f81
skibidibidop <beltran.mapb@gmail.com>
2023-02-24 10:54:08 +0800

    Add solution

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,12 +4,23 @@
 
 const divContainer = document.querySelector('#container');
 const neoPara = document.createElement('p');
-const neoHead3 = document.createElement('h3');
-
 divContainer.appendChild(neoPara);
 neoPara.textContent = 'Hey, I\'m Red!';
 neoPara.style.color = 'red';
 
+const neoHead3 = document.createElement('h3');
 divContainer.appendChild(neoHead3);
 neoHead3.textContent = 'I\'m a blue h3!';
 neoHead3.style.color = 'blue';
+
+const neoDiv = document.createElement('div');
+divContainer.appendChild(neoDiv);
+neoDiv.style.border = '1px solid black';
+neoDiv.style.backgroung = 'pink';
+
+    const neoHead1 = document.createElement('h1');
+    neoDiv.appendChild(neoHead1);
+    const neoPara2 = document.createElement('p');
+    neoDiv.appendChild(neoPara2);
+    neoHead1.textContent = 'I\'m in a div!';
+    neoPara2.textContent = 'Me too!';
