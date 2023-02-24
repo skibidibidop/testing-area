a06f90e2cfcd730b88f9e93d14e5053edeabe1ed
skibidibidop <beltran.mapb@gmail.com>
2023-02-24 10:36:58 +0800

    Add h3 with blue text in div

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,7 +4,12 @@
 
 const divContainer = document.querySelector('#container');
 const neoPara = document.createElement('p');
+const neoHead3 = document.createElement('h3');
 
 divContainer.appendChild(neoPara);
 neoPara.textContent = 'Hey, I\'m Red!';
 neoPara.style.color = 'red';
+
+divContainer.appendChild(neoHead3);
+neoHead3.textContent = 'I\'m a blue h3!';
+neoHead3.style.color = 'blue';
