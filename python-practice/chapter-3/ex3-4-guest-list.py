63747504ac2e620779e2f9d17aeb0490372805a3
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-05 15:33:06 +0800

    Add Chapter 3 exercises 4 to 7 solution

---
+++ python-practice/chapter-3/ex3-4-guest-list.py
@@ -0,0 +1,46 @@
+names = ['Person1', 'Person2', 'Person3']
+
+print(f"Would you like to go to dinner, {names[0]}?")
+print(f"Would you like to go to dinner, {names[1]}?")
+print(f"Would you like to go to dinner, {names[2]}?\n")
+
+print(f"{names[2]} can't make it, unfortunately.\n")
+
+del names[2]
+names.append("Person4")
+
+print(f"Would you like to go to dinner, {names[0]}?")
+print(f"Would you like to go to dinner, {names[1]}?")
+print(f"Would you like to go to dinner, {names[2]}?\n")
+
+print("Dear guests, a bigger table has been found!\n")
+
+names.insert(0, "Person5")
+names.insert(1, "Person6")
+names.append("Person7")
+
+print(f"Would you like to go to dinner, {names[0]}?")
+print(f"Would you like to go to dinner, {names[1]}?")
+print(f"Would you like to go to dinner, {names[2]}?")
+print(f"Would you like to go to dinner, {names[3]}?")
+print(f"Would you like to go to dinner, {names[4]}?")
+print(f"Would you like to go to dinner, {names[5]}?\n")
+
+print(f"Dear guests, my apologies. The bigger table won't arrive in time for our dinner.\n")
+
+removed = names.pop()
+print(f"{removed}, my apologies but we won't be able to accommodate you.")
+removed = names.pop()
+print(f"{removed}, my apologies but we won't be able to accommodate you.")
+removed = names.pop()
+print(f"{removed}, my apologies but we won't be able to accommodate you.")
+removed = names.pop()
+print(f"{removed}, my apologies but we won't be able to accommodate you.\n")
+
+print(f"{names[0]}, you are still invited to dinner.")
+print(f"{names[1]}, you are still invited to dinner.")
+
+del names[1]
+del names[0]
+
+print(names)
