e99b6f2c53bc31f8d1439490f2990ba4d15e9775
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-05 19:51:56 +0800

    Add exercise 4-13 solution

---
+++ python-practice/chapter-4/ex4-13-buffet.py
@@ -0,0 +1,12 @@
+basics = ("bread", "butter", "jam", "beans", "ham")
+
+for basic in basics:
+    print(basic)
+
+# basics[1] = "juice"
+# Commented out to remove error. Tuples can't be modified
+
+basics = ("bread", "rice", "jam", "tocino", "ham")
+
+for basic in basics:
+    print(basic)
