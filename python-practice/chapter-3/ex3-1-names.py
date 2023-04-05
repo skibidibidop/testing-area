870420ced3d0fc96f03a97b28eb2d6ccdc419217
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-05 13:19:08 +0800

    Add solution to exercise 3-3

---
+++ python-practice/chapter-3/ex3-1-names.py
@@ -0,0 +1,6 @@
+names = ['royce', 'gerard', 'jerry', 'julius', 'jinky']
+
+print(f"Hi, {names[0].title()}! Welcome.")
+print(f"Hi, {names[1].title()}! Welcome.")
+print(f"Hi, {names[2].title()}! Welcome.")
+print(f"Hi, {names[3].title()}! Welcome.")
---
+++ python-practice/chapter-3/ex3-3-your-own-list.py
@@ -0,0 +1,5 @@
+transpo = ['Toyota', 'Ford', 'Honda']
+
+print(f"{transpo[0]} cars are affordable.")
+print(f"{transpo[1]} cars are expensive.")
+print(f"I don't have an opinion about {transpo[2]} cars.")
