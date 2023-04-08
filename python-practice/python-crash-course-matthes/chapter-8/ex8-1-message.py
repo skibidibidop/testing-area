773d696b03b41041e1658d4c0fd42ae8f7e7770c
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-04-08 00:51:04 +0000

    Add chapter-8 dir, solution to ex8-1

---
+++ python-practice/python-crash-course-matthes/chapter-8/ex8-1-message.py
@@ -0,0 +1,10 @@
+# Make function display_message()
+# Function should print what I learned in Chapter 8 so far
+# Call and check if display is correct
+
+def display_message():
+    """Print what I learned in Chapter 8"""
+    print("I learned how to define and call functions. "
+        "Docstring is also new to me.")
+
+display_message()
