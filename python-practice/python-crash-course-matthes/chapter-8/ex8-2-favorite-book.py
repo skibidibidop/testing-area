929279a9a48ee358f2f066546c6ce4ec26985ca8
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-04-08 00:56:30 +0000

    Add solution to exercise 8-2

---
+++ python-practice/python-crash-course-matthes/chapter-8/ex8-2-favorite-book.py
@@ -0,0 +1,11 @@
+# Make favorite_book(title)
+# Function should print "One of my favorite books is
+# Lord of the Rings"
+# Use the title argument to specify book title
+
+def favorite_book(title):
+    """Print specified book title"""
+    print(f"One of my favorite books is "
+        f"{title}.")
+
+favorite_book("Lord of the Rings")
