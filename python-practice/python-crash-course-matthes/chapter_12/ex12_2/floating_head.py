4c4cf00d795894d8bfda2ff47f5ef57371ff6927
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-13 07:15:56 +0800

    Add dir for exercise 12-2

---
+++ python-practice/python-crash-course-matthes/chapter_12/ex12_2/floating_head.py
@@ -0,0 +1,13 @@
+# This is a screen to show a floating head
+
+import sys
+
+import pygame
+
+class FloatingHead:
+    """A screen to show a floating head."""
+
+    def __init__(self):
+        """Initialize"""
+        pygame.init()
+        self.screen = pygame.display.
---
+++ python-practice/python-crash-course-matthes/chapter_12/ex12_2/head.py
@@ -0,0 +1,10 @@
+# Get bmp image of character
+# Make class that draws character at the center of the screen
+# Match bg color of character to the screen bg color
+
+import sys
+
+import pygame
+
+class Head:
+    """
