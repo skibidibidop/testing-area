700bc0070890449f41c74f5e703a5d36e2368ffd
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-13 08:21:54 +0800

    Add solution to exercise 12-2

--- python-practice/python-crash-course-matthes/chapter_12/ex12_2/floating_head.py
+++ python-practice/python-crash-course-matthes/chapter_12/ex12_2/floating_head.py
@@ -3,11 +3,34 @@
 import sys
 
 import pygame
+from head import Head
 
 class FloatingHead:
     """A screen to show a floating head."""
 
     def __init__(self):
-        """Initialize"""
+        """Initialize screen size, bg color."""
         pygame.init()
-        self.screen = pygame.display.
+        self.screen = pygame.display.set_mode((640, 480))
+        self.bg_color = (100, 100, 100)
+        self.head = Head(self)
+
+        pygame.display.set_caption("Floating Head")
+
+
+    def run_window(self):
+        """Launch window to show a floating head."""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.bg_color)
+            self.head.blitme()
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    window = FloatingHead()
+    window.run_window()
+
--- python-practice/python-crash-course-matthes/chapter_12/ex12_2/head.py
+++ python-practice/python-crash-course-matthes/chapter_12/ex12_2/head.py
@@ -7,4 +7,17 @@ import sys
 import pygame
 
 class Head:
-    """
+    """A floating head."""
+
+    def __init__(self, window):
+        """Initialize floating head and position."""
+        self.screen = window.screen
+        self.screen_rect = window.screen.get_rect()
+        self.image = pygame.image.load("images/head.bmp")
+        self.rect = self.image.get_rect()
+        self.rect.center = self.screen_rect.center
+
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
+
