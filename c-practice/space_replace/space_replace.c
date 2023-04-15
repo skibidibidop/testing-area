f727d150a377399de9b3104d47e267cfec4ee794
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-15 08:07:47 +0800

    Add program that replaces string of spaces with 1 space

---
+++ c-practice/space_replace/space_replace.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void) {
+	int previous = 0;
+	int character;
+
+	while ( (character = getchar()) != EOF ) {
+		if ( (!isspace(previous)) ) {
+			printf("%c", character);
+		}
+		else if ( (isspace(previous)) && (!isspace(character)) ) {
+			printf("%c", character);
+		}
+
+		previous = character;
+	}
+
+	return 0;
+}
