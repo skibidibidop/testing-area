553ddcd11ea2c19e6de060ed91441c6bae10c180
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-15 10:06:34 +0800

    Add dir for histogram exercise, incomplete

---
+++ c-practice/histogram/horizontal_histogram.c
@@ -0,0 +1,13 @@
+// Horizontal bar histogram of length of words in input
+// Use arrays
+// Use these as input:
+// a ab ab abc abc abc abcd abcd abcde
+// 	length(freq): 1(1) 2(2) 3(3) 4(2) 5(1)
+// asd jkli qwerty zidmm jein neuy lkjf a n der jl ku la
+// 	1(2) 2(3) 3(2) 4(3) 6(1) 5(1)
+
+#include <stdio.h>
+
+int main(void) {
+	
+}
