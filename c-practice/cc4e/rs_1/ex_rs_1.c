624f12eabdf7b8bb315d83e9148bb5a8c8d996b7
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-04-22 01:48:16 +0000

    Add folder and 1st solution to online K & R exercises (cc4e)

---
+++ c-practice/cc4e/rs_1/ex_rs_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    char hello_world[] = "Hello world";
+  	char answer[] = "Answer";
+  	int answer_num = 15;
+  	char name[] = "Name";
+  	char sarah[] = "Sarah";
+  
+	printf("%s\n", hello_world);
+  	printf("%s %d\n", answer, answer_num);
+  	printf("%s %s\n", name, sarah);
+  
+  	return 0;
+}
