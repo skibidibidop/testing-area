0230424fd0590788da9532232fc0686ec4cf382a
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-24 16:45:15 +0800

    Add input loop, trim_end function

--- c-practice/knr/1_18/ex1_18.c
+++ c-practice/knr/1_18/ex1_18.c
@@ -9,5 +9,52 @@
 #define MAXCHAR 10000
 
 int main(void) {
+	char character;
+	char m_storage[MAXCHAR];
+	int m_i = 0;
+
+	printf("***TRAILING SPACE TRIMMER***\n");
+	printf("Provide input, then hit enter.\n");
+	printf("Ctrl + D to end input phase.\n");
+	printf("Input:\n");
+
+	while ( (character = getchar()) != EOF ) {
+		if (character == '\n') {
+			m_storage[m_i] = '\0';
+		}
+		else {
+			m_storage[m_i] = character;
+		}
+
+		m_i++;
+	}
+
+	trim_end(m_storage, m_i);
+	print_array(m_storage, m_i);
+
+	return 0;
+}
+
+void trim_end(char *te_storage, te_length) {
+	bool te_trailing;
+	char *te_end = te_storage + te_length;
+
+	while (te_end != te_storage) {
+		if (*te_end == '\0') {
+			te_trailing = true;
+		}
+		else if (!isblank(*te_end) || *te_end != '\0') {
+			te_trailing = false;
+		}
+
+		if (te_trailing = true) {
+			*te_end = '\0';
+		}
+
+		te_end--;
+	}
+}
+
+void print_array(char *pa_storage, pa_length) {
 
 }
