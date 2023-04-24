43db3d2bcbc2d5de128562c72ff81b6239603bda
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-24 16:21:43 +0800

    Add standard libs, boilerplate

---
+++ c-practice/knr/1_18/ex1_18.c
@@ -0,0 +1,13 @@
+// Remove trailing blanks and tabs per line of input
+// Delete entirely blank lines
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAXCHAR 10000
+
+int main(void) {
+
+}
