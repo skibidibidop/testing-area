4ebd5fa5515c3911611c480ff701096ef4fa918b
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-25 17:37:31 +0800

    Change program description

--- c-practice/knr/1_20/ex1_20.c
+++ c-practice/knr/1_20/ex1_20.c
@@ -1,4 +1,14 @@
-// Replace tabs in input with the proper number
-// of spaces until the next tab stop
-
-
+/*
+Assumed space per tab stop: 8
+	Assign to a (variable / symbolic parameter?)
+Replace tabs in input with correct number of '-' up to
+	the next tab stop
+If input is 'one\t', output should be one-----
+	3 characters for 'one' + 5 dashes since the
+	total number of spaces before each tab stop = 8
+If input is '\tone\t', output should be --------one-----
+	8 dashes because input was started with \t
+If input is 'verylongwordmorethaneight\t', output:
+	1234567|1234567|1234567|1234567|
+	verylongwordmorethaneight-------
+*/
