fee2f078398cbc4f57b54d8837e9510b51ce9bed
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-27 17:46:24 +0800

    Put input loop in function

--- c-practice/knr/1_21/ex1_21.c
+++ c-practice/knr/1_21/ex1_21.c
@@ -14,11 +14,25 @@ If no. of consecutive spaces >= TABSTOP
 #define MAXCHAR 10000
 #define TABSTOP 8
 
+void store_input(int s_arr[]);
+
 int main(void) {
 	int arr[MAXCHAR];
+
+	printf("Provide input. Ctrl + d to end.\n");
+	printf("Input: \n");
+
+	store_input(arr);
+
+	return 0;
+}
+
+void store_input(int s_arr[]) {
 	int character;
 
-	while ( (character = getchar()) != EOF ) {
-
+	for (int s_i = 0; (character = getchar()) != EOF; s_i++) {
+		s_arr[s_i] = character;
 	}
+
+	return;
 }
