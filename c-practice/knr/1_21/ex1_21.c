e3c27335864a584c4efc0b63a1251f59d80840de
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-27 18:28:09 +0800

    Change program description

--- c-practice/knr/1_21/ex1_21.c
+++ c-practice/knr/1_21/ex1_21.c
@@ -2,11 +2,6 @@
 Put string input in array
 Replace strings of blanks w/ min. no. of \t & - to achieve
 	the same spacing. Use the same TABSTOP (8) in 1-20
-If no. of consecutive spaces >= TABSTOP
-	no. of \\t to print = TABSTOP / 8
-		The printed \t is a representation of an
-		actual tab which is 8 characters wide
-	no. of - to print  = TABSTOP % 8
 */
 
 #include <stdio.h>
