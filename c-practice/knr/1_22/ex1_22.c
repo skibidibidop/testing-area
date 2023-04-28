f5c4da7d6065fcc44169cf447900548c74f348e2
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-28 12:29:39 +0800

    Add function: newline every 20th char

--- c-practice/knr/1_22/ex1_22.c
+++ c-practice/knr/1_22/ex1_22.c
@@ -12,6 +12,9 @@ int main(void) {
 	int storage[MAXCHAR];
 
 	store_input(storage);
+	fold(storage);
+
+	return 0;
 }
 
 void store_input(si_arr[]) {
@@ -23,5 +26,25 @@ void store_input(si_arr[]) {
 		si_i++;
 	}
 
+	si_i++;
+	si_arr[si_i] = '\0';
+
 	return;
+}
+
+void fold(f_arr[]) {
+	int f_limit = 0;
+
+	for (int f_i = 0; f_arr[f_i] != '\0'; f_i++) {
+		f_limit = (f_i + 1) % COLUMN;
+
+		if ( f_limit == 0 && !isblank(f_arr[f_i]) ) {
+			printf("-\n");
+		}
+		else if ( f_limit == 0 && isblank(f_arr[f_i]) ) {
+			printf("\n");
+		}
+
+		printf("%c", f_arr[f_i]);
+	}
 }
