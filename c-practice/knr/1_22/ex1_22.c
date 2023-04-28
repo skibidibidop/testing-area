70bbb1e5215df78df8d38ff44bb0927ae7263cfd
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-28 12:33:58 +0800

    Add program description, prompt

--- c-practice/knr/1_22/ex1_22.c
+++ c-practice/knr/1_22/ex1_22.c
@@ -4,12 +4,22 @@ Exceeding characters will be printed on a new line
 */
 
 #include <stdio.h>
+#include <ctype.h>
 
 #define COLUMN 20
 #define MAXCHAR 10000
 
+void store_input(int si_arr[]);
+void fold(int f_arr[]);
+
 int main(void) {
 	int storage[MAXCHAR];
+
+	printf("***20-character lines***\n");
+	printf("Input will be turned into multiple lines ");
+	printf("with 20 characters max.\n");
+	printf("Provide input. Ctrl + d to end input phase.\n");
+	printf("Input:\n");
 
 	store_input(storage);
 	fold(storage);
@@ -17,7 +27,7 @@ int main(void) {
 	return 0;
 }
 
-void store_input(si_arr[]) {
+void store_input(int si_arr[]) {
 	int si_char;
 	int si_i = 0;
 
@@ -32,7 +42,7 @@ void store_input(si_arr[]) {
 	return;
 }
 
-void fold(f_arr[]) {
+void fold(int f_arr[]) {
 	int f_limit = 0;
 
 	for (int f_i = 0; f_arr[f_i] != '\0'; f_i++) {
@@ -47,4 +57,6 @@ void fold(f_arr[]) {
 
 		printf("%c", f_arr[f_i]);
 	}
+
+	return;
 }
