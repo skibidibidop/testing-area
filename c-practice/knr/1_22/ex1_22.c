abad92a345319ffc15d8375a355238628ccb7ebc
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-28 12:19:04 +0800

    Add function: store input in array

--- c-practice/knr/1_22/ex1_22.c
+++ c-practice/knr/1_22/ex1_22.c
@@ -6,7 +6,22 @@ Exceeding characters will be printed on a new line
 #include <stdio.h>
 
 #define COLUMN 20
+#define MAXCHAR 10000
 
 int main(void) {
-	
+	int storage[MAXCHAR];
+
+	store_input(storage);
+}
+
+void store_input(si_arr[]) {
+	int si_char;
+	int si_i = 0;
+
+	while ( (si_char = getchar()) != EOF ) {
+		si_arr[si_i] = si_char;
+		si_i++;
+	}
+
+	return;
 }
