804303c8a1b32eff65ba84383e099b5bf2eb5c06
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-29 12:10:09 +0800

    Add KNR exercise 1-5 solution

--- c-practice/knr/1_5/ex1_5.c
+++ c-practice/knr/1_5/ex1_5.c
@@ -4,6 +4,23 @@
 
 #include <stdio.h>
 
-int main(void) {
+#define STEP 20
 
+// Fahrenheit to Celsius
+int main(void) {
+	float fahr = 300.0;
+	float cels;
+	char f[] = "Fahrenheit";
+	char c[] = "Celsius";
+
+	printf("%15s %15s\n", f, c);
+
+	float fahr_const = 5.0 / 9.0;
+	while (fahr >= 0) {
+		cels = (fahr - 32.0) * fahr_const;
+		printf("%15.0f %15.2f\n", fahr, cels);
+		fahr -= STEP;
+	}
+
+	return 0;
 }
