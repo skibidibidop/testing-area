34c67f1a95fdd664a19e58f29025da8f6c1a3b2b
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-29 08:48:38 +0800

    Add program description

---
+++ c-practice/knr/3_2/ex3_2.c
@@ -0,0 +1,11 @@
+/*
+Store input in array0.
+Convert escape sequences to their visible representations.
+Store in array1, and then print. Newline character will be
+printed as \n, etc.
+Convert escape sequences in array1 back to how they are
+supposed to be displayed. Store in array2, then print.
+*/
+
+
+
