0635db428cb755dc489fe82cfda67ca94d45ebc7
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-29 12:00:19 +0800

    Add KNR exercise 1-4 solution

--- c-practice/knr/1_4/ex1_4.c
+++ c-practice/knr/1_4/ex1_4.c
@@ -1,6 +1,24 @@
 // Convert Celsius to Fahrenheit
 // For 0, 20, ..., 300 Celsius
 
+#include <stdio.h>
+
+#define STEP 20
+
 int main(void) {
+	float celsius = 0;
+	float fahr;
+	char c[] = "Celsius";
+	char f[] = "Fahrenheit";
 	
+	printf("%15s %15s\n", c, f);
+
+	float cels_const = 9.0 / 5.0;
+	while (celsius <= 300) {
+		fahr = (celsius * cels_const) + 32;
+		printf("%15.0f %15.1f\n", celsius, fahr);
+		celsius += STEP;
+	}
+
+	return 0;
 }
