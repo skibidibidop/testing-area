cbb9623f7ca01cc0b1c273be8dd55027d020f5b5
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-30 15:36:10 +0800

    Add boilerplate for main, initialize, store_input

---
+++ c-practice/knr/1_14/ex1_14.c
@@ -0,0 +1,27 @@
+// Print histogram of character frequencies in input
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAXCHAR 10000
+
+int main(void) {
+	char storage[MAXCHAR];
+	int count[MAXCHAR];
+
+	initialize(count);
+	int length = store_input(storage, count);
+
+	return 0;
+}
+
+// Initialize all elements to 0
+void initialize(int i_count[]) {
+
+}
+
+// Store characters and character count in separate
+// arrays. Return character array length
+int store_input(char s_storage[], int s_count[]) {
+
+}
