b1357131db92d6d4d26de4cb3aad3aa0c18435b2
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-30 15:49:14 +0800

    Add body of store_input function

--- c-practice/knr/1_14/ex1_14.c
+++ c-practice/knr/1_14/ex1_14.c
@@ -27,5 +27,30 @@ void initialize(int i_count[]) {
 // Store characters and character count in separate
 // arrays. Return character array length
 int store_input(char s_storage[], int s_count[]) {
-	
+	int s_char;
+	int s_length = 0;
+	bool s_existing;
+
+	while ( (s_char = getchar()) != EOF ) {
+		s_length++;
+		s_existing = false;
+
+		for (int s_i = 0; s_i < s_length; s_i++) {
+			if (s_storage[s_i] == s_char) {
+				s_count[s_i]++;
+				s_existing = true;
+			}
+		}
+
+		if (s_existing == false) {
+			s_storage[length] = s_char;
+			s_count[length]++;
+		}
+	}
+
+	return s_length;
+}
+
+void print_histogram(char p_storage[], int p_count[]) {
+
 }
