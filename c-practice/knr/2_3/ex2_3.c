316a3910fa9baa3431b22e943dbd6e05ecff3a68
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-01 17:47:28 +0800

    Add partial solution to ex 2-3

--- c-practice/knr/2_3/ex2_3.c
+++ c-practice/knr/2_3/ex2_3.c
@@ -4,9 +4,50 @@
 
 #include <stdio.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 #define MAXCHAR 1000
 
-int main(void) {
+void store_input(char s_input[]);
+bool check_prefix(char cp_input[]);
 
+int main(void) {
+	char input[MAXCHAR];
+
+	store_input(input);
+	bool prefix = check_prefix(input);
+	verify_input(input, prefix);
+
+	printf("%s\n", input);
+
+	return 0;
+}
+
+// Convert input to lowercase, store in array
+void store_input(char s_input[]) {
+	char s_element;
+	int s_i = 0;
+
+	while ( (s_element = getchar()) != '\n' ) {
+		s_input[s_i] = tolower(s_element);
+		s_i++;
+	}
+
+	s_input[s_i++] = '\0';
+
+	return;
+}
+
+bool check_prefix(char cp_input[]) {
+	if (cp_input[0] == '0' && cp_input[1] == 'x') {
+		return true;
+	}
+
+	return false;
+}
+
+void verify_input(char v_input[], bool v_prefix) {
+	if (v_prefix == true) {
+		
+	}
 }
