05fa07d271cf865942e605b2f7fb86194c059340
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-01 10:24:39 +0800

    Add KNR exercise 2-1 solution

--- c-practice/knr/2_1/ex2_1.c
+++ c-practice/knr/2_1/ex2_1.c
@@ -1,9 +1,25 @@
-// Print ranges of char, short, int, long, float,
+// Print ranges of char, short, int, long,
 // 	both signed and unsigned
 
 #include <stdio.h>
 #include <limits.h>
 
 int main(void) {
+	char smax[] = "Signed Max";
+	char smin[] = "Signed Min";
+	char umax[] = "Unsigned Max";
+	char t[] = "Type";
 
+	printf("*** LIMITS ***\n\n");
+	printf("%5s %25s %25s %25s\n", t, umax, smax, smin);
+	printf("%5s %25u %25i %25i\n", "char", UCHAR_MAX, SCHAR_MAX,
+		SCHAR_MIN);
+	printf("%5s %25u %25i %25i\n", "short", USHRT_MAX, SHRT_MAX,
+		SHRT_MIN);
+	printf("%5s %25u %25i %25i\n", "int", UINT_MAX, INT_MAX,
+		INT_MIN);
+	printf("%5s %25lu %25li %25li\n", "long", ULONG_MAX,
+		LONG_MAX, LONG_MIN);
+
+	return 0;
 }
