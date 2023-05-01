cebb846e375fdf21d53a9870dcb94d1b1a248735
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-01 17:15:45 +0800

    Add boilerplate

---
+++ c-practice/knr/2_3/ex2_3.c
@@ -0,0 +1,12 @@
+// Convert hex input to decimal, then print
+// Allowed input: 0 to 9, a to f, A to F
+// Leading 0x or 0X optional
+
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAXCHAR 1000
+
+int main(void) {
+
+}
