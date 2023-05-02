f1160b67eddbbd7cbe9c40ef441910e97a92822f
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-02 18:01:11 +0800

    Add boilerplate

---
+++ c-practice/knr/2_5/ex2_5.c
@@ -0,0 +1,12 @@
+// If a character from string2 matches a character from string1 for
+// 	the first time, return the location of the element from
+// 	string1
+// Return -1 if no matches
+
+#include <stdio.h>
+
+#define MAXCHAR 1000
+
+int main(void) {
+
+}
