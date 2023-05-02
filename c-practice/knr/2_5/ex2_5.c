f8243da701a877d296da839755156b6e10652fbf
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-02 18:06:16 +0800

    Add function for storing input in array

--- c-practice/knr/2_5/ex2_5.c
+++ c-practice/knr/2_5/ex2_5.c
@@ -7,6 +7,33 @@
 
 #define MAXCHAR 1000
 
-int main(void) {
+void store_input(char si_arr[]);
 
+int main(void) {
+	char string1[MAXCHAR];
+	char string2[MAXCHAR];
+	int string3[MAXCHAR];
+
+	printf("***MATCH LOCATOR***\n");
+	printf("Input reference: ");
+	store_input(string2);
+	printf("Input string for processing: ");
+	store_input(string2);
+
+	return 0;
+}
+
+// Store input in array
+void store_input(char si_arr[]) {
+	char si_char;
+	int si_i = 0;
+
+	while ( (si_char = getchar()) != '\n' ) {
+		si_arr[si_i] = si_char;
+		si_i++;
+	}
+
+	si_arr[si_i] = '\0';
+
+	return;
 }
