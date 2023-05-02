75b5eb055bbea81f2f3238bb72db2722eeee8550
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-02 12:23:49 +0800

    Add function to check if invalid, prefixed, or not prefixed

--- c-practice/knr/2_3/ex2_3.c
+++ c-practice/knr/2_3/ex2_3.c
@@ -7,13 +7,21 @@
 #include <stdbool.h>
 
 #define MAXCHAR 1000
+#define NOT_PREFIXED 0
+#define PREFIXED 1
+#define INVALID 2
 
-bool store_input(char s_input[]);
+int verify_input(char s_input[]);
 
 int main(void) {
 	char input[MAXCHAR];
 
-	bool prefix = store_input(input);
+	int hex_prefix = verify_input(input);
+	
+	if (hex_prefix == INVALID) {
+		printf("INVALID\n");
+		return 0;
+	}
 
 	printf("%s\n", input);
 
@@ -21,22 +29,43 @@ int main(void) {
 }
 
 // Convert input to lowercase, store in array
-// Identify if input has 0x prefix
-bool store_input(char s_input[]) {
+// Identify if prefixed, not prefixed, or invalid
+int verify_input(char s_input[]) {
 	char s_element;
 	int s_i = 0;
+	int s_status;
+
+	// Set to 0 to avoid garbage val problems
+	s_input[0] = '0';
+	s_input[1] = '0';
 
 	while ( (s_element = getchar()) != '\n' ) {
 		s_input[s_i] = tolower(s_element);
+
+		if (s_input[0] == '0' && s_input[1] == 'x') {
+			s_status = PREFIXED;
+		}
+		else if ( s_input[0] == '0' && s_input[1] != 'x' &&
+			!isdigit(s_input[1]) ) {
+			return INVALID;
+		}
+		else if ( isdigit(s_input[0]) && isdigit(s_input[1]) ) {
+			s_status = NOT_PREFIXED;
+		}
+
+		if ( s_i >= 2 && !isdigit(s_input[s_i]) ) {
+			if ( s_input[s_i] < 'a' ||
+				(s_input[s_i] > 'f' &&
+				 s_input[s_i] != 'x') ) {
+			return INVALID;
+			}
+		}
+
 		s_i++;
 	}
 
 	s_input[s_i++] = '\0';
 
-	if (s_input[0] == '0' && s_input[1] == 'x') {
-		return true;
-	}
-
-	return false;
+	return s_status;
 }
 
