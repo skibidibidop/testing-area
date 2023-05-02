4c166312a05f093b1898d8323987e3cfca731254
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-02 12:42:52 +0800

    Remove conditional if not prefixed by 0x

--- c-practice/knr/2_3/ex2_3.c
+++ c-practice/knr/2_3/ex2_3.c
@@ -5,25 +5,21 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <string.h>
+#include <math.h>
 
 #define MAXCHAR 1000
-#define NOT_PREFIXED 0
 #define PREFIXED 1
 #define INVALID 2
 
 int verify_input(char s_input[]);
+void htoi(char h_input[]);
 
 int main(void) {
 	char input[MAXCHAR];
 
 	int hex_prefix = verify_input(input);
-	
-	if (hex_prefix == INVALID) {
-		printf("INVALID\n");
-		return 0;
-	}
-
-	printf("%s\n", input);
+	htoi(input, hex_prefix);
 
 	return 0;
 }
@@ -49,9 +45,6 @@ int verify_input(char s_input[]) {
 			!isdigit(s_input[1]) ) {
 			return INVALID;
 		}
-		else if ( isdigit(s_input[0]) && isdigit(s_input[1]) ) {
-			s_status = NOT_PREFIXED;
-		}
 
 		if ( s_i >= 2 && !isdigit(s_input[s_i]) ) {
 			if ( s_input[s_i] < 'a' ||
@@ -69,3 +62,23 @@ int verify_input(char s_input[]) {
 	return s_status;
 }
 
+// Convert hex to decimal, then print
+void htoi(char h_input[], int h_prefix) {
+	long h_decimal = 0;
+	int h_length = strlen(h_input);
+	double h_base = 16.0;
+	double h_place = 0.0;
+	double h_multiplier = pow(h_base, h_place);
+	int h_i = 0;
+
+	if (h_prefix == PREFIXED) {
+		// Skip first 2 elements (0 and x) in array
+		h_i = 2;
+	}
+
+	for ( ; h_input[h_i] != '\0'; h_i++) {
+
+	}
+
+	return;
+}
