a8b94c331cb3672038bd236beadd1ab17855e13c
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-02 18:35:44 +0800

    Add KNR exercise 2-5 solution

--- c-practice/knr/2_5/ex2_5.c
+++ c-practice/knr/2_5/ex2_5.c
@@ -8,6 +8,8 @@
 #define MAXCHAR 1000
 
 void store_input(char si_arr[]);
+void get_firstmatch(char gf_input[], char gf_ref[], int gf_loc[]);
+void print_locations(char pl_input[], int pl_loc[]);
 
 int main(void) {
 	char to_process[MAXCHAR];
@@ -19,6 +21,9 @@ int main(void) {
 	store_input(reference);
 	printf("Input string for processing: ");
 	store_input(to_process);
+
+	get_firstmatch(to_process, reference, locations);
+	print_locations(to_process, locations);
 
 	return 0;
 }
@@ -38,4 +43,32 @@ void store_input(char si_arr[]) {
 	return;
 }
 
+void get_firstmatch(char gf_input[], char gf_ref[], int gf_loc[]) {
+	int gf_i;
+	int gf_j;
 
+	for (gf_i = 0; gf_input[gf_i] != '\0'; gf_i++) {
+		for (gf_j = 0; gf_ref[gf_j]; gf_j++) {
+			if (gf_input[gf_i] == gf_ref[gf_j]) {
+				gf_loc[gf_i] = gf_j;
+				break;
+			}
+
+			gf_loc[gf_i] = -1;
+		}
+	}
+
+	return;
+}
+
+void print_locations(char pl_input[], int pl_loc[]) {
+	printf("%15s %15s\n", "Character", "Index # in reference");
+
+	for (int pl_i = 0; pl_input[pl_i] != '\0'; pl_i++) {
+		printf("%15c %15i\n", pl_input[pl_i], pl_loc[pl_i]);
+	}
+
+	printf("\n-1: character not found in reference.\n");
+
+	return;
+}
