680170ccce753953de81d1ea1111eaa55844a91b
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-03 00:43:22 +0000

    Add KNR exercise 2-10 solution

--- c-practice/knr/2_10/ex2_10.c
+++ c-practice/knr/2_10/ex2_10.c
@@ -4,6 +4,32 @@
 
 #include <stdio.h>
 
-int main(void) {
+#define MAXCHAR 1000
+#define CASEGAP 32
 
+void lower(int l_storage[]);
+
+int main(void) {
+    int storage[MAXCHAR];
+
+    printf("Input: ");
+    lower(storage);
+    printf("Converted: %ls\n", storage);
+
+    return 0;
+}
+
+void lower(int l_storage[]) {
+    int l_char;
+    int l_i = 0;
+
+    while ( (l_char = getchar() ) != '\n' ) {
+        l_storage[l_i] = (l_char >= 'A' && l_char <= 'Z') ? l_char + CASEGAP :
+                                                            l_char;
+        l_i++;
+    }
+
+    l_storage[l_i] = '\0';
+
+    return;
 }
