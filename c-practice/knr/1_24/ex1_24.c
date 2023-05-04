96d7f3117f2f628d7367a1b7923d58732893137d
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-04 12:48:56 +0800

    Add boilerplate

--- c-practice/knr/1_24/ex1_24.c
+++ c-practice/knr/1_24/ex1_24.c
@@ -1,1 +1,11 @@
+// Check C program for the following syntax errors:
+// Unclosed (), [], {}, '', "", /**/
+// Incomplete escape sequences
 
+#include <stdio.h>
+
+#define MAXCHAR 10000
+
+int main(void) {
+
+}
