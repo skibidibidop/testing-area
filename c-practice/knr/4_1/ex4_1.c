14cb0e46b9072a3b5028b95a91bc133769770f12
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-05 10:49:50 +0800

    Add boilerplate

---
+++ c-practice/knr/4_1/ex4_1.c
@@ -0,0 +1,12 @@
+// Return position of rightmost occurrence
+// 	of pattern[] in input[]
+// Return -1 if no occurrence
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAXCHAR 10000
+
+int main(void) {
+
+}
