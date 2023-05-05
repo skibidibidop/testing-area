1466135d92b19efb68d0543c731ab11cfc32b09c
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-05 10:54:08 +0800

    Add prompt

--- c-practice/knr/4_1/ex4_1.c
+++ c-practice/knr/4_1/ex4_1.c
@@ -8,5 +8,17 @@
 #define MAXCHAR 10000
 
 int main(void) {
+	char pattern[MAXCHAR];
+	char input[MAXCHAR];
 
+	printf("PATTERN MATCH (REVERSE)\n\n");
+	printf("Finds the first occurrence of a specified ");
+	printf("pattern nearest to the end of your input.\n");
+
+	printf("Pattern: ");
+	get_pattern(pattern);
+	printf("Input: ");
+	get_input(input);
+
+	return 0;
 }
