3b005bcfbb19365ebfc513e8a11ddc9dbe8a1162
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-06 20:44:11 +0000

    Add strcat2

--- c-practice/knr/5_3/ex5_3.c
+++ c-practice/knr/5_3/ex5_3.c
@@ -5,8 +5,25 @@
 
 #define MAXCHAR 10000
 
-int main(void) {
+void strcat2(char *s, char *t);
 
+int main(void) {
+    char s[] = "string1"
+    char t[] = "string2"
+
+    strcat2(&s, &t);
+
+    return 0;
+}
+
+void strcat2(char *s, char *t) {
+    while (*s != '\0') {
+        s++;
+    }
+
+    while ( (*s++ = *t++) != '\0');
+
+    return;
 }
 
 /* Original strcat from Chapter 2
@@ -18,7 +35,7 @@ void strcat(char s[], char t[]) {
         i++;
     }
 
-    while ( (s[i++] = t[j++] != '\0') );
+    while ( (s[i++] = t[j++]) != '\0' );
 
     return;
 }
