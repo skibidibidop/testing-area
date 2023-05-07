6e17d6e012cfcca1cd8cf450b14882a2bf48f977
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-07 00:56:33 +0000

    Add store_input

--- c-practice/knr/5_4/ex5_4.c
+++ c-practice/knr/5_4/ex5_4.c
@@ -6,5 +6,28 @@
 #define MAXCHAR 10000
 
 int main(void) {
-    
+    char s[MAXCHAR];
+    char t[MAXCHAR];
+
+    printf("Original: ");
+    int length_s = store_input(s);
+    printf("Search: ");
+    int length_t = store_input(t);
+
+    return 0;
+}
+
+int store_input(char *input) {
+    char c;
+    int length = 0;
+
+    while ( (c = getchar()) != '\n') {
+        *input = c;
+        input++;
+        length++;
+    }
+
+    *(++input) = '\0';
+
+    return length;
 }
