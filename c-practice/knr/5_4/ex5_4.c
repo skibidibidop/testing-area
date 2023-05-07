3c064ce2c3392f2323f78910bc1eac1f0edfb276
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-07 01:11:08 +0000

    Add strend, print_result

--- c-practice/knr/5_4/ex5_4.c
+++ c-practice/knr/5_4/ex5_4.c
@@ -5,14 +5,22 @@
 
 #define MAXCHAR 10000
 
+int store_input(char *input);
+int strend(char *s, char *t, int len_s, int len_t);
+void print_result(int result, char *s, char *t);
+
 int main(void) {
     char s[MAXCHAR];
     char t[MAXCHAR];
 
     printf("Original: ");
-    int length_s = store_input(s);
+    int len_s = store_input(s);
     printf("Search: ");
-    int length_t = store_input(t);
+    int len_t = store_input(t);
+
+    int result = strend(s, t, len_s, len_t);
+
+    print_result(result, s, t);
 
     return 0;
 }
@@ -30,4 +38,31 @@ int store_input(char *input) {
     *(++input) = '\0';
 
     return length;
+}
+
+int strend(char *s, char *t, int len_s, int len_t) {
+    char *s_end = s + len_s;        // point to s terminator
+    char *t_end = t + len_t;        // point to t terminator
+
+    while (t_end >= t) {
+        if (*t_end != *s_end) {
+            return 0; // t does not occur at end of s
+        }
+
+        s_end--;
+        t_end--;
+    }
+
+    return 1; // t occurs at end of s
+}
+
+void print_result(int result, char *s, char *t) {
+    if (result == 0) {
+        printf("%s does not occur at the end of %s.\n", t, s);
+    }
+    else {
+        printf("%s can be found in the end of %s.\n", t, s)
+    }
+
+    return;
 }
