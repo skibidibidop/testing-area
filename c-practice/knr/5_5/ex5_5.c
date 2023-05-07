893b532de1e64ac871f1ca3c45aa0ff79d6d6639
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-07 15:33:37 +0800

    Add strncat2

--- c-practice/knr/5_5/ex5_5.c
+++ c-practice/knr/5_5/ex5_5.c
@@ -5,6 +5,7 @@
 
 void store_input(char *input);
 char * strncpy2(char *s, char *ct, int n);
+char * strncat2(char *s, char *ct, int n);
 
 int main(void) {
 	// Copy
@@ -17,6 +18,7 @@ int main(void) {
 	store_input(copy_this);
 	printf("No. of characters: ");
 	scanf("%i", &copy_n);
+
 	strncpy2(copy_holder, copy_this, copy_n);
 	printf("String copied: %s\n\n", copy_holder);
 
@@ -26,7 +28,15 @@ int main(void) {
 	int cat_n = 0;
 
 	printf("CONCATENATOR\n");
-	char *concatenated = strncat(
+	printf("First string: ");
+	store_input(cat_first);
+	printf("Second string: ");
+	store_input(cat_second);
+	printf("No. of characters from 2nd string to concatenate: ");
+	scanf("%i", &cat_n);
+
+	strncat2(cat_first, cat_second, cat_n);
+	printf("Concatenated string: %s\n\n", cat_first);
 
 	return 0;
 }
@@ -34,8 +44,8 @@ int main(void) {
 void store_input(char *input) {
 	char c;
 
-	while ( (c = getchar()) != '\n') {
-	*input++ = c;
+	while ( (c = getchar()) != '\n' ) {
+		*input++ = c;
 	}
 
 	*input = '\0';
@@ -68,6 +78,19 @@ char * strncpy2(char *s, char *ct, int n) {
 
 // strncat(s, ct, n): concatenate at most n characters of ct
 // to end of s, terminate with \0, return s
+char * strncat2(char *s, char *ct, int n) {
+	char *s_start = s;
+	char *s_end = s + strlen(s); // point to '\0'
+	char *limit = ct + (n - 1);
+	
+	while (ct <= limit) {
+		*s_end++ = *ct++;
+	}
+
+	*s_end = '\0';
+
+	return s_start;
+}
 
 //strncmp(cs, ct, n): compare at most n characters of cs to
 // ct, return <0 if cs < ct, 0 if cs == ct, >0 if cs > ct
