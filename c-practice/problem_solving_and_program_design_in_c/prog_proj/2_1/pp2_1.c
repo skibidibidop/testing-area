701970c3efed0638224f080ffca8772524583e5b
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-08 23:17:48 +0000

    Add main flow

--- c-practice/problem_solving_and_program_design_in_c/prog_proj/2_1/pp2_1.c
+++ c-practice/problem_solving_and_program_design_in_c/prog_proj/2_1/pp2_1.c
@@ -8,5 +8,20 @@
 #include <stdio.h>
 
 int main(void) {
-    
+    long double prev_odo;
+    long double curr_odo;
+
+    printf("***MILEAGE REIMBURSEMENT CALCULATOR***\n");
+    printf("Enter beginning odometer reading: ");
+    scanf("%lf", &prev_odo);
+    printf("Enter current odometer reading: ");
+    scanf("%lf", &curr_odo);
+
+    reimburse(prev_odo, curr_odo);
+
+    return 0;
+}
+
+void reimburse(long double prev_odo, long double curr_odo) {
+
 }
