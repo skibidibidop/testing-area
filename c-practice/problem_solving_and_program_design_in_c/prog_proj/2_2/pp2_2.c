d23afc53238b7b0446cf8ee5d6f3a1211a11124a
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-09 14:07:38 +0800

    Add print_details

--- c-practice/problem_solving_and_program_design_in_c/prog_proj/2_2/pp2_2.c
+++ c-practice/problem_solving_and_program_design_in_c/prog_proj/2_2/pp2_2.c
@@ -11,8 +11,16 @@
 #define GRAVITY 9.8
 #define CUBICM_TO_KG 1000
 #define W_TO_MW 1000
-#define NINETY_PERCENT 0.9
+#define EFFICIENCY 0.9
 
 int main(void) {
 
+}
+
+void print_details(void) {
+	printf("Hydroelectric Power Calculator\n");
+	printf("Provide the height of the dam and the flow rate
+		in cubic meters to get power in MW.\n");
+
+	return;
 }
