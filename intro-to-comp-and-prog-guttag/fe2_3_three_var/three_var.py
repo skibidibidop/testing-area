8592d9f07e55b8eadf43c912e26d69960c7d5780
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-09 20:41:26 -0400

    Add solution

--- intro-to-comp-and-prog-guttag/fe2_3_three_var/three_var.py
+++ intro-to-comp-and-prog-guttag/fe2_3_three_var/three_var.py
@@ -4,3 +4,28 @@
 # Print largest odd no.
 # If no odd no., print smallest even no.
 
+import sys
+
+x = 9
+y = 5000
+z = 35
+biggest = x
+smallest= x
+
+if (x % 2 != 0) or (y % 2 != 0) or (z % 2 != 0):
+    if y > biggest:
+        biggest = y
+  
+    if z > biggest:
+        biggest = z
+    
+    print(biggest)
+    sys.exit()
+
+if y < smallest:
+    smallest = y
+    
+if z < smallest:
+    smallest = z
+    
+print(smallest)
