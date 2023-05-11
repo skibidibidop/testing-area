d21985d73be1a0b0c129e5b9ae5b08bf32711f3b
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-11 16:57:52 +0800

    Add solution

--- intro-to-comp-and-prog-guttag/4_1_is_in/string_is_in.py
+++ intro-to-comp-and-prog-guttag/4_1_is_in/string_is_in.py
@@ -6,3 +6,23 @@ Date: May 11, 2023
 is_in function checks if a string occurs
 in another
 """
+
+def is_in(string1, string2):
+    """ Check if one of the strings occurs
+        in the other."""
+
+    if string1 in string2 or string2 in string1:
+        return True
+
+    return False
+
+text_one = input("String 1: ")
+text_two = input("String 2: ")
+
+result = is_in(text_one, text_two)
+
+if result == True:
+    print("One of the strings occurs in the other.")
+else:
+    print("These strings don't occur in each other.")
+
