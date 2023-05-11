3b356340302f0b3ecfda1b58e01418149a0fec3a
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-11 14:40:23 +0800

    Add solution

--- intro-to-comp-and-prog-guttag/fe3_1_sum_of_prime/sum_of_prime.py
+++ intro-to-comp-and-prog-guttag/fe3_1_sum_of_prime/sum_of_prime.py
@@ -6,3 +6,26 @@ Date: May 11, 2023
 Print the sum of all prime numbers from
 3 to 999
 """
+
+sum = 0
+
+for num in range(3, 1000, 2):
+    checker = 5
+    flag = 0
+
+    while (checker * checker) <= num:
+        if num % checker == 0 or num % (checker + 2) == 0:
+            flag = 1
+            break
+
+        checker += 6
+
+    if flag == 1 or num % 3 == 0:
+        continue
+
+    sum += num
+
+sum += 3
+
+print(f"Sum: {sum}")
+
