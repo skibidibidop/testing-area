45b07587448931b3d506c6958955fe74c63d56a7
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-11 12:15:25 +0800

    Add solution

--- intro-to-comp-and-prog-guttag/fe3_1_root_pwr/root_pwr.py
+++ intro-to-comp-and-prog-guttag/fe3_1_root_pwr/root_pwr.py
@@ -3,9 +3,31 @@
 Author: Mark Beltran
 Date: May 11, 2023
 
-Check if any positive integer raised
-to the power of 2, 3, 4, or 5 will result
-to the user's input
+Check if any integer raised to the power
+of 2, 3, 4, or 5 will result to the user's
+input
 """
 
+num = int(input("Enter an integer: "))
+# Starting root at 2 since
+# 0 raised to any power is 0 and
+# 1 raised to any power is 1
+root = 2
+# 1 < pwr < 6 according to the finger exercise
+ans = 0
+
+while not ans >= num:
+    for pwr in range(2, 6):
+        ans = root**pwr
+
+        if ans >= num:
+            break
+
+    root += 1
+
+if ans == num:
+    print(f"{root - 1} raised to {pwr} is {num}")
+else:
+    print("No integer raised to the power of 2, 3, 4,",
+            "or 5 will result to your number")
 
