396da8d5ef9feaf04f561d6407efae18c762505c
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-11 11:25:56 +0800

    Add solution

--- intro-to-comp-and-prog-guttag/fe3_1_exhaustive_prime/exhaustive_prime.py
+++ intro-to-comp-and-prog-guttag/fe3_1_exhaustive_prime/exhaustive_prime.py
@@ -11,15 +11,14 @@ This is modified code from Figure 3-2.
 """
 
 x = int(input("Enter an integer greater than 2: "))
-smallest_divisor = None
+biggest_divisor = None
 
 for guess in range(2, x):
     if x % guess == 0:
-        smallest_divisor = guess
-        break
+        biggest_divisor = guess
 
-if smallest_divisor != None:
-    print(f"Smallest divisor of {x} is {smallest_divisor}")
+if biggest_divisor != None:
+    print(f"Biggest divisor of {x} is {biggest_divisor}")
 else:
     print(f"{x} is a prime number.")
 
