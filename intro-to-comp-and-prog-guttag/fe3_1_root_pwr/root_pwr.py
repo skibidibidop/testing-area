37def111bf43d46961d6d7e2972d7748f61fff0b
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-11 11:40:34 +0800

    Add description

---
+++ intro-to-comp-and-prog-guttag/fe3_1_root_pwr/root_pwr.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Mark Beltran
+Date: May 11, 2023
+
+Check if any positive integer raised
+to the power of 2, 3, 4, or 5 will result
+to the user's input
+"""
+
+
