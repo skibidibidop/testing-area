fad82002475fb688a7da861667e63df383c1cfd1
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-12 11:06:25 +0800

    Add description

---
+++ intro-to-comp-and-prog-guttag/fe4_5_find_last/find_last.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Mark Beltran
+Date: May 12, 2023
+
+Find the last occurrence of specified
+substring in string.
+"""
+
+def find_last(full, sub):
+    """
+    full & sub are non-empty strings
+    Returns the index of the last occurrence of
+    sub in full.
+    Returns None if sub does not occur in full.
+    """
+
+
+
+full_str = input("Provide original string: ")
+sub_str = input("Provide substring: ")
