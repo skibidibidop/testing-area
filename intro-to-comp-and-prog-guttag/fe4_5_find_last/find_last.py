020cb5b23c3dd9d8a4090458509d970fe30bc0ca
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-12 15:05:35 +0800

    Add solution

--- intro-to-comp-and-prog-guttag/fe4_5_find_last/find_last.py
+++ intro-to-comp-and-prog-guttag/fe4_5_find_last/find_last.py
@@ -15,7 +15,34 @@ def find_last(full, sub):
     Returns None if sub does not occur in full.
     """
 
+    f_index = len(full) - 1
+    s_index = len(sub) - 1
+    reset = s_index
 
+    while True:
+        if full[f_index] == sub[s_index]:
+            s_index -= 1
+        else:
+            s_index = reset
+
+        f_index -= 1
+
+        if s_index < 0 or f_index < 0:
+            break
+
+    if s_index < 0: # Match
+        f_index += 1
+        return f_index
+
+    return None
 
 full_str = input("Provide original string: ")
 sub_str = input("Provide substring: ")
+
+result = find_last(full_str, sub_str)
+
+if result == None:
+    print("No match.")
+else:
+    print(f"Match at index {result} of the original string.")
+
