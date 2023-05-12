663ad410fb66b884d1fb3a438c1e200881eb79ea
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-12 10:32:54 +0800

    Add solution

--- intro-to-comp-and-prog-guttag/fe4_4_lambda_exp/lambda_exp.py
+++ intro-to-comp-and-prog-guttag/fe4_4_lambda_exp/lambda_exp.py
@@ -9,4 +9,11 @@ Otherwise, divide the 1st arg with the 2nd
 arg and return the quotient.
 """
 
+print("Let's divide! Remember, we can't divide by 0.")
+a = int(input("First number: "))
+b = int(input("Second number: "))
+
+c = (lambda x, y: x / y if y != 0 else None)(a, b)
+
+print(f"Answer: {c}")
 
