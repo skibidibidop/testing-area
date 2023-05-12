01cba65e555c7d61b3bd125286f7363d1c31f539
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-12 10:04:15 +0800

    Add description

---
+++ intro-to-comp-and-prog-guttag/fe4_4_lambda_exp/lambda_exp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Mark Beltran
+Date: May 12, 2023
+
+Use lambda expression with 2 arguments.
+If the second argument is 0, return None.
+Otherwise, divide the 1st arg with the 2nd
+arg and return the quotient.
+"""
+
+
