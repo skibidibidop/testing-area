ae1ac9a08e1497d3fdf6e6bea7d4dafb7c1ff7c2
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-13 17:01:45 +0800

    Add solution

--- python-practice/py4e/ex3_3/ex3_3.py
+++ python-practice/py4e/ex3_3/ex3_3.py
@@ -16,3 +16,19 @@ Score Grade
 If the user enters a value out of range, print a
 suitable error message and exit.
 """
+
+score = float(input("Enter score between 0.0 and 1.0: "))
+
+if score >= 0.9 and score < 1.0:
+    print("A")
+elif score >= 0.8 and score < 0.9:
+    print("B")
+elif score >= 0.7 and score < 0.8:
+    print("C")
+elif score >= 0.6 and score < 0.7:
+    print("D")
+elif score < 0.6:
+    print("F")
+else:
+    print("Provide a score between 0.0 and 1.0.")
+
