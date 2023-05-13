2d030eca40338398a538476e4d33d9e99f5af311
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-13 00:41:22 +0000

    Add description

---
+++ intro-to-comp-and-prog-guttag/fe5_4_raise_sum/raise_sum.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Mark Beltran
+Date: May 13, 2023
+
+Take 2 lists of same length.
+Raise each element of list 1 to the power
+of the element in the same index. 
+Print sum of all raised elements.
+"""
