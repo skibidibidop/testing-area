4015bb115c4b411b2f83f370dc488ad464be3b1a
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-13 00:50:53 +0000

    Add raise_sum_all docstring

--- intro-to-comp-and-prog-guttag/fe5_4_raise_sum/raise_sum.py
+++ intro-to-comp-and-prog-guttag/fe5_4_raise_sum/raise_sum.py
@@ -8,3 +8,15 @@ Raise each element of list 1 to the power
 of the element in the same index. 
 Print sum of all raised elements.
 """
+
+def raise_sum_all(list1, list2):
+    """
+    list1 and list2 are lists of the same length containing
+    numbers. Returns the sum of raising each element in list1
+    to the power of the element in the same index in list2.
+    """
+
+
+
+ = raise_sum_all([1,2], [2,3])
+print(
