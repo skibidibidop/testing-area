1746f13942552218f6c6e558339c6697e7451ed5
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-15 18:10:46 -0400

    Add I Exercise 16 solution

--- htdp/image_area.rkt
+++ htdp/image_area.rkt
@@ -1,1 +1,19 @@
+#|
+Author: Mark Beltran
+Date: May 16, 2023
 
+Count the number of pixels
+in an image.
+|#
+
+(require 2htdp/image)
+
+(define image1
+  (rectangle 40 30 "solid" "red"))
+
+(define image2
+  (rectangle 20 30 "solid" "blue"))
+
+(define (image-area image)
+  (* (image-height image)
+     (image-width image)))
