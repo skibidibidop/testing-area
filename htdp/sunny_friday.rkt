24d9d6680f8a60063f1cec204874130f051d5846
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-15 17:49:39 -0400

    Add I Exercise 15 solution

--- htdp/sunny_friday.rkt
+++ htdp/sunny_friday.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname sunny_friday) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 #|
 Author: Mark Beltran
 Date: May 16, 2023
@@ -8,3 +5,13 @@ Date: May 16, 2023
 If sunny is false or friday is true,
 the output is #true.
 |#
+
+(define sun_stat #true)
+(define fri_stat #false)
+
+(define (sunfri sunny friday)
+  (if (or (false? sunny) (false? (not fri_stat)))
+      #true
+      #false))
+
+(sunfri sun_stat fri_stat)
