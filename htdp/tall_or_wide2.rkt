3cc6424d0c44b75d2758ac4ea3ea710547e1eac7
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-15 18:30:57 -0400

    Add I Exercise 17 solution

--- htdp/tall_or_wide2.rkt
+++ htdp/tall_or_wide2.rkt
@@ -1,1 +1,29 @@
+#|
+Author: Mark Beltran
+Date: May 16, 2023
 
+Classify if image is
+tall, wide, or square.
+|#
+
+(require 2htdp/image)
+
+; Images
+(define image_t
+  (rectangle 30 50 "solid" "red"))
+(define image_w
+  (rectangle 50 30 "solid" "blue"))
+(define image_s
+  (square 30 "solid" "violet"))
+(define image_s2
+  (rectangle 20 20 "solid" "yellow"))
+
+; Function
+(define (proportion image)
+  (cond[(> (image-height image)
+           (image-width image))
+        "Tall"]
+       [(< (image-height image)
+           (image-width image))
+        "Wide"]
+       [else "Square"]))
