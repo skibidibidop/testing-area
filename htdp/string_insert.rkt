b68735e493383832288404b066b07c7735e7ef24
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-15 19:22:42 -0400

    Add I Exercise 19 solution

--- htdp/string_insert.rkt
+++ htdp/string_insert.rkt
@@ -1,1 +1,19 @@
+#|
+Author: Mark Beltran
+Date: May 16, 2023
 
+Insert _ at selected index of string.
+Reject if empty string.
+|#
+
+(define START 0)
+
+(define (string_insert str num)
+  (if (or (empty? str)
+          (>= num (string-length str))
+          (not (string? str)))
+      (error "Invalid index number.")
+      (string-append
+       (substring str START num)
+       "_"
+       (substring str num (string-length str)))))
