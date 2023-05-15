62860e78722d35fe3f0f1409ceaf3b15a363ebc7
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-15 17:31:23 +0800

    Create file, add description

---
+++ htdp/distance.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname distance) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: May 15, 2023
+
+Compute the distance from the origin (0, 0)
+to the provided point.
+|#
