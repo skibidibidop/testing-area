9c2a074346de125a481d1c01f969ae59d6cf4c72
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-16 17:46:05 +0800

    Add convert function

--- htdp/io_ctof/io_ctof.rkt
+++ htdp/io_ctof/io_ctof.rkt
@@ -12,13 +12,22 @@ Write converted value to another file.
 
 (require 2htdp/batch-io)
 
+; Constants in the C to F conversion formula
 (define F_CONST32 32.0)
 (define F_CONSTFRAC 9/5)
 
 ; Convert Celsius to Fahrenheit
 (define (ctof c)
-  (string-append "Fahrenheit: "
-                 (number->string-digits
-                  (+ F_CONST32
-                     (* c F_CONSTFRAC))
-                  2)))
+  (+ F_CONST32
+     (* c F_CONSTFRAC)))
+
+; Read C from input, convert to F, write to output
+(define (convert in out)
+  (write-file out
+              (string-append
+               (number->string-digits
+                (ctof
+                 (string->number
+                  (read-file in)))
+                2)
+               "\n")))
