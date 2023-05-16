a0c91c78ad0ebf54fe03c51ed5287df890142a0d
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-16 16:41:21 +0800

    Create dir, initial file with description

---
+++ htdp/io_ctof/io_ctof.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname io_ctof) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: May 16, 2023
+
+Read Celsius values from 1 file.
+Convert to Fahrenheit.
+Write converted value to another file.
+|#
+
+(require 2htdp/batch-io)
