8fabd114580c6e5c3d77c38727fdac68b5802720
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-15 21:54:45 -0400

    Add I Exercise 20 solution

--- htdp/string_delete.rkt
+++ htdp/string_delete.rkt
@@ -5,3 +5,11 @@ Date: May 16, 2023
 Delete ith element in provided string.
 Reject empty strings.
 |#
+
+(define (string_delete str num)
+  (if (or (empty? str) (>= num (string-length str)))
+      (error "Invalid input!")
+      (string-append
+       (substring str 0 num)
+       (substring str (+ num 1) (- (string-length str) 1)))))
+              
