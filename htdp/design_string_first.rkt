18dab95d897cdc94c743983686c3219ab167cabc
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-18 10:33:52 +0800

    Create design_string_first.rkt, add description

---
+++ htdp/design_string_first.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname design_string_first) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: May 18, 2023
+
+Extract the last character from non-empty string.
+|#
