f0b08c22b180d50fb630142ed57e6115f824dc2e
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-18 13:46:23 +0800

    Create moving_car.rkt

---
+++ htdp/moving_car.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname moving_car) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: May 18, 2023
+
+Render moving car. Stop after the car is out of the scene.
+|#
