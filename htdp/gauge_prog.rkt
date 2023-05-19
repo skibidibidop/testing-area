6aa024f42af23428d2e37b32dc4f77ce211440a0
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-19 16:38:43 +0800

    Add I Exercise 47 solution

--- htdp/gauge_prog.rkt
+++ htdp/gauge_prog.rkt
@@ -11,13 +11,12 @@ Happiness gauge
 (require 2htdp/image)
 (require 2htdp/universe)
 
-(define scaler 1)
+(define scaler 2)
 (define SCENE_WIDTH (* scaler 100))
 (define SCENE_HEIGHT (* scaler 10))
 (define X_CENTER (/ SCENE_WIDTH 2))
 (define Y_CENTER (/ SCENE_HEIGHT 2))
 
-; IMAGES
 (define gauge
   (rectangle SCENE_WIDTH SCENE_HEIGHT
              "solid" "red"))
@@ -30,6 +29,11 @@ Happiness gauge
   (/ (image-height gauge) 2))
 
 (define x_coord X_CENTER)
+(define lower_limit (- 0 X_CENTER))
+(define upper_limit X_CENTER)
+
+(define happy_up (/ SCENE_WIDTH 3))
+(define happy_down (/ SCENE_WIDTH 5))
 
 ; FUNCTION DEFINITIONS /////////////////////
 ; A WorldState is a Number
@@ -41,24 +45,30 @@ Happiness gauge
 ; Gives big-bang the image to be rendered
 ; based on the current WorldState
 (define (give_img x_pos)
-  (place-image gauge
-               (- SCENE_WIDTH x_pos)
-               IMG_Y_CENTER
-               bg))
+  (place-image gauge x_pos IMG_Y_CENTER bg))
 
 ; WorldState -> WorldState
 ; Gives big-bang the new WorldState for
 ; each tick
 (define (give_x cw)
-  (+ cw 0.1))
+  (if (<= cw lower_limit)
+      lower_limit
+      (- cw 0.1)))
 
 ; WorldState String -> WorldState
 ; Gives new WorldState to big-bang for
 ; each keystroke
-(define (change_mood cw ke) ...)
+(define (change_mood cw ke)
+  (cond[(key=? ke "up")
+        (if (>= (+ cw happy_up)
+                upper_limit)
+            upper_limit
+            (+ cw happy_up))]
+       [(key=? ke "down") (- cw happy_down)]))
 
 ; MAIN ////////////////////////////////////
 (big-bang x_coord
   [to-draw give_img]
-  [on-tick give_x])
+  [on-tick give_x]
+  [on-key change_mood])
   
