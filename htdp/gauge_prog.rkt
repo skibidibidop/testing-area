b9cecb73b29578f4004762cf079231128d9977d8
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-19 16:03:56 +0800

    Add rendering, tick handler

--- htdp/gauge_prog.rkt
+++ htdp/gauge_prog.rkt
@@ -5,21 +5,31 @@
 Author: Mark Beltran
 Date: May 19, 2023
 
-Happiness gauge for cat
+Happiness gauge
 |#
 
 (require 2htdp/image)
 (require 2htdp/universe)
 
-; MEASUREMENTS
-(define scale 1)
-(define SCENE_WIDTH (* scale 100))
-(define SCENE_HEIGHT (* scale 10))
+(define scaler 1)
+(define SCENE_WIDTH (* scaler 100))
+(define SCENE_HEIGHT (* scaler 10))
+(define X_CENTER (/ SCENE_WIDTH 2))
+(define Y_CENTER (/ SCENE_HEIGHT 2))
 
 ; IMAGES
 (define gauge
   (rectangle SCENE_WIDTH SCENE_HEIGHT
              "solid" "red"))
+(define bg
+  (empty-scene SCENE_WIDTH SCENE_HEIGHT))
+
+(define IMG_X_CENTER
+  (/ (image-width gauge) 2))
+(define IMG_Y_CENTER
+  (/ (image-height gauge) 2))
+
+(define x_coord X_CENTER)
 
 ; FUNCTION DEFINITIONS /////////////////////
 ; A WorldState is a Number
@@ -28,3 +38,27 @@ Happiness gauge for cat
 ; border
 
 ; WorldState -> Image
+; Gives big-bang the image to be rendered
+; based on the current WorldState
+(define (give_img x_pos)
+  (place-image gauge
+               (- SCENE_WIDTH x_pos)
+               IMG_Y_CENTER
+               bg))
+
+; WorldState -> WorldState
+; Gives big-bang the new WorldState for
+; each tick
+(define (give_x cw)
+  (+ cw 0.1))
+
+; WorldState String -> WorldState
+; Gives new WorldState to big-bang for
+; each keystroke
+(define (change_mood cw ke) ...)
+
+; MAIN ////////////////////////////////////
+(big-bang x_coord
+  [to-draw give_img]
+  [on-tick give_x])
+  
