7c0755033d787fe7234ed8767321a7695b18dd6d
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-19 18:53:52 -0400

    Add I Exercise 49 solution

--- htdp/cond_rocket.rkt
+++ htdp/cond_rocket.rkt
@@ -6,3 +6,31 @@ Refactor code from Exercise 49 to use only
 one place-image expression with the help of
 nested conds.
 |#
+
+(require 2htdp/image)
+
+(define WIDTH  100)
+(define HEIGHT  60)
+(define MTSCN  (empty-scene WIDTH HEIGHT)) ; short for empty scene
+
+(define ROCKET
+  (triangle 5 "solid" "red"))
+(define ROCKET-CENTER-TO-TOP
+  (- HEIGHT (/ (image-height ROCKET) 2)))
+ 
+(define (create-rocket-scene.v5 h)
+  (cond
+    [(cond[(> h ROCKET-CENTER-TO-TOP)
+           (= h ROCKET-CENTER-TO-TOP)]
+          [else (= h h)])
+     (place-image ROCKET 50 h MTSCN)]))
+
+  #| ORIGINAL CODE:
+  (cond
+    [(<= h ROCKET-CENTER-TO-TOP)
+     (place-image ROCKET 50 h MTSCN)]
+    [(> h ROCKET-CENTER-TO-TOP)
+     (place-image ROCKET 50 ROCKET-CENTER-TO-TOP MTSCN)]))
+|#
+
+(create-rocket-scene.v5 50)
