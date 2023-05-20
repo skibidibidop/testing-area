6bc91267166640e562b72dece49495a4a6a3ade7
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-20 18:15:08 +0800

    Create launching_rocket_cd.rkt with description

---
+++ htdp/launching_rocket_cd.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname launching_rocket_cd) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: May 20, 2023
+
+Solve along with sample problem for launching
+rocket countdown.
+|#
