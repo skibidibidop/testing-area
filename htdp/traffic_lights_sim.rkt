c6a5fba2eee24c8be8c32f8e2ea5fbdd917383ef
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-19 21:21:40 -0400

    Add definitions: images for lights

--- htdp/traffic_lights_sim.rkt
+++ htdp/traffic_lights_sim.rkt
@@ -4,3 +4,10 @@ Date: May 20, 2023
 
 Simulation of a rudimentary traffic light system.
 |#
+
+(require 2htdp/image)
+(require 2htdp/universe)
+
+(define red_light (circle 10 "solid" "red"))
+(define green_light (circle 10 "solid" "green"))
+(define yellow_light (circle 10 "solid" "yellow"))
