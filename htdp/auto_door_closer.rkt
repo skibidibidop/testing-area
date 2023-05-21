f087e802b47f40750a690bd46767901272fd2350
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-21 16:22:41 +0800

    Create auto_door_closer.rkt w/ description

---
+++ htdp/auto_door_closer.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname auto_door_closer) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: May 21, 2023
+
+Sample problem: automatic door closer.
+|#
