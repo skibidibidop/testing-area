db50750a5736a2dcc7e46ce1186dc1a779c06fcb
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-21 18:31:21 +0800

    Add tests for door_action function

--- htdp/auto_door_closer.rkt
+++ htdp/auto_door_closer.rkt
@@ -23,14 +23,33 @@ Closed can be locked with a key
 (define CLOSED "closed")
 (define OPEN "open")
 
+(define LOCK_ACT "l")
+(define UNLOCK_ACT "u")
+(define OPEN_ACT " ")
+
 ; DoorState -> DoorState
 ; If DoorState is OPEN, closes the
 ; door during one tick
-(define (door_closer dc_ds) ...)
+(check-expect (door_closer LOCKED) LOCKED)
+(check-expect (door_closer CLOSED) CLOSED)
+(check-expect (door_closer OPEN) CLOSED)
+(define (door_closer dc_ds)
+  (cond[(string=? dc_ds LOCKED) LOCKED]
+       [(string=? dc_ds CLOSED) CLOSED]
+       [(string=? dc_ds OPEN) CLOSED]))
 
 ; DoorState KeyEvent -> DoorState
 ; Detects actions (key events) and determines
 ; if they are valid based on current DoorState
+(check-expect (door_action LOCKED OPEN_ACT) LOCKED)
+(check-expect (door_action LOCKED LOCK_ACT) LOCKED)
+(check-expect (door_action LOCKED UNLOCK_ACT) CLOSED)
+(check-expect (door_action CLOSED OPEN_ACT) OPEN)
+(check-expect (door_action CLOSED LOCK_ACT) LOCKED)
+(check-expect (door_action CLOSED UNLOCK_ACT) CLOSED)
+(check-expect (door_action OPEN OPEN_ACT) OPEN)
+(check-expect (door_action OPEN LOCK_ACT) OPEN)
+(check-expect (door_action OPEN UNLOCK_ACT) OPEN)
 (define (door_action da_ds da_ke) ...)
 
 ; DoorState -> Image
@@ -38,7 +57,8 @@ Closed can be locked with a key
 (define (door_render dr_ds) ...)
 
 ; DoorState -> DoorState
-(big-bang bb_ds
+(define (auto_close ac_ds)
+  (big-bang ac_ds)
   [to-draw door_render]
   [on-tick door_closer 1]
   [on-key door_action])
