da5e50a6dac466561a3a54c99d20cfcb8e47191c
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-22 16:01:31 +0800

    Add main, put_dot func, x_move func

--- htdp/red_dot.rkt
+++ htdp/red_dot.rkt
@@ -5,19 +5,55 @@
 Author: Mark Beltran
 Date: May 22, 2023
 
-Move red dot on canvas with keys.
+Move red dot across canvas.
 Mouse click to reset.
+Use structures.
 |#
 
 (require 2htdp/image)
 (require 2htdp/universe)
 
 (define scaler 10)
+(define SCENE_WIDTH (* scaler 10))
+(define SCENE_HEIGHT (* scaler 10))
+(define SCENE_XCENTER (/ SCENE_WIDTH 2))
+(define SCENE_YCENTER (/ SCENE_HEIGHT 2))
 
-(define BG (empty-scene (* scaler 10)
-                         (* scaler 10)))
+(define BG (empty-scene SCENE_WIDTH
+                        SCENE_HEIGHT))
 (define DOT (circle (* scaler 0.3)
                     "solid" "red"))
 
-; A posn is a structure that represents the state
-; of the world.
+; Movement speed of DOT, pixels/tick
+(define MOVE_SPEED 3)
+
+; A posn is a structure that represents
+; the current world state.
+;     (make-posn [Number Number])
+; Interpretation: x-coordinate, y-coordinate
+
+; posn -> posn
+(define (main p0)
+  (big-bang p0
+    [on-tick x_move]
+    [on-mouse reset_dot]
+    [to-draw put_dot]))
+
+; posn -> Image
+; Renders DOT based on the current world state.
+(check-expect (put_dot (make-posn 5 SCENE_Y_CENTER)
+                       (place-image DOT 5 SCENE_Y_CENTER BG)))
+(define (put_dot current_state)
+  (place-image DOT
+               (posn-x current_state)
+               (posn-y current_state)
+               BG))
+
+; posn -> posn
+; Increases the x-coordinate of current_state by
+; MOVE_SPEED (pixels/tick)
+(check-expect (x_move (make-posn 15 0) (make-posn (+ 15 MOVE_SPEED) 0)))
+(define (x_move current_state)
+  (make-posn (+ (posn-x current_state) MOVE_SPEED) (posn-y current_state)))
+
+
