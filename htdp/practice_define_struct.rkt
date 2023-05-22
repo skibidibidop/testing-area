1e556304ceb19c6880a306330ca4123c0a52e880
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-21 23:48:41 -0400

    Add data definitions

--- htdp/practice_define_struct.rkt
+++ htdp/practice_define_struct.rkt
@@ -5,8 +5,16 @@ Date: May 22, 2023
 Practice defining structure types
 |#
 
-(define-struct contact_entry [name home_num office_num cell_num])
+; A phone_num is a structure:
+; (make-phone_num [Number Number])
+; Interpretation: area code, phone number
 (define-struct phone_num [area_code number])
+
+; A contact_entry is a structure:
+; (make-contact_entry [String phone_num phone_num phone_num])
+; Interpretation: the name, home number, office number, and
+; cellphone number of a contact
+(define-struct contact_entry [name home_num office_num cell_num])
 
 (define contact_mark
   (make-contact_entry "Mark"
