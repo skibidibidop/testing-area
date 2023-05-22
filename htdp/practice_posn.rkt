037e2c9bb9603fee5ecdf63c6e2bb724ac7ae57c
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-21 21:51:19 -0400

    Update practice_posn.rkt

--- htdp/practice_posn.rkt
+++ htdp/practice_posn.rkt
@@ -12,4 +12,11 @@ Practice posn structure
 (check-expect (distance_to_0 (make-posn 8 6)) 10)
 (check-expect (distance_to_0 (make-posn 5 12)) 13)
 (define (distance_to_0 ap)
-  )
+  (sqrt
+   (+
+    (sqr (posn-x ap))
+    (sqr (posn-y ap)))))
+
+(distance_to_0 (make-posn 3 4))
+(distance_to_0 (make-posn 6 (* 2 4)))
+(+ (distance_to_0 (make-posn 12 5)) 10)
