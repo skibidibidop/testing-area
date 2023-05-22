779260314a7730a583af966f02ef8da91985de8a
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-22 16:55:32 +0800

    Add ufo_tracking.rkt with description

---
+++ htdp/ufo_tracking.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ufo_tracking) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: May 22, 2023
+
+Keep track of UFO moving across the canvas
+at varying speed.
+|#
+
+(require 2htdp/image)
+(require 2htdp/universe)
