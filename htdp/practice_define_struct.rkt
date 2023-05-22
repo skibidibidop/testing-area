640cf6bbbb210c39bb41c96285ed175684e937aa
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-21 23:24:16 -0400

    Update practice_define_struct.rkt

--- htdp/practice_define_struct.rkt
+++ htdp/practice_define_struct.rkt
@@ -4,3 +4,14 @@ Date: May 22, 2023
 
 Practice defining structure types
 |#
+
+(define-struct contact_entry [name home_num office_num cell_num])
+(define-struct phone_num [area_code number])
+
+(define contact_mark
+  (make-contact_entry "Mark"
+                      [make-phone_num 123 456789]
+                      [make-phone_num 456 098746]
+                      [make-phone_num 789 091872]))
+
+(phone_num-number (contact_entry-home_num contact_mark))
