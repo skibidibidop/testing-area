79e98b3809f5d36ef3e6c6ef636fed98636546a1
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-23 18:35:12 -0400

    Add converting function

--- htdp/time_to_seconds.rkt
+++ htdp/time_to_seconds.rkt
@@ -19,3 +19,19 @@ Convert time (hh:mm:ss) to seconds.
 ; (make-time hour_time non_hour_time non_hour_time)
 ; Interpretation: represents time passed
 ; since midnight
+(define-struct time [hrs mins secs])
+
+(define S_IN_H 3600)
+(define S_IN_M 60)
+
+(define tester1 (make-time 13 0 45))
+(define tester2 (make-time 22 11 35))
+
+; time -> integer
+; Converts time to seconds
+(check-expect (time->seconds tester1) 46845)
+(check-expect (time->seconds tester2) 79895)
+(define (time->seconds user_time)
+  (+ (* (time-hrs user_time) S_IN_H)
+     (* (time-mins user_time) S_IN_M)
+     (time-secs user_time)))
