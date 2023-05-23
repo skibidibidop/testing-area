dcbc429f24ff7cc26a1df656f4965d31b3465557
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-23 17:41:09 +0800

    Create get_3d_distance.rkt

---
+++ htdp/get_3d_distance.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname get_3d_distance) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: May 23, 2023
+
+Computes the distance of objects in
+a 3D space to the origin.
+|#
