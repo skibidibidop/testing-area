c021501702e3710ec95eee422afea4a1bc4c679e
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-23 18:23:36 -0400

    Add data definitions

--- htdp/time_to_seconds.rkt
+++ htdp/time_to_seconds.rkt
@@ -4,3 +4,18 @@ Date: May 24, 2023
 
 Convert time (hh:mm:ss) to seconds.
 |#
+
+; A non_hour_time is a positive integer
+; from 0 to 59
+; Interpretation: can represent seconds
+; and minutes
+
+; An hour_time is a positive integer
+; from 0 to 23
+; Interpretation: represents hours passed
+; since midnight
+
+; A time is a structure:
+; (make-time hour_time non_hour_time non_hour_time)
+; Interpretation: represents time passed
+; since midnight
