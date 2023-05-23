0a1025e400d9be5ee3fde0105f5d1ec7e4168aa9
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-05-23 17:33:44 +0800

    Add Exercise 79 solution

--- htdp/data_definition_examples.rkt
+++ htdp/data_definition_examples.rkt
@@ -8,3 +8,35 @@ Date: May 23, 2023
 Add examples to given data definitions.
 |#
 
+; A Color is one of:
+; --- "white"
+; --- "yellow"
+; --- "orange"
+; --- "green"
+; --- "red"
+; --- "blue"
+; --- "black"
+; Example: (define sample_color "blue")
+
+; H is a Number between 0 and 100
+; -- 0 to 49: Depressed
+; -- 50 to 60: Meh
+; -- 61 to 80: Content
+; -- 81 to 95: Happy
+; -- 96 to 100: Ecstatic
+; Interpretation: represents a happiness value
+
+(define-struct person [fstname lstname male?])
+; A Person is a structure:
+; (make-person [String String Boolean])
+; Interpretation: contains the first and last name,
+; and gender of an individual
+(make-person "John" "Smith" #true)
+
+(define-struct dog [owner name age happiness])
+; A Dog is a structure:
+; (make-dog [Person String PositiveInteger H])
+; Interpretation: contains details about the dog's
+; owner, the dog's name, its age, and happiness level
+(make-dog (make-person "John" "Smith" #true)
+           "Optimus Prime" 10 81)
---
+++ htdp/data_definition_examples.rkt~
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname data_definition_examples) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: May 23, 2023
+
+Add examples to given data definitions.
+|#
+
