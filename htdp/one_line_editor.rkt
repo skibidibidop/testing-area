129d001285475b4d2cfafb0bbef5be3512bd8f64
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-23 20:46:16 -0400

    Add definition for structure (editor)

--- htdp/one_line_editor.rkt
+++ htdp/one_line_editor.rkt
@@ -2,5 +2,13 @@
 Author: Mark Beltran
 Date: May 24, 2023
 
-One-line text editor
+One line text editor
 |#
+
+(define-struct editor [pre post])
+; editor: a structure
+; (make-editor string string)
+; Interpretation: (make-editor left right)
+; One line editor, visible text is
+; (string-append left right), cursor is rendered
+; between the two parts
