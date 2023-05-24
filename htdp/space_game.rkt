d688cd062ce1bba9dd3b35d00ed614861ee2e506
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-23 20:13:54 -0400

    Add data definitions

--- htdp/space_game.rkt
+++ htdp/space_game.rkt
@@ -4,3 +4,16 @@ Date: May 24, 2023
 
 Space game?
 |#
+
+; non_neg: a non-negative Integer
+; Example: (define coord 50)
+; Interpretation: integer >= 0, used to
+; represent x-coordinate of tank
+
+; space_game: a structure
+; (make-space_game [Number non_neg])
+; Interpretation: distance (no. of pixels)
+; of UFO from the top edge, distance of tank
+; from the left edge
+; Example: (define sample_sg 30 5)
+(define-struct space_game [ufo tank])
