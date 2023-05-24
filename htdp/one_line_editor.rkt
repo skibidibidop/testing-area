ef8d0361b183b1c2a0ca9b0b1652d300d81aac68
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-23 22:45:32 -0400

    Add definitions, (render)

--- htdp/one_line_editor.rkt
+++ htdp/one_line_editor.rkt
@@ -5,10 +5,42 @@ Date: May 24, 2023
 One line text editor
 |#
 
+(require 2htdp/image)
+(require 2htdp/universe)
+
+(define scaler 10)
+(define box_width (* scaler 20))
+(define box_height (* scaler 2))
+(define xcenter (/ box_width 2))
+(define ycenter (/ box_height 2))
+(define text_box (empty-scene box_width box_height))
+(define cursor (rectangle 1 box_height "solid" "red"))
+
 (define-struct editor [pre post])
 ; editor: a structure
 ; (make-editor string string)
-; Interpretation: (make-editor left right)
+; Interpretation: (make-editor l r)
 ; One line editor, visible text is
 ; (string-append left right), cursor is rendered
 ; between the two parts
+
+; editor -> image
+; Puts text image and cursor in text box. Cursor
+; position is based on data from editor
+(check-expect (render (make-editor "Hith" "ere!"))
+              (place-image (beside (text "Hith" 16 "black")
+                                   cursor
+                                   (text "ere!" 16 "black"))
+                           xcenter ycenter
+                           text_box))
+
+(define (render whole_txt)
+  (place-image (beside (text (editor-pre whole_txt) 16 "black")
+                        cursor
+                        (text (editor-post whole_txt) 16 "black"))
+               xcenter ycenter
+               text_box))
+
+(define input (make-editor "Testing1" "Testing2!"))
+
+(render input)
