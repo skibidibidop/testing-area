a601bb1df4a22ab6edfe6c0935dd3af1d1071ed9
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-25 19:11:12 -0400

    Add (wstate [h x] ) data definition

--- htdp/chameleon_pet.rkt
+++ htdp/chameleon_pet.rkt
@@ -10,3 +10,12 @@ Date: May 26, 2023
 - Keystroke "b" turns the chameleon blue.
 - Keystroke "g" turns the chameleon green.
 |#
+
+(define-struct wstate [h x])
+; wstate: a structure
+; (make-wstate Number Number)
+; Interpretation: (make-wstate happiness cham_x)
+; - wstate-h is the happiness gauge's center point.
+; The happiness gauge being completely out of the scene
+; represents depleted happiness.
+; - wstate-x the chameleon's x-coordinate.
