bc93ab72d386afcfdcf88a920fc134abea040205
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-25 17:53:56 -0400

    Create chameleon_pet.rkt w/ description

---
+++ htdp/chameleon_pet.rkt
@@ -0,0 +1,12 @@
+#|
+Author: Mark Beltran
+Date: May 26, 2023
+
+- Chameleon walks from left to right.
+- If right border is reached, teleport to left border.
+- Happiness gauge decreases over time.
+- Happiness can be increased by 2 with keystroke "down".
+- Keystroke "r" turns the chameleon red.
+- Keystroke "b" turns the chameleon blue.
+- Keystroke "g" turns the chameleon green.
+|#
