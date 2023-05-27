b59c6ed131ee846583b75098e85dd123ea6ebca2
skibidibidop <dev.mapb@gmail.com>
2023-05-27 16:33:49 +0800

    Create simple_space_invaders.rkt w/ description

---
+++ htdp/simple_space_invaders.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname simple_space_invaders) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: May 27, 2023
+
+Simple space invaders game
+|#
