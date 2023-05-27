2674e73008b30e85a34d9003a401d4b0695dc88b
skibidibidop <dev.mapb@gmail.com>
2023-05-27 16:12:42 +0800

    Improve constant definitions

--- htdp/chameleon_pet.rkt
+++ htdp/chameleon_pet.rkt
@@ -24,13 +24,18 @@ Date: May 26, 2023
 (define SCN_MAXX (* SCALER 30))
 (define SCN_MINY 0)
 (define SCN_MAXY (* SCALER 10))
+(define SCN_XCENTER (/ SCN_MAXX 2))
+(define BG_PARTITION (/ SCN_MAXX 3))
 (define CHAM_ANGLE 140)
 (define HG_UP 2)
+(define HG_XPOS SCN_XCENTER)
+(define HG_YPOS (/ SCALER 2))
 
 (define hgauge
-  (rectangle SCN_MAXX SCALER "solid" "red"))
-(define hg_xpos (/ SCN_MAXX 2))
-(define hg_ypos (/ (image-height hgauge) 2))
+  (place-image
+   (rectangle SCN_MAXX SCALER "solid" "purple")
+   HG_XPOS HG_YPOS
+   (empty-scene SCN_MAXX SCALER)))
 
 (define cham
   (rhombus (* SCALER 4) CHAM_ANGLE "outline" "red"))
@@ -39,8 +44,10 @@ Date: May 26, 2023
 (define cham_ypos (- SCN_MAXY
                      (/ (image-height cham) 2)))
 
-(define bg (empty-scene SCN_MAXX SCN_MAXY))
-(define SCN_XCENTER (/ (image-width bg) 2))
+(define bg
+  (beside (empty-scene BG_PARTITION SCN_MAXY "green")
+          (empty-scene BG_PARTITION SCN_MAXY "white")
+          (empty-scene BG_PARTITION SCN_MAXY "red")))
 
 (define-struct vcham [hg x color])
 ; vcham: a structure
@@ -57,10 +64,10 @@ Date: May 26, 2023
 ; Decreases vcham-hg by HUNGER per tick.
 ; The chameleon's position resets when its right side touches
 ; the right border.
-(check-expect (time_step (make-vcham hg_xpos cham_xpos "red"))
-              (make-vcham (- hg_xpos HUNGER) (+ cham_xpos MOVSPD) "red"))
-(check-expect (time_step (make-vcham hg_xpos (- SCN_MAXX cham_xhalf)  "red"))
-              (make-vcham (- hg_xpos HUNGER) cham_xhalf "red"))
+(check-expect (time_step (make-vcham HG_XPOS cham_xpos "red"))
+              (make-vcham (- HG_XPOS HUNGER) (+ cham_xpos MOVSPD) "red"))
+(check-expect (time_step (make-vcham HG_XPOS (- SCN_MAXX cham_xhalf)  "red"))
+              (make-vcham (- HG_XPOS HUNGER) cham_xhalf "red"))
 (define (time_step chamln)
   (cond[(>= (vcham-x chamln) (- SCN_MAXX cham_xhalf))
         (make-vcham (- (vcham-hg chamln) HUNGER)
@@ -72,7 +79,7 @@ Date: May 26, 2023
 
 ; vcham -> image
 ; Signals (render) to change the color of cham based on vcham-color
-(check-expect (change_color (make-vcham hg_xpos cham_xpos "red"))
+(check-expect (change_color (make-vcham HG_XPOS cham_xpos "red"))
               (rhombus (* SCALER 4) CHAM_ANGLE "outline" "red"))
 (define (change_color chamln)
               (cond[(string=? (vcham-color chamln) "red")
@@ -85,29 +92,29 @@ Date: May 26, 2023
 ; vcham -> Image
 ; Renders images of the chameleon and happiness gauge depending
 ; on the current vcham.
-(check-expect (render (make-vcham hg_xpos cham_xpos "red"))
+(check-expect (render (make-vcham HG_XPOS cham_xpos "red"))
               (place-images (list hgauge
                                   (rhombus (* SCALER 4) CHAM_ANGLE "outline" "red"))
-                            (list (make-posn hg_xpos hg_ypos)
+                            (list (make-posn HG_XPOS HG_YPOS)
                                   (make-posn cham_xpos cham_ypos))
                             bg))
 (define (render chamln)
   (place-images
    (list hgauge
          (change_color chamln))
-   (list (make-posn (vcham-hg chamln) hg_ypos)
+   (list (make-posn (vcham-hg chamln) HG_YPOS)
          (make-posn (vcham-x chamln) cham_ypos))
    bg))
 
 ; (increment) tester values and results
-(define inc_t0 (make-vcham hg_xpos cham_xpos "red"))
-(define inc_r0 (make-vcham hg_xpos cham_xpos "red"))
-(define inc_t1 (make-vcham (- hg_xpos 1) cham_xpos "red"))
-(define inc_r1 (make-vcham hg_xpos cham_xpos "red"))
-(define inc_t2 (make-vcham (- hg_xpos 2) cham_xpos "red"))
-(define inc_r2 (make-vcham hg_xpos cham_xpos "red"))
-(define inc_t3 (make-vcham (- hg_xpos 3) cham_xpos "red"))
-(define inc_r3 (make-vcham (- hg_xpos 1) cham_xpos "red"))
+(define inc_t0 (make-vcham HG_XPOS cham_xpos "red"))
+(define inc_r0 (make-vcham HG_XPOS cham_xpos "red"))
+(define inc_t1 (make-vcham (- HG_XPOS 1) cham_xpos "red"))
+(define inc_r1 (make-vcham HG_XPOS cham_xpos "red"))
+(define inc_t2 (make-vcham (- HG_XPOS 2) cham_xpos "red"))
+(define inc_r2 (make-vcham HG_XPOS cham_xpos "red"))
+(define inc_t3 (make-vcham (- HG_XPOS 3) cham_xpos "red"))
+(define inc_r3 (make-vcham (- HG_XPOS 1) cham_xpos "red"))
 
 ; vcham String -> vcham
 ; Allows happiness to be increased up to the max level only (the gauge's
@@ -118,21 +125,21 @@ Date: May 26, 2023
 (check-expect (increment inc_t3 "down") inc_r3)
 (define (increment chamln ke)
   (cond[(>= (+ (vcham-hg chamln) HG_UP) SCN_XCENTER)
-        (make-vcham hg_xpos (vcham-x chamln) (vcham-color chamln))]
+        (make-vcham HG_XPOS (vcham-x chamln) (vcham-color chamln))]
        [else (make-vcham (+ (vcham-hg chamln) HG_UP)
                          (vcham-x chamln)
                          (vcham-color chamln))]))
 
 ; (change_mood) test values and results
-(define cm_t0 (make-vcham hg_xpos cham_xpos "red"))
-(define cm_t0_r0 (make-vcham hg_xpos cham_xpos "red"))
-(define cm_t0_rred (make-vcham hg_xpos cham_xpos "red"))
-(define cm_t1 (make-vcham (- hg_xpos 1) cham_xpos "red"))
-(define cm_t1_r1 (make-vcham hg_xpos cham_xpos "red"))
-(define cm_t1_rblue (make-vcham (- hg_xpos 1) cham_xpos "blue"))
-(define cm_t2 (make-vcham (- hg_xpos 3) cham_xpos "red"))
-(define cm_t2_r2 (make-vcham (- hg_xpos 1) cham_xpos "red"))
-(define cm_t2_rgreen (make-vcham (- hg_xpos 3) cham_xpos "green"))
+(define cm_t0 (make-vcham HG_XPOS cham_xpos "red"))
+(define cm_t0_r0 (make-vcham HG_XPOS cham_xpos "red"))
+(define cm_t0_rred (make-vcham HG_XPOS cham_xpos "red"))
+(define cm_t1 (make-vcham (- HG_XPOS 1) cham_xpos "red"))
+(define cm_t1_r1 (make-vcham HG_XPOS cham_xpos "red"))
+(define cm_t1_rblue (make-vcham (- HG_XPOS 1) cham_xpos "blue"))
+(define cm_t2 (make-vcham (- HG_XPOS 3) cham_xpos "red"))
+(define cm_t2_r2 (make-vcham (- HG_XPOS 1) cham_xpos "red"))
+(define cm_t2_rgreen (make-vcham (- HG_XPOS 3) cham_xpos "green"))
 
 ; vcham String -> vcham
 ; Changes the following properties depending on the keystroke:
