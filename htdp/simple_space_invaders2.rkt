4ce1da479e1a2297e9ab8171ae9520863982db26
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-31 18:42:09 -0400

    Add definitions: images

--- htdp/simple_space_invaders2.rkt
+++ htdp/simple_space_invaders2.rkt
@@ -8,3 +8,22 @@ to represent the game state.
 
 (require 2htdp/image)
 (require 2htdp/universe)
+
+; IMAGES ///////////////////////////////////////////////////
+
+(define SCALER 10)
+
+(define SWIDTH (* SCALER 20))
+(define SHEIGHT (* SCALER 10))
+
+(define UFO_IMG (circle (* SCALER 2) "solid" "magenta"))
+
+(define TANK_IMG (rectangle (* SCALER 3) SCALER
+                            "solid" "light steel blue"))
+
+(define MSL_IMG (isosceles-triangle
+                 (* SCALER 0.5) 30 "solid" "red"))
+
+(define BG (empty-scene SWIDTH SHEIGHT))
+
+; 
