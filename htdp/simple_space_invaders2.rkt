d12eec7e904a38d619688daad49b1cde3f95ff24
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-31 19:07:28 -0400

    Add data def: tank

--- htdp/simple_space_invaders2.rkt
+++ htdp/simple_space_invaders2.rkt
@@ -20,10 +20,26 @@ to represent the game state.
 
 (define TANK_IMG (rectangle (* SCALER 3) SCALER
                             "solid" "light steel blue"))
+(define TANK_RMSPD 3)
+(define TANK_LMSPD -3)
 
 (define MSL_IMG (isosceles-triangle
                  (* SCALER 0.5) 30 "solid" "red"))
 
 (define BG (empty-scene SWIDTH SHEIGHT))
 
-; 
+; DATA DEFINITIONS /////////////////////////////////////////
+
+(define-struct tank [loc vel])
+; tank: a structure
+; (make-tank Number Number)
+; Interp.: (make-tank xcoord vel)
+; xcoord for the tank's x-coordinate, vel for the tank's
+; velocity
+#;
+(define (fn_for_tank t)
+  (...(tank-loc t) (tank-vel t)))
+
+(define-struct gstate [ufo tank msl])
+; gstate: a structure
+; (make-gstate posn 
