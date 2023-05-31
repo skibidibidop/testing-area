4624ac2b9222e7deb53ccf2cc3efbe8a88c37491
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-05-31 17:55:44 -0400

    Create simple_space_invaders2.rkt w/ description

---
+++ htdp/simple_space_invaders2.rkt
@@ -0,0 +1,10 @@
+#|
+Author: Mark Beltran
+Date: June 1, 2023
+
+Simple space invaders game using a single structure type
+to represent the game state.
+|#
+
+(require 2htdp/image)
+(require 2htdp/universe)
