638379b9f5bdd130c1b80d220377b3184a5c5631
skibidibidop <dev.mapb@gmail.com>
2023-06-02 13:41:19 +0800

    Create cat_cham.rkt with description

---
+++ htdp/cat_cham.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname cat_cham) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: June 2, 2023
+
+Simulates a cat or chameleon pet based on initial input.
+Certain key strokes can change the chosen pet's mood.
+|#
