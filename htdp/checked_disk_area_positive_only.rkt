0d513fd65edb535e411b41f11e0dff898728b844
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-03 21:48:32 -0400

    Update checked_disk_area_positive_only.rkt

--- htdp/checked_disk_area_positive_only.rkt
+++ htdp/checked_disk_area_positive_only.rkt
@@ -4,3 +4,17 @@ Date: June 4, 2023
 
 Compute the area of a disk. For input, allow positive numbers only.
 |#
+
+; Number -> Number
+; Computes area of disk with radius v
+(define (area_of_disk v)
+  (* pi (* v v)))
+
+; Any -> Number
+; computes the area of a disk with radius v,
+; if v is a number
+(define (checked_area_of_disk v)
+  (cond
+    [(and (number? v) (positive? v)) (area_of_disk v)]
+    [else (error "area_of_disk: positive number expected")]))
+    
