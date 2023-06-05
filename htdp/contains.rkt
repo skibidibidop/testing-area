0fafcf55661d565718bc0dd7e86e749e86e9f04d
skibidibidop <dev.mapb@gmail.com>
2023-06-06 07:26:41 +0800

    Create contains.rkt w/ description

---
+++ htdp/contains.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname contains) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: June 6, 2023
+
+contains? function: takes a string and a list of strings as arguments
+and checks if the former is in the latter.
+|#
