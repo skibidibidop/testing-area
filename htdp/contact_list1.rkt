3af3f447d9ec34a1090a45cf6245c21166921064
skibidibidop <dev.mapb@gmail.com>
2023-06-05 18:51:17 +0800

    Add partial function def: has_flatt?

--- htdp/contact_list1.rkt
+++ htdp/contact_list1.rkt
@@ -9,6 +9,8 @@ Design a function that consumes a list of contacts and checking if
 it has "Flatt".
 |#
 
+; DATA DEFINITIONS /////////////////////////////////////////////////////////////
+
 ; name_list is one of:
 ; - '()
 ; - (cons String name_list)
@@ -17,3 +19,16 @@ it has "Flatt".
 ; (define nl2 (cons "Name1" '()))
 ; (define nl3 (cons "Name1"
 ;                   (cons "Name2" '())))
+
+; FUNCTION DEFINITIONS /////////////////////////////////////////////////////////
+
+; name_list -> Boolean
+; determines whether "Flatt" is in a name_list
+(define (has_flatt? nlist) #false)
+(check-expect (has_flatt? '()) #false)
+(check-expect (has_flatt? (cons "Find" '())) #false)
+(check-expect (has_flatt? (cons "Flatt" '())) #true)
+(check-expect (has_flatt? (cons "Find" (cons "Flatt" (cons "Huh" '())))) #true)
+(check-expect (has_flatt? (cons "Find"
+                                (cons "Huh" (cons "A" (cons "ASD" '())))))
+              #false)
