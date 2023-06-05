8fbbc606987cda7a93348b09d997199bb3beaa74
skibidibidop <dev.mapb@gmail.com>
2023-06-05 18:33:02 +0800

    Create contact_list1.rkt w/ description

---
+++ htdp/contact_list1.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname contact_list1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: June 5, 2023
+
+Design a function that consumes a list of contacts and checking if
+it has "Flatt".
+|#
