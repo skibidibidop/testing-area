0b4a067bda166e2652d2a812768474b9d43e87d6
skibidibidop <dev.mapb@gmail.com>
2023-06-06 12:12:06 +0800

    Create checked_sum.rkt w/ description

---
+++ htdp/checked_sum.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname checked_sum) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: June 6, 2023
+
+Design function pos? to check if all numbers in a list are positive.
+Design checked_sum which signals an error if a number in a provided list is not
+positive. If all numbers in the list are positive, it provides the sum.
+|#
