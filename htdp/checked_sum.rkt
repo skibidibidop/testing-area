0ce2b27cc3b0bc2b00ed4c7b4df454451bfe981e
skibidibidop <dev.mapb@gmail.com>
2023-06-08 14:31:06 +0800

    Add (checked_sum)

--- htdp/checked_sum.rkt
+++ htdp/checked_sum.rkt
@@ -26,3 +26,18 @@ positive. If all numbers in the list are positive, it provides the sum.
 
 (define (pos? num)
   (> num 0))
+
+(define ERR_NOT_POSITIVE "There is a non-positive in the list.")
+
+; num_list -> Number
+; Provides sum if list only has positive numbers. Otherwise, signals an error.
+(check-expect (checked_sum (cons 3 (cons 2 '()))) 5)
+(check-error (checked_sum (cons 0 (cons 1 '()))) ERR_NOT_POSITIVE)
+(check-expect (checked_sum '()) 0)
+(check-error (checked_sum (cons -4 (cons 3 '()))) ERR_NOT_POSITIVE)
+
+(define (checked_sum nlist)
+  (cond [(empty? nlist) 0]
+        [(pos? (first nlist))
+         (+ (first nlist) (checked_sum (rest nlist)))]
+        [else (error ERR_NOT_POSITIVE)]))
