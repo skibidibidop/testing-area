3eb9f141746c9a4c2f98037d95cbea943851ca24
skibidibidop <dev.mapb@gmail.com>
2023-06-09 14:11:15 +0800

    Create check_sorted_list.rkt

---
+++ htdp/check_sorted_list.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname check_sorted_list) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: June 9, 2023
+
+Check if a non-empty list is sorted in descending order.
+|#
