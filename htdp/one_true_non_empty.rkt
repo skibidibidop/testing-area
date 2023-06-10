f0a098587f2d8c12865bf172031a95a1e37a0a07
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-10 18:54:30 -0400

    Add data def: nempty_bool_list

--- htdp/one_true_non_empty.rkt
+++ htdp/one_true_non_empty.rkt
@@ -4,3 +4,17 @@ Date: June 10, 2023
 
 Returns #true if at least one member of the non-empty list is #true.
 |#
+
+; DATA DEFINITIONS /////////////////////////////////////////////////////////////
+
+; a nempty_bool_list is one of:
+; - (cons Boolean '())
+; - (cons Boolean nempty_bool_list)
+; Interp.: a non-empty list of Boolean values
+; (define nblist1 (cons #true '()))
+; (define nblist2 (cons #false (cons #true '())))
+
+; FUNCTION DEFINITIONS /////////////////////////////////////////////////////////
+
+;
+(define (one_true bool_nlist) #true)
