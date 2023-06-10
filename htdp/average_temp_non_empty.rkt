44fd017c9355c9c19c9ec292ec53bb0f5b044b59
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-10 18:29:42 -0400

    Add completed (how_many)

--- htdp/average_temp_non_empty.rkt
+++ htdp/average_temp_non_empty.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname average_temp_non_empty) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 #|
 Author: Mark Beltran
 Date: June 10, 2023
@@ -24,7 +21,12 @@ Computes the average of all Celsius temperatures in a non-empty list.
 
 ; non_empty_list -> Number
 ; Returns count of list members
-(define (how_many nlist) 1)
+(check-expect (how_many (cons 3 (cons 2 '()))) 2)
+(check-expect (how_many (cons 5 '())) 1)
+
+(define (how_many nlist)
+  (cond[(empty? (rest nlist)) 1]
+       [else (+ (how_many (rest nlist)) 1)]))
 
 ; non_empty_list -> Number
 ; Returns sum of list members
