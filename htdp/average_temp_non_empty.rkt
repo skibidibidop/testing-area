2c1b64bf334116f431bbd3fc1c2f36846dc61e59
skibidibidop <dev.mapb@gmail.com>
2023-06-10 15:37:52 +0800

    Create average_temp_non_empty.rkt w/ desc

---
+++ htdp/average_temp_non_empty.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname average_temp_non_empty) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: June 10, 2023
+
+Computes the average of all Celsius temperatures in a non-empty list.
+|#
