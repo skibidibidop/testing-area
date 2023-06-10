45da8d0b70a1cebb274adcfb7edf163483697cc7
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-10 18:44:50 -0400

    Add data def: nempty_bool_list, wishlist: (all_true)

--- htdp/all_true_non_empty.rkt
+++ htdp/all_true_non_empty.rkt
@@ -5,3 +5,15 @@ Date: June 10, 2023
 Returns #false if a non-empty list of Boolean values has a member that
 is #false. Otherwise, return #true.
 |#
+
+; DATA DEFINITIONS /////////////////////////////////////////////////////////////
+
+; a nempty_bool_list is one of:
+; - (cons Boolean '())
+; - (cons Boolean nonempty_list)
+; Interp.: a non-empty list of Boolean values
+
+; FUNCTION DEFINITIONS /////////////////////////////////////////////////////////
+
+; nempty_bool_list -> Boolean
+(define (all_true bool_nlist) #true)
