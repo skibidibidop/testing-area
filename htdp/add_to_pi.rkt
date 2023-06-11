088404fa35aead73eb04919b82e77da81d867596
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-10 20:26:45 -0400

    Change program description

--- htdp/add_to_pi.rkt
+++ htdp/add_to_pi.rkt
@@ -2,7 +2,7 @@
 Author: Mark Beltran
 Date: June 10, 2023
 
-Adds a natural number to pi without using the + primitive operator.
+See (nat_num) function purpose.
 |#
 
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
@@ -15,3 +15,9 @@ Adds a natural number to pi without using the + primitive operator.
 ; (define (nat2 5000))
 
 ; FUNCTION DEFINITION //////////////////////////////////////////////////////////
+
+; nat_num -> Number
+; Adds nat_num to pi without using the + primtitive operator.
+(check-expect
+(define (add_to_pi nat)
+  (con
