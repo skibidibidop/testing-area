ee9811c6ad83b72543b573ff0187f31b1d403963
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-10 22:34:09 -0400

    Add completed (doll_count)

--- htdp/russian_dolls.rkt
+++ htdp/russian_dolls.rkt
@@ -14,5 +14,13 @@ Code along - Russian Doll example.
 ; Interp.: represents nested russian dolls, with color being the color of the
 ; outer doll and doll being the color of the inner doll
 
-
 ; FUNCTION DEFINITION //////////////////////////////////////////////////////////
+
+; russ_doll -> Number
+; Counts nested dolls
+(check-expect (doll_count (make-layer "yellow" (make-layer "green" "red"))) 3)
+
+(define (doll_count nested_dolls)
+  (cond[(string? nested_dolls) 1]
+       [else
+         (+ (doll_count (layer-doll nested_dolls)) 1)]))
