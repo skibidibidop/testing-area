bfe6f1ad8d6c861a1dfb40a8259a1d15e7c209a3
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-10 20:24:50 -0400

    Add data def: nat_num

--- htdp/add_to_pi.rkt
+++ htdp/add_to_pi.rkt
@@ -4,3 +4,14 @@ Date: June 10, 2023
 
 Adds a natural number to pi without using the + primitive operator.
 |#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; a nat_num is one of:
+; - 0
+; (add1 nat_num)
+; Interp.: a natural/counting number
+; (define (nat1 0))
+; (define (nat2 5000))
+
+; FUNCTION DEFINITION //////////////////////////////////////////////////////////
