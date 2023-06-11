5ca9d774034cd39500f6a4bb01aaed4cbb463a03
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-10 20:45:14 -0400

    Add data def: nat_num, (multiply) header

--- htdp/multiply.rkt
+++ htdp/multiply.rkt
@@ -7,4 +7,21 @@ See (multiply) function purpose.
 
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
+; a nat_num is one of:
+; 0
+; (add1 nat_num)
+; Interp.: a natural number
+; (define nat1 0)
+; (define nat2 10000)
+
 ; FUNCTION DEFINITION //////////////////////////////////////////////////////////
+
+; nat_num Number -> Number
+; Multiplies nat_num with Number without using the primitive operator *.
+(check-expect (multiply 3 4.5) 13.5)
+(check-expect (multiply 0 12) 0)
+(check-expect (multiply 5 -23) -115)
+
+(define (multiply nat)
+  (cond[zero? 0]
+       [else (
