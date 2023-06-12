bede052316fc5714bf012ac3b4030d2c641b9358
skibidibidop <dev.mapb@gmail.com>
2023-06-12 09:17:07 +0800

    Create shots_fired.rkt

---
+++ htdp/shots_fired.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname shots_fired) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: June 12, 2023
+
+Simulates firing shots. Shots are added to the bottom of the scene each time the
+space bar is pressed. Shots rise vertically at 1 px / tick.
+|#
