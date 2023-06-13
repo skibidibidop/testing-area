4fd37c69f61b0e67d2733a32288c765b5d895e83
skibidibidop <dev.mapb@gmail.com>
2023-06-13 09:50:17 +0800

    Add data def: posn_list

--- htdp/translate.rkt
+++ htdp/translate.rkt
@@ -8,3 +8,16 @@ Date: June 13, 2023
 Input: posn list
 Output: A posn list with each member translated to (make-posn x (+ y 1))
 |#
+
+; DATA DEFINITIONS /////////////////////////////////////////////////////////////
+
+; a posn_list is one of:
+; - '()
+; (cons posn posn_list)
+; Interp.: a list of posns
+; (define pl1 '())
+; (define pl2 (cons (make-posn 1 2) '()))
+; (define pl3 (cons (make-posn 3 4)
+;                   (cons (make-posn 5 6) '())))
+
+; FUNCTION DEFINITIONS /////////////////////////////////////////////////////////
