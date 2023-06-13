0d0a668615f177a3e3c90a89daf715bedc63ffd8
skibidibidop <dev.mapb@gmail.com>
2023-06-13 10:06:45 +0800

    Create legal.rkt w/ description

---
+++ htdp/legal.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname legal) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: June 13, 2023
+
+Input: posn list
+Output: posns with x-coordinates between 0-100 and y-coordinates between 0-200.
+|#
