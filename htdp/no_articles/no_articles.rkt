597078d06a5424432ece4ae6979ac00f3e220e42
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-15 05:46:23 +0800

    Add data def: string_list

--- htdp/no_articles/no_articles.rkt
+++ htdp/no_articles/no_articles.rkt
@@ -7,3 +7,16 @@ Removes all articles ("a", "an", "the") from a text file.
 Input: <file name>.txt
 Output: no_articles_<file name>.txt
 |#
+
+; DATA DEFINITIONS /////////////////////////////////////////////////////////////
+
+; a string_list is one of:
+; - '()
+; - (cons String string_list)
+; Interp.: a list of strings which represents a line in the file
+(define sl1 '())
+(define sl2 (cons "sla" '()))
+(define sl3 (cons "sla"
+                  (cons "slb" '())))
+
+; FUNCTION DEFINITIONS /////////////////////////////////////////////////////////
