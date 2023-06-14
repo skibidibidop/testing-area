35ac0750fe537e40d55e6f41422c51077f11ff2b
skibidibidop <dev.mapb@gmail.com>
2023-06-14 13:49:28 +0800

    Remove wishlist

--- htdp/list_of_list_of_words/list_of_list_of_words.rkt
+++ htdp/list_of_list_of_words/list_of_list_of_words.rkt
@@ -30,34 +30,3 @@ Code-along 10.3 Lists in lists, files
 ; (define lsl3 (cons
 ;               (cons "String1"
 ;                     (cons "String2" '())) '()))
-
-; FUNCTION DEFINITIONS /////////////////////////////////////////////////////////
-
-; String -> String
-; Produces file content as a string.
-(check-expect (read_file "ttt.txt")
-              (+ "TTT\n"
-                 "\nPut up in a place\n"
-                 "where it's easy to see\n"
-                 "the cryptic admonishment\n"
-                 "T.T.T.\n"
-                 "\nWhen you feel how depressingly\n"
-                 "slowly you climb,\n"
-                 "it's well to remember that\n"
-                 "Thing Take Time.\n"
-                 "\nPiet Hein"))
-
-(define (read_file f) ...)
-
-; String -> string_list
-; Produces file content as a list of strings, one per line
-(define (read_lines f) ...)
-
-; String -> string_list
-; Produces file content as a list of strings, one per word
-(define (read_words f) ...)
-
-; String -> list_of_string_lists
-; Produces file content as list of string_lists, one list per line and
-; one string per word
-(define (read_words_lines f) ...)
