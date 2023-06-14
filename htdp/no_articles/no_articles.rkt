4e629ce9cac075f8b284d903951c0ffc8a704991
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-15 05:50:32 +0800

    Add data def: lsl

--- htdp/no_articles/no_articles.rkt
+++ htdp/no_articles/no_articles.rkt
@@ -10,7 +10,7 @@ Output: no_articles_<file name>.txt
 
 ; DATA DEFINITIONS /////////////////////////////////////////////////////////////
 
-; a string_list is one of:
+; A string_list is one of:
 ; - '()
 ; - (cons String string_list)
 ; Interp.: a list of strings which represents a line in the file
@@ -18,5 +18,28 @@ Output: no_articles_<file name>.txt
 (define sl2 (cons "sla" '()))
 (define sl3 (cons "sla"
                   (cons "slb" '())))
+
+; A lsl (short for list of string_lists) is one of:
+; - '()
+; (cons string_list lsl)
+; Interp.: a list of string_lists, represents how the lines are organized
+; in the file
+(define lsl1 '())
+(define lsl2 (cons (cons "sla" '()) '()))
+(define lsl3 (cons
+              (cons "sla" '())
+              (cons
+               (cons "slb" '())
+               '())))
+(define lsl4 (cons
+              (cons "sla"
+                    (cons "slb"
+                          (cons "slc" '())))
+              (cons
+               (cons "sld"
+                     (cons "sle" '()))
+               (cons
+                (cons "slf" '())
+                '()))))
 
 ; FUNCTION DEFINITIONS /////////////////////////////////////////////////////////
