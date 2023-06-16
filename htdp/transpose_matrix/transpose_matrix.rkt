a05cced6c1241607957cc8498d7631771d77a4a5
skibidibidop <dev.mapb@gmail.com>
2023-06-16 13:31:28 +0800

    Create transpose_matrix.rkt with desc

---
+++ htdp/transpose_matrix/transpose_matrix.rkt
@@ -0,0 +1,22 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname transpose_matrix) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: June 16, 2023
+
+Transposes a matrix.
+|#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; A matrix is one of:
+; (cons row '())
+; (cons row matrix)
+; Constraint: all rows in matrix are of the same length
+
+; A row is one of:
+; - '()
+; - (cons Number row)
+
+; FUNCTIONS & RELATED //////////////////////////////////////////////////////////
