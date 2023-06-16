f5df5eaef36863ef63eb8dbbcf61b3f902a2e769
skibidibidop <dev.mapb@gmail.com>
2023-06-16 14:20:39 +0800

    Capitalize data defs

--- htdp/transpose_matrix/transpose_matrix.rkt
+++ htdp/transpose_matrix/transpose_matrix.rkt
@@ -10,28 +10,28 @@ Transposes a matrix.
 
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
-; A row is one of:
+; A Row is one of:
 ; - '()
-; - (cons Number row)
+; - (cons Number Row)
 ; Interp.: a row in a matrix
 (define row1 (cons 11 (cons 12 '())))
 (define row2 (cons 21 (cons 22 '())))
 
-; A matrix is one of:
-; (cons row '())
-; (cons row matrix)
+; A Matrix is one of:
+; (cons Row '())
+; (cons Row Matrix)
 ; Constraint: all rows in matrix are of the same length
 (define mat1 (cons row1 (cons row2 '())))
 
-; A num_list is one of:
+; A Num_list is one of:
 ; - '()
-; (cons Number num_list)
+; (cons Number Num_list)
 ; Interp.: a list of Numbers
 (define nl1 (cons 1 (cons 2 '())))
 
-; An lnl (short for list_of_num_lists) is one of:
+; An Lnl (short for list_of_num_lists) is one of:
 ; '()
-; (cons num_list lnl)
+; (cons Num_list Lnl)
 ; Interp.: a list of num_lists
 (define lnl1 (cons
               (cons 1 (cons 2 '()))
@@ -41,7 +41,7 @@ Transposes a matrix.
 
 ; FUNCTIONS & RELATED //////////////////////////////////////////////////////////
 
-; matrix -> matrix
+; Matrix -> Matrix
 ; Transposes the given matrix along the diagonal
 
 (define wor1 (cons 11 (cons 21 '())))
@@ -55,3 +55,8 @@ Transposes a matrix.
     [(empty? (first mat)) '()]
     [else
      (cons (first* mat) (transpose (rest* mat)))]))
+
+; Matrix -> Num_list
+; Takes the first members of all num_lists in a matrix (aka column 1)
+; and creates a new Row
+(define (first* mat) '()
