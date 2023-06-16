260e75079d9b6d6596654f3ba924a46b86a032db
skibidibidop <dev.mapb@gmail.com>
2023-06-16 15:22:13 +0800

    Create editor2.rkt

---
+++ htdp/editor2/editor2.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname editor2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: June 16, 2023
+
+Code-along: 10.4 Graphical Editor, Revisited
+|#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
