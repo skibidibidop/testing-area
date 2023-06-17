368e892ed97b8b45a975385778e6109d7852f876
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-17 19:18:39 -0400

    Update list_practice.rkt

--- htdp/list_practice.rkt
+++ htdp/list_practice.rkt
@@ -4,3 +4,18 @@ Date: June 18, 2023
 
 Practice lists
 |#
+
+; #1
+(list "a" "b" "c" "d")
+; #2
+(list (list 1 2))
+; #3
+(list "a"
+      (list 1)
+      (list #false))
+; #4
+(list (list "a" 2)
+      (list "hello"))
+; #5
+(list (list 1 2)
+      (list 2))
