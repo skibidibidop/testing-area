80a0133965fc3c42c1175a73b808f147447e2762
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-17 19:22:36 -0400

    Update cons_to_list.rkt

--- htdp/cons_to_list.rkt
+++ htdp/cons_to_list.rkt
@@ -5,3 +5,16 @@ Date: June 18, 2023
 (cons) to (list) practice
 |#
 
+; (list 0 1 2 3 4 5)
+(cons 0 (cons 1 (cons 2 (cons 3 (cons 4 (cons 5 '()))))))
+
+; (list (list "he" 0) (list "it" 1) (list "lui" 14))
+(cons
+ (cons "he" (cons 0 '()))
+ (cons "it" (cons 1 '()))
+ (cons "lui" (cons 14 '())))
+
+; (list 1 (list 1 2) (list 1 2 3)))
+(cons 1
+      (cons 1 (cons 2 '()))
+      (cons 1 (cons 2 (cons 3 '()))))
