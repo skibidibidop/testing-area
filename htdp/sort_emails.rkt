55ff951291d9aeb11c479ac844435e4859dda0bb
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-18 18:55:11 -0400

    Add data def: e_mail

--- htdp/sort_emails.rkt
+++ htdp/sort_emails.rkt
@@ -4,3 +4,12 @@ Date: June 19, 2023
 
 Sorts emails by date (no. of seconds since beginning of time).
 |#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+(define-struct e_mail [from date message])
+; (make-e_mail String Number String)
+; Interp.: (make-email f d m)
+; Represents text m sent by f, d seconds after the beginning of time
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
