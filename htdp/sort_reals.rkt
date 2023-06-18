25f46e238ce812909aa882a5f8c131fbf381f4bf
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-17 20:50:58 -0400

    Add data def: num_list

--- htdp/sort_reals.rkt
+++ htdp/sort_reals.rkt
@@ -4,3 +4,15 @@ Date: June 18, 2023
 
 Code-along: 11.3 sample problem (sort reals)
 |#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; A num_list is one of:
+; - '()
+; - (cons Number num_list)
+; Interp.: A list of numbers
+; (define nl1 '())
+; (define nl2 (cons 1 '()))
+; (define nl3 (cons 1 (cons 2 '())))
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
