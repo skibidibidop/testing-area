e6dc0ad92d1c6278de44d3f1d2ee8178063c770f
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-18 19:45:46 -0400

    Add data def: email_list

--- htdp/sort_emails.rkt
+++ htdp/sort_emails.rkt
@@ -7,10 +7,18 @@ Sorts emails by date (no. of seconds since beginning of time) or name.
 
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
+; An Email_list is one of:
+; - '()
+; - (cons e_mail email_list)
+; Interp.: A list of e_mail structures
+; (define eml1 '())
+; (define eml2
+;   (list (make-e_mail "Name1" 500 "Hi!")
+;         (make-e_mail "Name2" 40 "Hello!")))
+
 (define-struct e_mail [from date message])
 ; (make-e_mail String Number String)
 ; Interp.: (make-email f d m)
 ; Represents text m sent by f, d seconds after the beginning of time
 
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
-
