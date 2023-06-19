7016a5186ab2bd549448fd57479f91c4870d0622
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-18 23:28:39 -0400

    Add image constants, (render) function header

--- htdp/polygon.rkt
+++ htdp/polygon.rkt
@@ -5,6 +5,12 @@ Date: June 19, 2023
 Code-along: 11.4 Polygon sample problem
 |#
 
+(define (SCALER 10))
+(define SWIDTH (* SCALER 5))
+(define SHEIGHT (* SCALER 5))
+
+(define BG (empty-scene SWIDTH SHEIGHT))
+
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
 ; A Polygon is one of:
@@ -13,3 +19,8 @@ Code-along: 11.4 Polygon sample problem
 ; Interp.: a non-empty list of at least 3 Posns representing points of a polygon
 
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; Image Polygon -> Image
+; Renders polygon p into image img
+(define (render img p)
+  img)
