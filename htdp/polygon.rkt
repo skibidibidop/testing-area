f3634da859bd005a7d234a63787a9d18e381a676
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-18 23:25:25 -0400

    Add data def: polygon

--- htdp/polygon.rkt
+++ htdp/polygon.rkt
@@ -4,3 +4,12 @@ Date: June 19, 2023
 
 Code-along: 11.4 Polygon sample problem
 |#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; A Polygon is one of:
+; - (cons list Posn Posn Posn)
+; - (cons Posn Polygon)
+; Interp.: a non-empty list of at least 3 Posns representing points of a polygon
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
