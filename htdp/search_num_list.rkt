0c1628f5456092674e00a274a92d246ecfa65f56
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-18 22:31:26 -0400

    Add data def: sorted_numlist

--- htdp/search_num_list.rkt
+++ htdp/search_num_list.rkt
@@ -7,4 +7,12 @@ Determines if a number occurs in a sorted list.
 
 ; DATA DEFINITIONS /////////////////////////////////////////////////////////////
 
+; a sorted_numlist is one of:
+; - '()
+; - (cons Number num_list)
+; Interp.: a list of number sorted in ascending order
+; (define nl1 '())
+; (define nl2 (list 1 2 3 4))
+; (define nl3 (list -4 -3 -2 -1 0))
+
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
