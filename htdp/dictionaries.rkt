2fc1b97e93dbfb4e3e99cdd3538effabf360b953
skibidibidop <dev.mapb@gmail.com>
2023-06-20 11:00:06 +0800

    Create dictionaries.rkt w/ description

---
+++ htdp/dictionaries.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname dictionaries) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: June 20, 2023
+
+Project
+Real-World Data: Dictionaries
+|#
