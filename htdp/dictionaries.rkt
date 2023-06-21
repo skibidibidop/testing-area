13c55d4e2c850ca042adf8878766170e5ac8502e
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-21 08:27:41 +0800

    Remove (get_firsts), (lcl_maker), (count_by_letter)

--- htdp/dictionaries.rkt
+++ htdp/dictionaries.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname dictionaries) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 #|
 Author: Mark Beltran
 Date: June 20, 2023
@@ -12,7 +9,6 @@ Dictionary location: /usr/share/dict/words
 |#
 
 (require 2htdp/batch-io)
-
 
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
@@ -61,58 +57,3 @@ Dictionary location: /usr/share/dict/words
      (if (string=? (first (explode (first dict))) ltr)
          (+ (starts_with# ltr (rest dict)) 1)
          (starts_with# ltr (rest dict)))]))
-
-; Dictionary -> Lcl
-; Counts the number of times each Letter is used as the first character of a
-; word in a Dictionary.
-(define SAMPLE_DICT1 (list "aa" "ba" "bb" "ca" "cb" "cc"))
-(define SAMPLE_DICT2 (list "aa" "ab" "ac" "ba" "ca" "cc" "cd" "ce"))
-
-(check-expect (count_by_letter '()) '())
-(check-expect (count_by_letter SAMPLE_DICT1)
-              (list
-               (make-ltr_counts "a" 1)
-               (make-ltr_counts "b" 2)
-               (make-ltr_counts "c" 3)))
-(check-expect (count_by_letter SAMPLE_DICT2)
-              (list
-               (make-ltr_counts "a" 3)
-               (make-ltr_counts "b" 1)
-               (make-ltr_counts "c" 4)))
-
-(define (count_by_letter dict)
-  (cond
-    [(empty? dict) '()]
-    [else (lcl_maker (get_firsts dict) dict)]))
-
-; Dictionary -> Ltr_list
-; Creates a list (set) of all first letters in provided Dictionary
-(check-expect (get_firsts '()) '())
-(check-expect (get_firsts SAMPLE_DICT1) (list "a" "b" "c"))
-(check-expect (get_firsts SAMPLE_DICT2) (list "a" "b" "c"))
-
-(define (get_firsts dict)
-  (cond
-    [(empty? dict) '()]
-    [else
-     (if (not (string=? (first dict)
-                        (first (rest dict))))
-         (cons (first dict))
-         (cons (get_firsts (rest dict))))]))
-
-; Ltr_list Dictionary -> lcl
-; Creates a list of Ltr_counts based on provided Dictionary
-(check-expect (lcl_maker
-               (list "a" "b" "c") SAMPLE_DICT1)
-              (list
-               (make-ltr_counts "a" 1)
-               (make-ltr_counts "b" 2)
-               (make-ltr_counts "c" 3)))
-
-(define (lcl_maker llist dict)
-  (cond
-    [(empty? llist) '()]
-    [else
-     (cons (make-ltr_counts (first llist)
-                            (starts_with# (first llist) dict))
-           (lcl_maker (rest llist) dict))]))
