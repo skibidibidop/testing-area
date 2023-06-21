06ca00441be83d47e6674d52e9e32310caef4631
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-06-21 11:03:50 +0800

    Restore (count_by_letter), (lcl_maker), modify (count_by_letter) test

--- htdp/dictionaries.rkt
+++ htdp/dictionaries.rkt
@@ -10,6 +10,7 @@ Dictionary location: /usr/share/dict/words
 
 (require 2htdp/batch-io)
 
+
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
 (define LOCATION "/usr/share/dict/words")
@@ -57,3 +58,60 @@ Dictionary location: /usr/share/dict/words
      (if (string=? (first (explode (first dict))) ltr)
          (+ (starts_with# ltr (rest dict)) 1)
          (starts_with# ltr (rest dict)))]))
+
+; Dictionary -> Lcl
+; Counts the number of times each Letter is used as the first character of a
+; word in a Dictionary.
+(define SAMPLE_DICT1 (list "aa" "ba" "bb" "ca" "cb" "cc"))
+
+(check-expect (count_by_letter '()) '())
+(check-expect (count_by_letter SAMPLE_DICT1)
+              (list
+               (make-ltr_counts "a" 1)
+               (make-ltr_counts "b" 2)
+               (make-ltr_counts "c" 3)
+               (make-ltr_counts "d" 0)
+               (make-ltr_counts "e" 0)
+               (make-ltr_counts "f" 0)
+               (make-ltr_counts "g" 0)
+               (make-ltr_counts "h" 0)
+               (make-ltr_counts "i" 0)
+               (make-ltr_counts "j" 0)
+               (make-ltr_counts "k" 0)
+               (make-ltr_counts "l" 0)
+               (make-ltr_counts "m" 0)
+               (make-ltr_counts "n" 0)
+               (make-ltr_counts "o" 0)
+               (make-ltr_counts "p" 0)
+               (make-ltr_counts "q" 0)
+               (make-ltr_counts "r" 0)
+               (make-ltr_counts "s" 0)
+               (make-ltr_counts "t" 0)
+               (make-ltr_counts "u" 0)
+               (make-ltr_counts "v" 0)
+               (make-ltr_counts "w" 0)
+               (make-ltr_counts "x" 0)
+               (make-ltr_counts "y" 0)
+               (make-ltr_counts "z" 0)))
+               
+(define (count_by_letter dict)
+  (cond
+    [(empty? dict) '()]
+    [else (lcl_maker LETTERS dict)]))
+
+; Ltr_list Dictionary -> lcl
+; Creates a list of Ltr_counts based on provided Dictionary
+(check-expect (lcl_maker
+               (list "a" "b" "c") SAMPLE_DICT1)
+              (list
+               (make-ltr_counts "a" 1)
+               (make-ltr_counts "b" 2)
+               (make-ltr_counts "c" 3)))
+
+(define (lcl_maker llist dict)
+  (cond
+    [(empty? llist) '()]
+    [else
+     (cons (make-ltr_counts (first llist)
+                            (starts_with# (first llist) dict))
+           (lcl_maker (rest llist) dict))]))
