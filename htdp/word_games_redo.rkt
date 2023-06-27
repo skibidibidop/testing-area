af6d5cc6d28fd7b6bfde8f3678c76e0e7866fd78
skibidibidop <dev.mapb@gmail.com>
2023-06-27 16:16:43 +0800

    Create word_games_redo.rkt

---
+++ htdp/word_games_redo.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname word_games_redo) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: June 27, 2023
+
+Redoing the Word Games project.
+|#
