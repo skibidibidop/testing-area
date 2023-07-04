2c7d32be1c65abf7254d3ff1c2acdad61e3a4f06
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-05 05:56:49 +0800

    Add image constants

--- htdp/simple_tetris.rkt
+++ htdp/simple_tetris.rkt
@@ -8,6 +8,17 @@ Project: Simple Tetris
 (require 2htdp/image)
 (require 2htdp/universe)
 
+(define SCALER 10)
+
+(define SCENE_WIDTH (* SCALER 10))
+(define SCENE_HEIGHT (* SCALER 10))
+
+(define BLOCK_SIZE SCALER)
+
+(define BLOCK
+  (overlay (square (- BLOCK_SIZE 1) "solid" "red")
+           (square BLOCK_SIZE "outline" "black")))
+
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
