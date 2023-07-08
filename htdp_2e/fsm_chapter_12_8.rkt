d50d7bdb07e45088da6d855ea6142d42b669c3c2
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-08 09:26:50 +0800

    Add data def: FSM, Transition, FSM_state

--- htdp_2e/fsm_chapter_12_8.rkt
+++ htdp_2e/fsm_chapter_12_8.rkt
@@ -3,8 +3,24 @@ Author: Mark Beltran
 Date: July 8, 2023
 
 Chapter 12.8 Finite State Machines
+
+Outline:
+
 |#
 
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; An FSM is one of:
+; - '()
+; - (cons Transition FSM)
+; Interp.: represents the transitions that a finite state machine can take
+; from one state to another in reaction to keystrokes
+
+(define-struct transition [current next])
+; (make-transition FSM_state FSM_state)
+; Interp.: (make-transition "green" "yellow"), represents the current state
+; ("green") and the state to transition to ("yellow") in reaction to keystrokes
+
+; An FSM_state is a Color
 
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
