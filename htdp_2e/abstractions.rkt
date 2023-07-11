34144743e96b458c739f526c480f08fbd55ccf01
skibidibidop <dev.mapb@gmail.com>
2023-07-11 09:21:56 +0800

    Create abstractions.rkt

---
+++ htdp_2e/abstractions.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname abstractions) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: July 11, 2023
+
+Finger exercises for III Abstractions
+|#
+
+; Exercise 235: Use the contains? function to define functions that search for
+; "atom" "basic", and "zoo", respectively.
