a8a802fab1fd161b5f87203f7ae8636a82d69e69
skibidibidop <dev.mapb@gmail.com>
2023-07-11 10:33:55 +0800

    Create exercise_238.rkt

---
+++ htdp_2e/exercise_238.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname exercise_238) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: July 11, 2023
+
+Exercise 238
+Abstract the given functions into a single function. Both consume non-empty
+lists of numbers (Nelon) and produce a single number.
+Define inf-1 and sup-1 in terms of the abstract function. Test with the given
+lists.
+|#
+
+(define given_list1
+  (list 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1))
+(define given_list2
+  (list 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25))
