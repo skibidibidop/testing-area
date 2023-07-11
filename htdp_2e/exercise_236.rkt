64c4a3d46b4598d306cfb80a4b69a09dd229c0a4
skibidibidop <dev.mapb@gmail.com>
2023-07-11 09:41:34 +0800

    Add data def: Num_list

---
+++ htdp_2e/exercise_236.rkt
@@ -0,0 +1,20 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname exercise_236) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: July 11, 2023
+
+Exercise 236
+Create test suites for the given functions
+|#
+
+; DATA DEFINITIONS /////////////////////////////////////////////////////////////
+
+; A Num_list is one of:
+; '()
+; (cons Number Num_list)
+; Interp.: a list of Numbers
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
