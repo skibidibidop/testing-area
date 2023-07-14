6aa4e247e15a4102bc1e0549b854326252023256
skibidibidop <dev.mapb@gmail.com>
2023-07-14 15:54:23 +0800

    Add functions from Figure 94

--- htdp_2e/exercise_252.rkt
+++ htdp_2e/exercise_252.rkt
@@ -8,6 +8,43 @@ Date: July 13, 2023
 Exercise 252
 |#
 
+(require 2htdp/image)
+
+(define EMT
+  (empty-scene 100 100))
+(define DOT
+  (circle 3 "solid" "red"))
+
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
+; A [List_of ITEM] is one of:
+; - '()
+; - (cons ITEM [List_of ITEM])
+
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; [List_of Number] -> Number
+(define (product l)
+  (cond
+    [(empty? l) 1]
+    [else
+     (* (first l)
+        (product (rest l)))]))
+
+; [List_of Posn] -> Image
+(define (image* l)
+ (cond
+   [(empty? l) EMT]
+   [else
+    (place_dot
+     (first l)
+     (image* (rest l)))]))
+
+; Posn Image -> Image
+(define (place_dot p img)
+  (place-image
+   DOT
+   (posn-x p) (pons-y p)
+   img))
+
+; 
