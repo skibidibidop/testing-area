3974aee373cbcf4264fd9cf0dc0edc8bb11b70ad
skibidibidop <dev.mapb@gmail.com>
2023-07-18 16:35:28 +0800

    Add Ex. 10 dir, file

---
+++ htdp_2e_redo/ex_10/ex_10.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex_10) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: July 18, 2023
+
+Exercise 10
+|#
+
+; Relax, eat, sleep, then onto the next chapter.
