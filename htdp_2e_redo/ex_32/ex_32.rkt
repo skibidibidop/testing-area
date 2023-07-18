e1f1af46da98f11cc3a2ede3072cfb9c38aa6a1e
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-19 07:56:37 +0800

    Update ex_32.rkt

---
+++ htdp_2e_redo/ex_32/ex_32.rkt
@@ -0,0 +1,15 @@
+#|
+Author: Mark Beltran
+Date: July 19, 2023
+
+Exercise 32.
+
+Most people no longer use desktop computers just to run
+applications but also employ cell phones, tablets, and their cars’ information
+control screen. Soon people will use wearable computers in the form of
+intelligent glasses, clothes, and sports gear. In the somewhat more distant
+future, people may come with built-in bio computers that directly interact
+with body functions. Think of ten different forms of events that software
+applications on such computers will have to deal with.
+|#
+
