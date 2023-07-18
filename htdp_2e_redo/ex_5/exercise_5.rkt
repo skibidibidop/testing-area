a0b24f384be0c8fca3a5e2ea43111eb05f103a6d
skibidibidop <dev.mapb@gmail.com>
2023-07-18 14:06:33 +0800

    Add Exercise 5 dir, exercise_5.rkt

---
+++ htdp_2e_redo/ex_5/exercise_5.rkt
@@ -0,0 +1,27 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise_5) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: July 18, 2023
+
+Exercise 5.
+|#
+
+(require 2htdp/image)
+
+(define SCALER 10)
+
+(define SWIDTH (* SCALER 15))
+(define SHEIGHT (* SCALER 10))
+
+(define LEFT_SIDE
+  (triangle/sas SCALER
+                90
+                SCALER
+                "solid" "brown"))
+(define RIGHT_SIDE
+  (triangle/ass 90
+                SCALER
+                SCALER
+                "solid" "brown"))
