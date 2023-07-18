d6c206dab8101fbcdbec37e15023310ba893fec0
skibidibidop <dev.mapb@gmail.com>
2023-07-18 13:42:50 +0800

    Add Exercise 4 dir, solution

---
+++ htdp_2e_redo/ex_4/exercise_4.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise_4) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: July 18, 2023
+
+Exercise 4.
+|#
+
+(define str "helloworld")
+(define i 5)
+
+(define START 0)
+
+(string-append
+ (substring str START i)
+ (substring str (+ i 1)))
