4ce9d265f2e492402698b98c1882d837bcc6d8c3
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-19 08:08:07 +0800

    Update ex_32.rkt

--- htdp_2e_redo/ex_32/ex_32.rkt
+++ htdp_2e_redo/ex_32/ex_32.rkt
@@ -13,3 +13,13 @@ with body functions. Think of ten different forms of events that software
 applications on such computers will have to deal with.
 |#
 
+; 1. Cortisol/Adrenaline/Stress levels
+; 2. Dryness of eyes, vibrates to remind person to blink
+; 3. Hydration level
+; 4. Reminders to stretch/move if sitting/standing for too long
+; 5. Fiber level, provides constipation warning
+; 6. Posture monitoring
+; 7. Infectious disease detector
+; 8. Abnormal brain activity monitor
+; 9. Malignant growth detector
+; 10. Stimulant monitor, notifies if too much stimulant intake
