eabd48729c50c3575cf2bd3cfecfa743434f3d01
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-20 06:26:19 +0800

    Update ex_53.rkt

---
+++ htdp_2e_redo/ex_53/ex_53.rkt
@@ -0,0 +1,13 @@
+#|
+Author: Mark Beltran
+Date: July 20, 2023
+
+Exercise 53. The design recipe for world programs demands that you
+translate information into data and vice versa to ensure a complete
+understanding of the data definition. It’s best to draw some world scenarios and
+to represent them with data and, conversely, to pick some data examples
+and to draw pictures that match them. Do so for the LR definition, including
+at least HEIGHT and 0 as examples.
+|#
+
+; Drawing done on pen and paper.
