0a621095b6d845e16440846c19d91f56c44b841e
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-19 09:38:27 +0800

    Update ex_41.rkt

--- htdp_2e_redo/ex_41/ex_41.rkt
+++ htdp_2e_redo/ex_41/ex_41.rkt
@@ -11,7 +11,7 @@ to right on the world canvas, three pixels per clock tick.
 (require 2htdp/universe)
 (require 2htdp/image)
 
-(define WHEEL_RADIUS 5)
+(define WHEEL_RADIUS 5) ; Point of control
 (define WHEEL_DIST (* WHEEL_RADIUS 3))
 
 (define SWIDTH (* WHEEL_RADIUS 50))
@@ -26,16 +26,44 @@ to right on the world canvas, three pixels per clock tick.
 (define BODY_YPOS (- SHEIGHT (/ (image-height BODY) 1.8)))
 (define WHEEL_YPOS (- SHEIGHT WHEEL_RADIUS))
 
-(define CAR
-  (place-images
-   (list WHEEL
-         WHEEL
-         BODY)
-   (list (make-posn (- XCENTER WHEEL_DIST) WHEEL_YPOS)
-         (make-posn (+ XCENTER WHEEL_DIST) WHEEL_YPOS)
-         (make-posn XCENTER BODY_YPOS))
-   BG))
+(define MOVE_SPEED 3) ; Pixels/tick
 
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
+; A WorldState is a Number
+; Interp.: the car's x-coordinate
+
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; WorldState -> Image
+; Renders image of car on the given x-coordinate
+(check-expect (render 15)
+              (place-images
+               (list WHEEL WHEEL BODY)
+               (list (make-posn (+ (- XCENTER WHEEL_DIST) 15) WHEEL_YPOS)
+                     (make-posn (+ (+ XCENTER WHEEL_DIST) 15) WHEEL_YPOS)
+                     (make-posn (+ XCENTER 15) BODY_YPOS))
+               BG))
+
+(define (render ws)
+  (place-images
+   (list WHEEL WHEEL BODY)
+   (list (make-posn (+ (- XCENTER WHEEL_DIST) ws) WHEEL_YPOS)
+         (make-posn (+ (+ XCENTER WHEEL_DIST) ws) WHEEL_YPOS)
+         (make-posn (+ XCENTER ws) BODY_YPOS))
+   BG))
+
+; WorldState -> WorldState
+; Updates WorldState per tick
+(check-expect (time_step 25) 28)
+
+(define (time_step ws)
+  (+ ws MOVE_SPEED))
+
+; WorldState -> WorldState
+(define (main state)
+  (big-bang state
+    [to-draw render]
+    [on-tick time_step]))
+
+(main -100)
