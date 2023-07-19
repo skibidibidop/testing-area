cf4ac6c7aca2e3d1fed4e3ef9fa0b8c7bb9cf19e
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-19 10:52:20 +0800

    Add exercise description

---
+++ htdp_2e_redo/ex_45/ex_45.rkt
@@ -0,0 +1,11 @@
+#|
+Author: Mark Beltran
+Date: July 19, 2023
+
+Exercise 45. Design a “virtual cat” world program that continuously
+moves the cat from left to right. Let’s call it cat-prog and let’s assume it
+consumes the starting position of the cat. Furthermore, make the cat move
+three pixels per clock tick. Whenever the cat disappears on the right, it
+reappears on the left. You may wish to read up on the modulo function.
+|#
+
