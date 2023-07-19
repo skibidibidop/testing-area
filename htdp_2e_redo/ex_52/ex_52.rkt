c767d28acc998dfcfdc4f09cbc35507dfc6abdbc
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-20 06:22:53 +0800

    Update ex_52.rkt

---
+++ htdp_2e_redo/ex_52/ex_52.rkt
@@ -0,0 +1,11 @@
+#|
+Author: Mark Beltran
+Date: July 20, 2023
+
+Exercise 52. Which integers are contained in the four intervals above?
+|#
+
+; [3, 5] contains 3, 4, 5
+; (3, 5] contains 4, 5
+; [3, 5) contains 3, 4
+; (3, 5) contains 4
