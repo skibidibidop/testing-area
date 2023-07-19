cec785e695e62f0c4ae6c433ece283ee30b4d0aa
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-19 11:22:20 +0800

    Add constants, data def, (render)

--- htdp_2e_redo/ex_45/ex_45.rkt
+++ htdp_2e_redo/ex_45/ex_45.rkt
@@ -9,3 +9,34 @@ three pixels per clock tick. Whenever the cat disappears on the right, it
 reappears on the left. You may wish to read up on the modulo function.
 |#
 
+(require 2htdp/image)
+(require 2htdp/universe)
+
+(define SCALER 10)
+(define SCN_WIDTH (* SCALER 50))
+(define SCN_HEIGHT (* SCALER 30))
+(define XCENTER (/ SCN_WIDTH 2))
+(define YCENTER (/ SCN_WIDTH 2))
+
+(define CAT (circle (* SCALER 2) "outline" "black"))
+(define BG (empty-scene SCN_WIDTH SCN_HEIGHT))
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+(define-struct catpos [x y])
+; (make-catpos Number Number)
+; Interp.: (make-catpos xpos ypos), represents CAT's
+; x (xpos), y (ypos) coordinates
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; Catpos -> Image
+; Draws CAT on BG with its position based on cpos
+(check-expect (render (make-catpos 30 90))
+              (place-image CAT 30 90 BG))
+
+(define (render cpos)
+  (place-image CAT
+               (catpos-x cpos)
+               (catpos-y cpos)
+               BG))
