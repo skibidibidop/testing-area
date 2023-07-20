a3ce97c51555a534b46cd7cfe802088b812e7e36
skibidibidop <dev.mapb@gmail.com>
2023-07-20 15:29:37 +0800

    Add Ex. 62 solution

--- htdp_2e_redo/ex_62/ex_62.rkt
+++ htdp_2e_redo/ex_62/ex_62.rkt
@@ -19,3 +19,69 @@ During a door simulation the “open” state is barely vis-
 ible. Modify door-simulation so that the clock ticks once every three
 seconds. Rerun the simulation.
 |#
+
+(require 2htdp/universe)
+(require 2htdp/image)
+
+(define LOCKED "locked")
+(define CLOSED "closed")
+(define OPEN "open")
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; A DoorState is one of:
+; -- LOCKED
+; -- CLOSED
+; -- OPEN
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; DoorState -> DoorState
+; closes an open door over the period of one tick
+(check-expect (door-closer LOCKED) LOCKED)
+(check-expect (door-closer CLOSED) CLOSED)
+(check-expect (door-closer OPEN) CLOSED)
+
+(define (door-closer state-of-door)
+  (cond
+    [(string=? LOCKED state-of-door) LOCKED]
+    [(string=? CLOSED state-of-door) CLOSED]
+    [(string=? OPEN state-of-door) CLOSED]))
+
+; DoorState KeyEvent -> DoorState
+; turns key event k into an action on state s
+(check-expect (door-action LOCKED "u") CLOSED)
+(check-expect (door-action CLOSED "l") LOCKED)
+(check-expect (door-action CLOSED " ") OPEN)
+(check-expect (door-action OPEN "a") OPEN)
+(check-expect (door-action CLOSED "a") CLOSED)
+
+(define (door-action s k)
+  (cond
+    [(and (string=? LOCKED s) (string=? "u" k))
+     CLOSED]
+    [(and (string=? CLOSED s) (string=? "l" k))
+     LOCKED]
+    [(and (string=? CLOSED s) (string=? " " k))
+     OPEN]
+    [else s]))
+
+; DoorState -> Image
+; translates the state s into a large text image
+(check-expect (door-render CLOSED)
+              (text CLOSED 40 "red"))
+
+(define (door-render s)
+  (text s 40 "red"))
+
+; MAIN /////////////////////////////////////////////////////////////////////////
+
+; DoorState -> DoorState
+; simulates a door with an automatic door closer
+(define (door-simulation initial-state)
+  (big-bang initial-state
+    [on-tick door-closer 3]
+    [on-key door-action]
+    [to-draw door-render]))
+
+(door-simulation LOCKED)
