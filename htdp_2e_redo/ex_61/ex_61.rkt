8f3598bcb3298b67daa76ec4b684adf3cf4a384a
skibidibidop <dev.mapb@gmail.com>
2023-07-20 15:11:56 +0800

    Add Ex. 61 answer

---
+++ htdp_2e_redo/ex_61/ex_61.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex_61) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: July 20, 2023
+
+Exercise 61.
+
+The function on the right is properly designed
+using the recipe for itemizations. It will continue to
+work even if the constants are redefined to strings.
+|#
