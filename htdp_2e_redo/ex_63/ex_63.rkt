ce150291c6c5aa38b19cde945e50cb1fa6ea2cbb
skibidibidop <dev.mapb@gmail.com>
2023-07-20 15:44:20 +0800

    Add exercise description

---
+++ htdp_2e_redo/ex_63/ex_63.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex_63) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: July 20, 2023
+
+Exercise 63. Evaluate the following expressions:
+
+• (distance-to-0 (make-posn 3 4))
+• (distance-to-0 (make-posn 6 (* 2 4)))
+• (+ (distance-to-0 (make-posn 12 5)) 10)
+
+by hand. Show all steps. Assume that sqr performs its computation in a
+single step. Check the results with DrRacket’s stepper.
+|#
+
