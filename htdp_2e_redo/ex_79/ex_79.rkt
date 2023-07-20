d693405258dac9bb944de041a5211b0a866e8871
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-21 06:28:55 +0800

    Update ex_79.rkt

---
+++ htdp_2e_redo/ex_79/ex_79.rkt
@@ -0,0 +1,23 @@
+#|
+Author: Mark Beltran
+Date: July 21, 2023
+
+Exercise 79. Create examples for the following data definitions:
+
+• ; A Color is one of:
+; --- "white"
+; --- "yellow"
+; --- "orange"
+; --- "green"
+; --- "red"
+; --- "blue"
+; --- "black"
+|#
+
+(define WHITE "white")
+(define YELLOW "yellow")
+(define ORANGE "orange")
+(define GREEN "green")
+(define RED "red")
+(define BLUE "blue")
+(define BLACK "black")
