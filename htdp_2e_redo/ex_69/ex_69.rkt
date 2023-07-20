40552932d62cb3ed09bdfcecc19c0822b945a907
skibidibidop <dev.mapb@gmail.com>
2023-07-20 17:15:49 +0800

    Add Ex. 60 solution

---
+++ htdp_2e_redo/ex_69/ex_69.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex_69) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: July 20, 2023
+
+Exercise 69. Draw box representations for the solution of exercise 65.
+|#
+
+; Box representations drawn by hand.
