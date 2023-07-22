3855a5a07ebe7ac320b46a5c4bbfab6625c95805
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-23 06:20:13 +0800

    Add Ex. 88 description

---
+++ htdp_2e_redo/ex_88/ex_88.rkt
@@ -0,0 +1,17 @@
+#|
+Author: Mark Beltran
+Date: July 23, 2023
+
+Exercise 88. Define a structure type that keeps track of the cat’s xcoordinate
+and its happiness. Then formulate a data definition for cats, dubbed VCat,
+including an interpretation.
+|#
+
+(require 2htdp/image)
+(require 2htdp/universe)
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; MAIN /////////////////////////////////////////////////////////////////////////
