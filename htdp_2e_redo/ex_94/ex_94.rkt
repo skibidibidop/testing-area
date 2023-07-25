41b295a888c087dabe663156ccd9802db1775103
skibidibidop <dev.mapb@gmail.com>
2023-07-25 08:23:35 +0800

    Add Ex. 94 solution

--- htdp_2e_redo/ex_94/ex_94.rkt
+++ htdp_2e_redo/ex_94/ex_94.rkt
@@ -35,3 +35,48 @@ stants that describe the dimensions of the world (canvas) and its objects.
 Also develop some background scenery. Finally, create your initial scene
 from the constants for the tank, the UFO, and the background.
 |#
+
+(require 2htdp/image)
+(require 2htdp/universe)
+
+(define SCALER 1) ; Single point of control
+
+(define SCN_WIDTH   (* SCALER 200))
+(define SCN_HEIGHT  (* SCALER 400))
+(define SCN_XCENTER (/ SCN_WIDTH 2))
+
+(define BG          (empty-scene SCN_WIDTH SCN_HEIGHT))
+
+(define TANK_WIDTH  (* SCALER 50))
+(define TANK_HEIGHT (* SCALER 10))
+(define TANK        (rectangle TANK_WIDTH TANK_HEIGHT
+                               "solid" "light steel blue"))
+(define TANK_YPOS   (- SCN_HEIGHT (/ TANK_HEIGHT 2)))
+
+(define UFO_RAD     (* SCALER 30))
+(define UFO        (circle UFO_RAD "solid" "dark purple"))
+(define UFO_XPOS    SCN_XCENTER)
+
+(define MISSILE_SIDE_SIZE (* SCALER 10))
+(define MISSILE (triangle MISSILE_SIDE_SIZE "solid" "red"))
+(define MISSILE_START_YPOS (- SCN_HEIGHT
+                              (+ TANK_HEIGHT
+                                 (/ MISSILE_SIDE_SIZE 2))))
+
+(define TANK_MOVSPD 3)
+(define UFO_MOVSPD  1)
+(define MSL_MOVSPD  3)
+
+(define INIT_STATE
+  (place-images
+   (list UFO TANK MISSILE)
+   (list (make-posn UFO_XPOS UFO_RAD)
+         (make-posn SCN_XCENTER TANK_YPOS)
+         (make-posn SCN_XCENTER MISSILE_START_YPOS))
+   BG))
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; MAIN /////////////////////////////////////////////////////////////////////////
