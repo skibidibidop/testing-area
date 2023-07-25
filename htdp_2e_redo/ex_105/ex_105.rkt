22c2308e5e1085c4d7c2d68d76875a58274b037e
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-26 06:43:09 +0800

    Add ex. 105 description

---
+++ htdp_2e_redo/ex_105/ex_105.rkt
@@ -0,0 +1,15 @@
+#|
+Author: Mark Beltran
+Date: July 26, 2023
+
+Exercise 105. Some program contains the following data definition:
+
+; A Coordinate is one of:
+; -- a NegativeNumber ; interpretation on the y axis, distance from top
+; -- a PositiveNumber ; interpretation on the x axis, distance from left
+; -- a Posn ; interpretation an ordinary Cartesian point
+
+Make up at least two data examples per clause in the data definition. For
+each of the examples, explain its meaning with a sketch of a canvas.
+|#
+
