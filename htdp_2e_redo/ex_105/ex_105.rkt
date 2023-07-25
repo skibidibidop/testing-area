299f7fc02b008496687bcf0cdee1404e5f451a62
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-26 07:37:49 +0800

    Add ex. 105 solution

--- htdp_2e_redo/ex_105/ex_105.rkt
+++ htdp_2e_redo/ex_105/ex_105.rkt
@@ -13,3 +13,13 @@ Make up at least two data examples per clause in the data definition. For
 each of the examples, explain its meaning with a sketch of a canvas.
 |#
 
+; I am confused by the name "NegativeNumber" because of how the y-axis
+; works in this language. A negative number for the y-coordinate would
+; mean that the point will be out of the canvas.
+(define Y_COORD -50)
+
+; PositiveNumber
+(define X_COORD 50)
+
+; Posn sample
+(define cart_point (make-posn 30 40))
