34c254f3b700d7ff3b98993e86f4863394835417
skibidibidop <dev.mapb@gmail.com>
2023-07-26 16:30:47 +0800

    Add (render) body

--- htdp_2e_redo/ex_107/ex_107.rkt
+++ htdp_2e_redo/ex_107/ex_107.rkt
@@ -37,18 +37,31 @@ Choose one of the alternatives and design the appropriate program.
 
 (define HGAUGE_WIDTH SCN_WIDTH)
 (define HGAUGE_HEIGHT (* SCALER 10))
+(define HAP_MAX (/ SCN_WIDTH 2))
+(define HAP_MIN (- 0 (/ SCN_WIDTH 2)))
+
+(define MOV_SPD 3)
 
 (define CAT_RAD (* SCALER 25))
 (define CAT (circle CAT_RAD "solid" "brown"))
 (define CAT_HGAUGE (rectangle HGAUGE_WIDTH HGAUGE_HEIGHT
                               "solid" "brown"))
-(define CAT_HGAUGE_YPOS (/HGAUGE_HEIGHT 2))
+(define CAT_HGAUGE_YPOS (/ HGAUGE_HEIGHT 2))
+(define CAT_YPOS (- SCN_HEIGHT CAT_RAD))
+(define CAT_MAX_XPOS (- SCN_WIDTH CAT_RAD))
+(define CAT_START_XPOS CAT_RAD)
+(define CAT_MOVE MOV_SPD)
 
 (define CHAM_WIDTH (* SCALER 40))
 (define CHAM_HEIGHT (* SCALER 15))
 (define CHAM_HGAUGE (rectangle HGAUGE_WIDTH HGAUGE_HEIGHT
                                "solid" "green"))
-(define CHAM_HGAUGE_YPOS HGAUGE_HEIGHT)
+(define CHAM_HGAUGE_YPOS (+ HGAUGE_HEIGHT
+                            (/ HGAUGE_HEIGHT 2)))
+(define CHAM_YPOS (- SCN_HEIGHT (/ CHAM_HEIGHT 2)))
+(define CHAM_MAX_XPOS (/ CHAM_WIDTH 2))
+(define CHAM_START_XPOS (- SCN_WIDTH (/ CHAM_WIDTH 2)))
+(define CHAM_MOVE (* MOV_SPD -1))
 
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
@@ -86,11 +99,48 @@ Choose one of the alternatives and design the appropriate program.
 ; "g"    - signals the chameleon to change its color to "green"
 ; "b"    - signals the chameleon to change its color to "blue"
 
+(define ZOO_START (make-zoo
+                   (make-vcat SCN_XCENTER HAP_MAX)
+                   (make-vcham SCN_XCENTER HAP_MAX "red")))
+(define ZOO_HUNGRY (make-zoo
+                    (make-vcat SCN_XCENTER HAP_MIN)
+                    (make-vcham SCN_XCENTER HAP_MIN "green")))
+(define CAT_BORDER (make-zoo
+                    (make-vcat CAT_MAX_XPOS HAP_MAX)
+                    (make-vcham SCN_XCENTER HAP_MAX "blue")))
+(define CHAM_BORDER (make-zoo
+                     (make-vcat SCN_XCENTER HAP_MAX)
+                     (make-vcham CHAM_MAX_XPOS HAP_MAX "red")))
+
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
 
 ; Zoo -> Image
 ; Renders image based on data from Zoo z
-(define (render z) BG)
+(check-expect (render ZOO_START)
+              (place-images
+               (list (rectangle CHAM_WIDTH CHAM_HEIGHT
+                                "outline" "red")
+                     CAT CHAM_HGAUGE CAT_HGAUGE)
+               (list (make-posn SCN_XCENTER CHAM_YPOS)
+                     (make-posn SCN_XCENTER CAT_YPOS)
+                     (make-posn HAP_MAX CHAM_HGAUGE_YPOS)
+                     (make-posn HAP_MAX CAT_HGAUGE_YPOS))
+               BG))
+
+(define (render z)
+  (place-images
+   (list (rectangle CHAM_WIDTH CHAM_HEIGHT
+                    "outline" (vcham-col (zoo-cham z)))
+         CAT CHAM_HGAUGE CAT_HGAUGE)
+   (list (make-posn
+          (vcham-loc (zoo-cham z)) CHAM_YPOS)
+         (make-posn
+          (vcat-loc (zoo-cat z)) CAT_YPOS)
+         (make-posn
+          (vcham-hap (zoo-cham z)) CHAM_HGAUGE_YPOS)
+         (make-posn
+          (vcat-hap (zoo-cat z)) CAT_HGAUGE_YPOS))
+   BG))
 
 ; Zoo -> Zoo
 ; Updates Zoo z per tick
