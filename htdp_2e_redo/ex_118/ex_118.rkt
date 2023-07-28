f15adcd6ef28f015cfa8c05c11bf74069877a021
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-29 06:13:16 +0800

    Add ex. 118 solution

---
+++ htdp_2e_redo/ex_118/ex_118.rkt
@@ -0,0 +1,14 @@
+#|
+Author: Mark Beltran
+Date: July 29, 2023
+
+Exercise 118. Take a look at the following sentences:
+
+1. (define (f x) x)
+2. (define (f x) y)
+3. (define (f x y) 3)
+
+Explain why they are syntactically legal definitions
+|#
+
+; These are all just regular function definitions
