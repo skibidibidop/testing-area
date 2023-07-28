b2104f34c45d957bd5ad8090d6a0146f8c11cc1e
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-28 10:36:27 +0800

    Add ex. 113 solution

--- htdp_2e_redo/ex_113/ex_113.rkt
+++ htdp_2e_redo/ex_113/ex_113.rkt
@@ -7,25 +7,36 @@ the preceding section: SIGS, Coordinate (exercise 105), and VAnimal.
 |#
 
 ; A SIGS is one of:
-; -- (make-aim UFO Tank
-)
-; -- (make-fired UFO Tank Missile
-)
+; -- (make-aim UFO Tank)
+; -- (make-fired UFO Tank Missile)
 ; interpretation represents the complete state of a
 ; space invader game
 
 ; A Coordinate is one of:
-; -- a NegativeNumber ; interpretation on the y axis, distance from top
-; -- a PositiveNumber ; interpretation on the x axis, distance from left
-; -- a Posn ; interpretation an ordinary Cartesian point
+; -- a NegativeNumber
+; interpretation on the y axis, distance from top
+; -- a PositiveNumber
+; interpretation on the x axis, distance from left
+; -- a Posn
+; interpretation an ordinary Cartesian point
 
 ; A VAnimal is either
 ; -- a VCat
 ; -- a VCham
 
+(define-struct aim [ufo tank])
+
+(define-struct fired [ufo tank missile])
+
 (define-struct vcat [loc hap])
 
 (define-struct vcham [loc hap col])
 
-(define (SIGS? s)
-  (or (
+(define (sigs? s)
+  (or (aim? s) (fired? s)))
+
+(define (coordinate? c)
+  (or (posn? c) (positive? c)))
+
+(define (vanimal? v)
+  (or (vcat? v) (vcham? v)))
