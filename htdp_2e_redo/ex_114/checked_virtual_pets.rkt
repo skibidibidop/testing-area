da3ef212ed304a9332dc858b52153bfdca92938f
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-29 05:26:04 +0800

    Update checked_virtual_pets.rkt

--- htdp_2e_redo/ex_114/checked_virtual_pets.rkt
+++ htdp_2e_redo/ex_114/checked_virtual_pets.rkt
@@ -1,29 +1,10 @@
 #|
 Author: Mark Beltran
-Date: July 26, 2023
+Date: July 29, 2023
 
-Exercise 106. In chapter 5.11 we discussed the creation of virtual pets
-that come with happiness gauges. One of the virtual pets is a cat; the other
-one, a chameleon. Each program is dedicated to a single pet, however.
-Design the cat-cham world program. Given both a location and an animal,
-it walks the latter across the canvas, starting from the given location.
-
-Here is the chosen data representation for animals:
-
-; A VAnimal is either
-; -- a VCat
-; -- a VCham
-
-where VCat and VCham are your data definitions from exercises 88 and 92.
-
-Given that VAnimal is the collection of world states, you need to design
-
-• a rendering function from VAnimal to Image;
-• a function for handling clock ticks, from VAnimal to VAnimal; and
-• a function for dealing with key events so that you can feed and pet
-and colorize your animal—as applicable.
-
-It remains impossible to change the color of a cat or to pet a chameleon.
+Exercise 114. Use the predicates from exercise 113 to check the space
+invader world program, the virtual pet program (exercise 106), and the
+editor program (chapter 5.10).
 |#
 
 (require 2htdp/image)
