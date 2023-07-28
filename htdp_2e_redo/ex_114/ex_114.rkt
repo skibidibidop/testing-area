1f22bc08d731b7cb5bb32a2f22a9de744e465992
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-29 05:16:31 +0800

    Add ex. 114 solution

--- htdp_2e_redo/ex_114/ex_114.rkt
+++ htdp_2e_redo/ex_114/ex_114.rkt
@@ -7,38 +7,4 @@ invader world program, the virtual pet program (exercise 106), and the
 editor program (chapter 5.10).
 |#
 
-; A SIGS is one of:
-; -- (make-aim UFO Tank)
-; -- (make-fired UFO Tank Missile)
-; interpretation represents the complete state of a
-; space invader game
-
-; A Coordinate is one of:
-; -- a NegativeNumber
-; interpretation on the y axis, distance from top
-; -- a PositiveNumber
-; interpretation on the x axis, distance from left
-; -- a Posn
-; interpretation an ordinary Cartesian point
-
-; A VAnimal is either
-; -- a VCat
-; -- a VCham
-
-(define-struct aim [ufo tank])
-
-(define-struct fired [ufo tank missile])
-
-(define-struct vcat [loc hap])
-
-(define-struct vcham [loc hap col])
-
-(define (sigs? s)
-  (or (aim? s) (fired? s)))
-
-(define (coordinate? c)
-  (or (posn? c) (positive? c)))
-
-(define (vanimal? v)
-  (or (vcat? v) (vcham? v)))
-
+; See other files in this folder
