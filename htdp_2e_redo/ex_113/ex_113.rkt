c5f912de4464c106e03397e752abb29c264e9a0c
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-28 10:32:56 +0800

    Add data defs

---
+++ htdp_2e_redo/ex_113/ex_113.rkt
@@ -0,0 +1,31 @@
+#|
+Author: Mark Beltran
+Date: July 28, 2023
+
+Exercise 113. Design predicates for the following data definitions from
+the preceding section: SIGS, Coordinate (exercise 105), and VAnimal.
+|#
+
+; A SIGS is one of:
+; -- (make-aim UFO Tank
+)
+; -- (make-fired UFO Tank Missile
+)
+; interpretation represents the complete state of a
+; space invader game
+
+; A Coordinate is one of:
+; -- a NegativeNumber ; interpretation on the y axis, distance from top
+; -- a PositiveNumber ; interpretation on the x axis, distance from left
+; -- a Posn ; interpretation an ordinary Cartesian point
+
+; A VAnimal is either
+; -- a VCat
+; -- a VCham
+
+(define-struct vcat [loc hap])
+
+(define-struct vcham [loc hap col])
+
+(define (SIGS? s)
+  (or (
