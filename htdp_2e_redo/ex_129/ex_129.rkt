a783ace210ca66340a06f0642f2768be8354a0da
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-29 09:39:41 +0800

    Add ex. 129 solution

---
+++ htdp_2e_redo/ex_129/ex_129.rkt
@@ -0,0 +1,22 @@
+#|
+Author: Mark Beltran
+Date: July 29, 2023
+
+Exercise 129. Create BSL lists that represent
+
+1. a list of celestial bodies, say, at least all the planets in
+our solar system;
+2. a list of items for a meal, for example, steak, french fries, beans, bread,
+water, Brie cheese, and ice cream; and
+3. a list of colors.
+
+Sketch some box representations of these lists, similar to those in figures 44
+and 45. Which of the sketches do you like better?
+|#
+
+(list "Mercury" "Venus" "Earth" "Mars" "Jupiter" "Saturn"
+      "Uranus" "Neptune" "PLUTO")
+
+(list "steak" "french fries" "beans" "bread" "water" "Brie" "ice cream")
+
+(list "red" "green" "blue")
