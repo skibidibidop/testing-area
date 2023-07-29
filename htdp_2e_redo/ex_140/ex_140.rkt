ef33cd1e9a2ecca28e4e3b83b0b82298f9aeb605
skibidibidop <dev.mapb@gmail.com>
2023-07-29 18:48:34 +0800

    Add ex. 140 description

---
+++ htdp_2e_redo/ex_140/ex_140.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex_140) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: July 29, 2023
+
+Exercise 140. Design the function all-true, which consumes a list of
+Boolean values and determines whether all of them are #true. In other
+words, if there is any #false on the list, the function produces #false.
+
+Now design one-true, a function that consumes a list of Boolean val-
+ues and determines whether at least one item on the list is #true.
+|#
