e572aa9362ac7930ef772d4ad881f9a2c74de71d
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-29 08:37:50 +0800

    Add ex. 125 solution

---
+++ htdp_2e_redo/ex_125/ex_125.rkt
@@ -0,0 +1,18 @@
+#|
+Author: Mark Beltran
+Date: July 29, 2023
+
+Exercise 125. Discriminate the legal from the illegal sentences:
+
+1. (define-struct oops [])
+2. (define-struct child [parents dob date])
+3. (define-struct (child person) [dob date])
+
+Explain why the sentences are legal or illegal
+|#
+
+#|
+1. Illegal, no selectors.
+2. Legal, follows the format for define-struct.
+3. Illegal, can't use a function as the struct name.
+|#
