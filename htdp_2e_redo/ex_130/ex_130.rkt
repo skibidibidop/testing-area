d3db7a74f0fb13bc0f9517c9491b0ffc0cc13d98
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-29 09:41:38 +0800

    Add ex. 130 solution

---
+++ htdp_2e_redo/ex_130/ex_130.rkt
@@ -0,0 +1,18 @@
+#|
+Author: Mark Beltran
+Date: July 29, 2023
+
+Exercise 130. Create an element of List-of-names that contains five
+Strings. Sketch a box representation of the list similar to those found in
+figure 44.
+
+Explain why
+
+(cons "1" (cons "2" '()))
+
+is an element of List-of-names and why (cons 2 '()) isn’t.
+|#
+
+; Sketch done on paper
+
+; The list provided above contains strings and not numbers.
