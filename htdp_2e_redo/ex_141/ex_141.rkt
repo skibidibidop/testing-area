d3ccd1259755ed4ac1ac3241a126293d6a7b8df4
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-07-30 10:43:04 +0800

    Add ex. 141 solution

--- htdp_2e_redo/ex_141/ex_141.rkt
+++ htdp_2e_redo/ex_141/ex_141.rkt
@@ -1,3 +1,43 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex_141) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: July 30, 2023
+
+Exercise 141. If you are asked to design the function cat, which consumes
+a list of strings and appends them all into one long string, you are
+guaranteed to end up with this partial definition:
+
+; List-of-string -> String
+; concatenates all strings in l into one long string
+(check-expect (cat '()) "")
+(check-expect (cat (cons "a" (cons "b" '()))) "ab")
+(check-expect
+ (cat (cons "ab" (cons "cd" (cons "ef" '()))))
+ "abcdef")
+
+(define (cat l)
+  (cond
+    [(empty? l) ""]
+    [else (... (first l) ... (cat (rest l)) ...)]))
+
+Fill in the table in figure 57. Guess a function that can create the desired
+result from the values computed by the sub-expressions.
+
+Use DrRacket’s stepper to evaluate (cat (cons "a" '()))
+|#
+
+#|
+(cons "a" (cons "b" '()))
+
+(first l) : "a"
+(rest l) : (cons "b" '())
+(cat (rest l)) : "b"
+(cat l) "ab"
+
+(cons "ab" (cons "cd" (cons "ef" '())))
+
+(first l) : "ab"
+(rest l) : (cons "cd" (cons "ef" '()))
+(cat (rest l)) : "cdef"
+(cat l : "abcdef"
+|#
+
