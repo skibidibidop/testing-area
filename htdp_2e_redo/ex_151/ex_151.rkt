989c529a058e8427a1600a2386119aa10b1148dd
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-02 10:33:54 +0800

    Add ex 151 solution

--- htdp_2e_redo/ex_151/ex_151.rkt
+++ htdp_2e_redo/ex_151/ex_151.rkt
@@ -9,3 +9,12 @@ Use DrRacket’s stepper to evaluate (multiply 3 x) for any x you
 like. How does multiply relate to what you know from grade school?
 |#
 
+; N Number -> Number
+; Multiplies Natural number n to Number x
+(check-within (multiply 2 3) (* 2 3) 0.001)
+
+(define (multiply n x)
+  (cond
+    [(= n 1) x]
+    [else
+     (+ x (multiply (- n 1) x))]))
