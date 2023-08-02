b6f3451dc3ce69a1ca3f2c8cf120791ccca699a2
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-02 08:48:30 +0800

    Add ex. 145 description

---
+++ htdp_2e_redo/ex_145/ex_145.rkt
@@ -0,0 +1,10 @@
+#|
+Author: Mark Beltran
+Date: July 30, 2023
+
+Exercise 145. Design the sorted>? predicate, which consumes a
+NEListof-temperatures and produces #true if the temperatures are sorted in
+descending order. That is, if the second is smaller than the first, the third
+smaller than the second, and so on. Otherwise it produces #false.
+|#
+
