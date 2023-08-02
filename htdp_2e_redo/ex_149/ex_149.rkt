a511ea5f19c0ad9b7b847fd87c59c64cebf4d289
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-02 10:09:46 +0800

    Add ex 149 solution

--- htdp_2e_redo/ex_149/ex_149.rkt
+++ htdp_2e_redo/ex_149/ex_149.rkt
@@ -17,4 +17,25 @@ How do copier and copier.v2 behave when you apply them to 0.1
 and "x"? Explain. Use DrRacket’s stepper to confirm your explanation.
 |#
 
+; N String -> List-of-strings
+; creates a list of n copies of s
+(check-expect (copier 0 "hello") '())
+(check-expect (copier 2 "hello")
+              (cons "hello" (cons "hello" '())))
 
+(define (copier n s)
+  (cond
+    [(zero? n) '()]
+    [(positive? n) (cons s (copier (sub1 n) s))]))
+
+; copier does not function properly if it isn't given an N and a String.
+; A new function has to be designed or copier has to be redesigned to
+; handle cases such as those.
+
+; Alternative definition provided:
+(define (copier.v2 n s)
+  (cond
+    [(zero? n) '()]
+    [else (cons s (copier.v2 (sub1 n) s))]))
+
+; 0.1 causes an infinite loop
