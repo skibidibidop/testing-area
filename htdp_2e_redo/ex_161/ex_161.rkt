2a6a3378badaef46ffcdc7d42c124047ddaab254
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-03 07:47:48 +0800

    Add ex 161 solution

--- htdp_2e_redo/ex_161/ex_161.rkt
+++ htdp_2e_redo/ex_161/ex_161.rkt
@@ -8,3 +8,25 @@ per hour. Now revise the entire program so that changing the wage for
 everyone is a single change to the entire program and not several.
 |#
 
+(define HOURLY_RATE 14)
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; Number -> Number
+; computes the wage for h hours of work
+(define (wage h)
+  (* HOURLY_RATE h))
+
+; List-of-numbers -> List-of-numbers
+; computes the weekly wages for all given weekly hours
+(check-expect (wage* '()) '())
+(check-expect (wage* (cons 28 '())) (cons (* 28 HOURLY_RATE) '()))
+(check-expect (wage* (cons 4 (cons 2 '())))
+              (cons (* 4 HOURLY_RATE) (cons (* 2 HOURLY_RATE) '())))
+
+(define (wage* whrs)
+  (cond
+    [(empty? whrs) '()]
+    [else (cons (wage (first whrs)) (wage* (rest whrs)))]))
