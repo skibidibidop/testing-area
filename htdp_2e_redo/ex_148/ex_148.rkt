ed4c35fb66965ef09f1d9a74ae07f93b3e78b82c
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-02 10:02:04 +0800

    Add ex 148 solution

---
+++ htdp_2e_redo/ex_148/ex_148.rkt
@@ -0,0 +1,14 @@
+#|
+Author: Mark Beltran
+Date: August 2, 2023
+
+Exercise 148. Compare the function definitions from this section (sum,
+how-many, all-true, one-true) with the corresponding function definitions
+from the preceding sections. Is it better to work with data definitions that
+accommodate empty lists as opposed to definitions for nonempty lists?
+Why? Why not?
+|#
+
+; I'm not entirely sure but I think that using non-empty lists is
+; redundant. It seems better to use data definitions that accomodate empty
+; lists.
