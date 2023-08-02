0d307b18d16f1aa5db0a831ce9111fabb9375ad6
skibidibidop <dev.mapb@gmail.com>
2023-08-02 18:11:14 +0800

    Add ex 154 description

---
+++ htdp_2e_redo/ex_154/ex_154.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex_154) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: August 2, 2023
+
+Exercise 154. Design the function colors. It consumes a Russian doll
+and produces a string of all colors, separated by a comma and a space. Thus
+our example should produce
+
+"yellow, green, red"
+|#
