79305108e08e5f406d1ad68b4310ad2072978c14
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-03 07:16:22 +0800

    Add ex 161 description

---
+++ htdp_2e_redo/ex_161/ex_161.rkt
@@ -0,0 +1,10 @@
+#|
+Author: Mark Beltran
+Date: August 3, 2023
+
+Exercise 161. Translate the examples into tests and make sure they all
+succeed. Then change the function in figure 64 so that everyone gets $14
+per hour. Now revise the entire program so that changing the wage for
+everyone is a single change to the entire program and not several.
+|#
+
