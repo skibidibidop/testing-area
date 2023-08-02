d6b93dc415b104171ea24e63046370f8add52b4c
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-03 06:49:14 +0800

    Add ex 160 description

---
+++ htdp_2e_redo/ex_160/ex_160.rkt
@@ -0,0 +1,9 @@
+#|
+Author: Mark Beltran
+Date: August 3, 2023
+
+Exercise 160. Design the functions set+.L and set+.R, which create
+a set by adding a number x to some given set s for the left-hand and right
+hand data definition, respectively.
+|#
+
