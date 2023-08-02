6c581c7e8a995b455f3eb23c91443ae025c3f898
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-03 06:13:10 +0800

    Add constants, functions from ex 159

--- htdp_2e_redo/ex_159/ex_159.rkt
+++ htdp_2e_redo/ex_159/ex_159.rkt
@@ -25,3 +25,73 @@ to nest it within another expression.
 (3) Recall that random creates random numbers.
 |#
 
+(require 2htdp/image)
+(require 2htdp/universe)
+
+(define SCALER 1)
+(define SCN_WIDTH   (* SCALER 80))
+(define SCN_HEIGHT  (* SCALER 180))
+(define SCN_XCENTER (/ SCN_WIDTH 2))
+(define SCN_YCENTER (/ SCN_HEIGHT 2))
+
+(define BG (empty-scene (* SCALER 80) (* SCALER 180)))
+
+(define CIRCLE_IMG (circle (* SCALER 10) "outline" "red"))
+(define SQUARE_IMG (square (* SCALER 10) "outline" "black"))
+
+(define RED_DOT (circle (* SCALER 2) "solid" "red"))
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; N Image -> Image
+; Produces a column of n copies of img
+(check-expect (col 0 CIRCLE_IMG) empty-image)
+(check-expect (col 1 SQUARE_IMG)
+              (above SQUARE_IMG empty-image))
+(check-expect (col 3 CIRCLE_IMG)
+              (above CIRCLE_IMG CIRCLE_IMG CIRCLE_IMG empty-image))
+
+(define (col n img)
+  (cond
+    [(= n 0) empty-image]
+    [else
+     (above img (col (- n 1) img))]))
+
+; N Image -> Image
+; Produces a row of n copies of img
+(check-expect (row 0 CIRCLE_IMG) empty-image)
+(check-expect (row 1 SQUARE_IMG)
+              (beside SQUARE_IMG empty-image))
+(check-expect (row 2 CIRCLE_IMG)
+              (beside CIRCLE_IMG CIRCLE_IMG empty-image))
+
+(define (row n img)
+  (cond
+    [(= n 0) empty-image]
+    [else
+     (beside img (row (- n 1) img))]))
+
+(define GRID_COLUMN (col 18 SQUARE_IMG))
+(define GRID
+  (place-image (row 8 GRID_COLUMN) SCN_XCENTER SCN_YCENTER BG))
+
+; Posn_list -> Image
+; Places red dots on GRID based on coordinates in Posn_list pl
+(check-expect (add-balloons '()) GRID)
+(check-expect (add-balloons (list (make-posn 5 10)))
+              (place-images
+               (list RED_DOT)
+               (list (make-posn 5 10))
+               GRID))
+
+(define (add-balloons pl)
+  (cond
+    [(empty? pl) GRID]
+    [else
+     (place-image
+      RED_DOT (posn-x (first pl)) (posn-y (first pl))
+      (add-balloons (rest pl)))]))
