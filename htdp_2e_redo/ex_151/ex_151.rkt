2fb4d6b10ae3bb7c236ecd28a6a89d3034e09810
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-02 10:29:53 +0800

    Add ex 151 description

---
+++ htdp_2e_redo/ex_151/ex_151.rkt
@@ -0,0 +1,11 @@
+#|
+Author: Mark Beltran
+Date: August 2, 2023
+
+Exercise 151. Design the function multiply. It consumes a natural
+number n and multiplies it with a number x without using *.
+
+Use DrRacket’s stepper to evaluate (multiply 3 x) for any x you
+like. How does multiply relate to what you know from grade school?
+|#
+
