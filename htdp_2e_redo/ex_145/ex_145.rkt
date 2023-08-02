01cbfa167f634d841fce1163578ec1099894c6ed
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-02 09:33:41 +0800

    Add ex 145 solution

--- htdp_2e_redo/ex_145/ex_145.rkt
+++ htdp_2e_redo/ex_145/ex_145.rkt
@@ -8,3 +8,21 @@ descending order. That is, if the second is smaller than the first, the third
 smaller than the second, and so on. Otherwise it produces #false.
 |#
 
+(define ERR_MSG "Non-empty list required.")
+
+; NEList-of-temperatures -> Boolean
+; Checks if non-empty list nloi is sorted in descending order
+(check-error  (sorted>? '()) ERR_MSG)
+(check-expect (sorted>? (cons 1 '())) #true)
+(check-expect (sorted>? (cons 0 (cons 1 '()))) #false)
+(check-expect (sorted>? (cons 0 (cons 2 (cons 1 '())))) #false)
+(check-expect (sorted>? (cons 3 (cons 2 (cons 1 '())))) #true)
+
+(define (sorted>? nloi)
+  (cond
+    [(empty? nloi) (error ERR_MSG)]
+    [(empty? (rest nloi)) #true]
+    [else
+     (and (> (first nloi) (first (rest nloi)))
+          (sorted>? (rest nloi)))]))
+            
