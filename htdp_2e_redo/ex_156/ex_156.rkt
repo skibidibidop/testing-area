0cdc5bf6dc5af62448c75f9c5b9b9f2a57789c84
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-03 05:13:10 +0800

    Add data def, contants

--- htdp_2e_redo/ex_156/ex_156.rkt
+++ htdp_2e_redo/ex_156/ex_156.rkt
@@ -6,7 +6,31 @@ Exercise 156. Equip the program in figure 61 with tests and make sure
 it passes those. Explain what main does. Then run the program via main.
 |#
 
+(require 2htdp/image)
+(require 2htdp/universe)
+
+(define HEIGHT 80) ; distances in terms of pixels
+(define WIDTH 100)
+(define XSHOTS (/ WIDTH 2))
+
+; graphical constants
+(define BACKGROUND (empty-scene WIDTH HEIGHT))
+(define SHOT (triangle 3 "solid" "red"))
+
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; A List-of-shots is one of:
+; -- '()
+; -- (cons Shot List-of-shots)
+; interpretation the collection of shots fired
+
+; A Shot is a Number.
+; interpretation represents the shot's y-coordinate
+
+; A ShotWorld is List-of-numbers.
+; interpretation each number on such a list
+; represents the y-coordinate of a shot
+
 
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
 
