eefdc43df0f66acf4cbafb572905859fbd888174
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-02 10:34:49 +0800

    Add ex 152 description

---
+++ htdp_2e_redo/ex_152/ex_152.rkt
@@ -0,0 +1,13 @@
+#|
+Author: Mark Beltran
+Date: August 2, 2023
+
+Exercise 152. Design two functions: col and row.
+
+The function col consumes a natural number n and an image img. It
+produces a column—a vertical arrangement—of n copies of img.
+
+The function row consumes a natural number n and an image img. It
+produces a row—a horizontal arrangement—of n copies of img.
+|#
+
