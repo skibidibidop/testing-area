75670a447ccaac7d8df724b205cf36a648145fa5
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-02 09:47:42 +0800

    Add (one-true)

---
+++ htdp_2e_redo/ex_147/ex_147.rkt
@@ -0,0 +1,35 @@
+#|
+Author: Mark Beltran
+Date: July 30, 2023
+
+Exercise 147. Develop a data definition for NEList-of-Booleans, a representation
+of non-empty lists of Boolean values. Then redesign the functions all-true and
+one-true from exercise 140.
+|#
+
+; An NEList-of-Booleans is one of:
+; (cons Boolean '())
+; (cons Boolean NEList-of-Booleans)
+
+(define ERR_MSG "Non-empty list required.")
+
+; NEList-of-Booleans -> Boolean
+; Checks if there is at least one #true value in nlob
+(check-error  (one-true '()) ERR_MSG)
+(check-expect (one-true (cons #true '())) #true)
+(check-expect (one-true (cons #false '())) #false)
+(check-expect (one-true (cons #true (cons #true '()))) #true)
+(check-expect (one-true (cons #true (cons #false '()))) #true)
+(check-expect (one-true (cons #false (cons #false '()))) #false)
+
+(define (one-true nlob)
+  (cond
+    [(empty? nlob) (error ERR_MSG)]
+    [(empty? (rest nlob)) (first nlob)]
+    [else
+     (or (first nlob)
+         (one-true (rest nlob)))]))
+
+; NEList-of-Booleans -> Boolean
+;
+(define (all-true nlob) #false)
