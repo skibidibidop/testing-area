5761edb9a846cd84d3751ec8df3550bf9aaee920
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-03 07:55:35 +0800

    Add data def: List-of-F, List-of-C, FTemp, CTemp

--- htdp_2e_redo/ex_163/ex_163.rkt
+++ htdp_2e_redo/ex_163/ex_163.rkt
@@ -2,6 +2,28 @@
 Author: Mark Beltran
 Date: August 3, 2023
 
-Exercise 163. Design convertFC. The function converts a list of measurements
+Exercise 163. Design convertFC. The function converts a list of measurements
 in Fahrenheit to a list of Celsius measurements.
 |#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; A List-of-F is one of:
+; '()
+; (cons FTemp List-of-F)
+; Interp.: A list of temperature readings in degrees Fahrenheit
+
+; A List-of-C is one of:
+; '()
+; (cons CTemp List-of-C)
+; Interp.: A list of temperature readings in degrees Celsius
+
+; An FTemp is a temperature reading in degrees Fahrenheit
+
+; A CTemp is a temperature reading in degrees Celsius
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; List-of-F -> List-of-C
+;
+(define (converFC loF) '())
