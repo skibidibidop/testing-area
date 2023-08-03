8ca97e83689111b5d37b5a1b8a26146587daa121
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-03 11:13:52 +0800

    Add ex 167 solution

--- htdp_2e_redo/ex_167/ex_167.rkt
+++ htdp_2e_redo/ex_167/ex_167.rkt
@@ -6,3 +6,17 @@ Exercise 167. Design the function sum, which consumes a list of Posns
 and produces the sum of all of its x-coordinates.
 |#
 
+; List-of-posns -> Number
+; Returns the sum of all x-coordinates in List-of-posn lop
+(check-expect (sum '()) 0)
+(check-expect (sum (list (make-posn 40 50)
+                         (make-posn 10 20)
+                         (make-posn 50 100)))
+              100)
+
+(define (sum lop)
+  (cond
+    [(empty? lop) 0]
+    [else
+     (+ (posn-x (first lop))
+        (sum (rest lop)))]))
