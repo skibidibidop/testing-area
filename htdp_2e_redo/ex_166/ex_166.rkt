ff7438393a2e0d09166ffa1396bcd31cea4f8c0e
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-03 10:22:15 +0800

    Add ex 166 description

---
+++ htdp_2e_redo/ex_166/ex_166.rkt
@@ -0,0 +1,23 @@
+#|
+Author: Mark Beltran
+Date: August 3, 2023
+
+Exercise 166. The wage*.v2 function consumes a list of work records
+and produces a list of numbers. Of course, functions may also produce lists
+of structures.
+
+Develop a data representation for paychecks. Assume that a paycheck
+contains two distinctive pieces of information: the employee’s name and an
+amount. Then design the function wage*.v3. It consumes a list of work
+records and computes a list of paychecks from it, one per record.
+
+In reality, a paycheck also contains an employee number. Develop a
+data representation for employee information and change the data definition
+for work records so that it uses employee information and not just a
+string for the employee’s name. Also change your data representation of
+paychecks so that it contains an employee’s name and number, too. Finally,
+design wage*.v4, a function that maps lists of revised work records
+to lists of revised paychecks.
+|#
+
+
