d5d4bdf237ea62edc3fb57268c34767ed39984e0
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-03 09:54:04 +0800

    Add (convert-euro)

--- htdp_2e_redo/ex_164/ex_164.rkt
+++ htdp_2e_redo/ex_164/ex_164.rkt
@@ -3,10 +3,52 @@ Author: Mark Beltran
 Date: August 3, 2023
 
 Exercise 164. Design the function convert-euro, which converts a
-list of US$ amounts into a list of e amounts. Look up the current exchange
+list of US$ amounts into a list of Euro amounts. Look up the current exchange
 rate on the web.
 
 Generalize convert-euro to the function convert-euro*, which
 consumes an exchange rate and a list of US$ amounts and converts the
 latter into a list of Euro amounts.
 |#
+
+(define USD_EUR 0.91)
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; A List-of-USD is one of:
+; '()
+; (cons USD List-of-USD)
+; Interp.: list of US Dollar amounts
+
+; A List-of-EUR is one of:
+; '()
+; (cons EUR List-of-EUR)
+; Interp.: list of Euro amounts
+
+; A USD is a Number
+; Interp.: represents an amount in US Dollars
+
+; A EUR is a Number
+; Interp.: represents an amount in Euros
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; List-of-USD -> List-of-EUR
+; Converts a list of USD amounts to a list of EUR amounts
+(check-expect (convert-euro '()) '())
+(check-within (convert-euro (list 1)) (list (* 1 USD_EUR)) 0.1)
+(check-within (convert-euro (list 30 500))
+              (list (* 30 USD_EUR) (* 500 USD_EUR))
+              0.001)
+
+(define (convert-euro lusd)
+  (cond
+    [(empty? lusd) '()]
+    [else
+     (cons (* (first lusd) USD_EUR)
+           (convert-euro (rest lusd)))]))
+
+;
+;
+(
+  
