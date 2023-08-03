0fef69f1438cefe6b4bd7107fc93e15958b166e0
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-03 08:06:01 +0800

    Add ex 164 description

---
+++ htdp_2e_redo/ex_164/ex_164.rkt
@@ -0,0 +1,12 @@
+#|
+Author: Mark Beltran
+Date: August 3, 2023
+
+Exercise 164. Design the function convert-euro, which converts a
+list of US$ amounts into a list of e amounts. Look up the current exchange
+rate on the web.
+
+Generalize convert-euro to the function convert-euro*, which
+consumes an exchange rate and a list of US$ amounts and converts the
+latter into a list of Euro amounts.
+|#
