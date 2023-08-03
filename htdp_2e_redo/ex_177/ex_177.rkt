93a6953b1d86dd83ebab15aacd505903aee38e0d
skibidibidop <dev.mapb@gmail.com>
2023-08-03 16:51:57 +0800

    Add ex 177 description

---
+++ htdp_2e_redo/ex_177/ex_177.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex_177) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: August 3, 2023
+
+Exercise 177. Design the function create-editor. The function con-
+sumes two strings and produces an Editor. The first string is the text to the
+left of the cursor and the second string is the text to the right of the cursor.
+The rest of the section relies on this function.
+|#
+
