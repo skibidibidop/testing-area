5ec28de010edb034c22b7e58d1dab80aae088a2c
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-03 10:05:26 +0800

    Add ex 165 description

---
+++ htdp_2e_redo/ex_165/ex_165.rkt
@@ -0,0 +1,13 @@
+#|
+Author: Mark Beltran
+Date: August 3, 2023
+
+Exercise 165. Design the function subst-robot, which consumes a
+list of toy descriptions (one-word strings) and replaces all occurrences of
+"robot" with "r2d2"; all other descriptions remain the same.
+
+Generalize subst-robot to substitute. The latter consumes two
+strings, called new and old, and a list of strings. It produces a new list of
+strings by substituting all occurrences of old with new.
+|#
+
