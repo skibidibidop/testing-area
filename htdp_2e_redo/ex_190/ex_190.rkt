00ac08b01cb7aac37a3d058a56ed593bfd01aed5
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-04 06:22:21 +0800

    Add ex 190 description

---
+++ htdp_2e_redo/ex_190/ex_190.rkt
@@ -0,0 +1,15 @@
+#|
+Author: Mark Beltran
+Date: August 4, 2023
+
+Exercise 190. Design the prefixes function, which consumes a list of
+1Strings and produces the list of all prefixes. A list p is a prefix of
+l if p and l are the same up through all items in p. For example, (list "a" "b"
+"c") is a prefix of itself and (list "a" "b" "c" "d").
+
+Design the function suffixes, which consumes a list of 1Strings and
+produces all suffixes. A list s is a suffix of l if p and l are the same from
+the end, up through all items in s. For example, (list "b" "c" "d") is
+a suffix of itself and (list "a" "b" "c" "d").
+|#
+
