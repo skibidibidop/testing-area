d49009960bc14314e65a179fc15fe22f9a456070
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-04 05:17:36 +0800

    Add ex 187 correct solution

--- htdp_2e_redo/ex_187/ex_187.rkt
+++ htdp_2e_redo/ex_187/ex_187.rkt
@@ -23,21 +23,22 @@ Hint Formulate a function that compares two elements of GamePlayer.
 
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
 
-; List-of-numbers -> Boolean
+; List-of-gp -> Boolean
 ; Checks if list lon is sorted in descending order
 (check-expect (sorted>? '()) #true)
-(check-expect (sorted>? (cons 1 '())) #true)
-(check-expect (sorted>? (cons 0 (cons 1 '()))) #false)
-(check-expect (sorted>? (cons 0 (cons 2 (cons 1 '())))) #false)
-(check-expect (sorted>? (cons 3 (cons 2 (cons 1 '())))) #true)
+(check-expect (sorted>? (list (make-gp "P1" 30)
+                              (make-gp "P2" 20)
+                              (make-gp "P3" 10))) #true)
+(check-expect (sorted>? (list (make-gp "P1" 20)
+                              (make-gp "P2" 30))) #false)
 
-(define (sorted>? lon)
+(define (sorted>? l)
   (cond
-    [(empty? lon) #true]
-    [(empty? (rest lon)) #true]
+    [(empty? l) #true]
+    [(empty? (rest l)) #true]
     [else
-     (and (> (first lon) (first (rest lon)))
-          (sorted>? (rest lon)))]))
+     (and (> (gp-score (first l)) (gp-score(first (rest l))))
+          (sorted>? (rest l)))]))
 
 ; List-of-gp -> List-of-gp
 ; produces a sorted version of l
@@ -62,11 +63,11 @@ Hint Formulate a function that compares two elements of GamePlayer.
     [(empty? l) '()]
     [(cons? l) (insert (first l) (sort> (rest l)))]))
 
-; Number List-of-numbers -> List-of-numbers
-; inserts n into the sorted list of numbers l
-(define (insert n l)
+; Gp List-of-gp -> List-of-gp
+; inserts agp into the sorted List-of-gp lgp
+(define (insert agp lgp)
   (cond
-    [(empty? l) (cons n '())]
-    [else (if (>= n (first l))
-              (cons n l)
-              (cons (first l) (insert n (rest l))))]))
+    [(empty? lgp) (cons agp '())]
+    [else (if (>= (gp-score agp) (gp-score (first lgp)))
+              (cons agp lgp)
+              (cons (first lgp) (insert agp (rest lgp))))]))
