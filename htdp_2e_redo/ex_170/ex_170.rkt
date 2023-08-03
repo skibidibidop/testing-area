45cf29b819577162e71edf997c96111ce3905f40
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-03 11:36:45 +0800

    Add ex 170 solution

--- htdp_2e_redo/ex_170/ex_170.rkt
+++ htdp_2e_redo/ex_170/ex_170.rkt
@@ -20,3 +20,23 @@ It replaces all occurrence of area code 713 with 281.
 ; A Three is a Number between 100 and 999.
 ; A Four is a Number between 1000 and 9999.
 
+; List-of-phone -> List-of-phone
+; Replaces all occurrence of area code 713 with 281
+(check-expect (replace '()) '())
+(check-expect (replace (list (make-phone 713 100 1000)
+                             (make-phone 101 400 5000)))
+              (list (make-phone 281 100 1000)
+                    (make-phone 101 400 5000)))
+
+(define (replace lop)
+  (cond
+    [(empty? lop) '()]
+    [else
+     (cons
+      (make-phone
+       (if (= (phone-area (first lop)) 713)
+           281
+           (phone-area (first lop)))
+       (phone-switch (first lop))
+       (phone-four (first lop)))
+      (replace (rest lop)))]))
