dbc459d70cca9af5c6401dceb76bd1496d0e0e72
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-03 11:14:44 +0800

    Add ex 168 description

---
+++ htdp_2e_redo/ex_168/ex_168.rkt
@@ -0,0 +1,11 @@
+#|
+Author: Mark Beltran
+Date: August 3, 2023
+
+Exercise 168. Design the function translate. It consumes and produces lists
+of Posns. For each (make-posn x y) in the former, the latter contains
+(make-posn x (+ y 1)). We borrow the word “translate” from geometry, where
+the movement of a point by a constant distance along a straight line is
+called a translation.
+|#
+
