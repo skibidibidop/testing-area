56b03165d66f78797ac40d8277700df6786a8b03
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-03 08:05:02 +0800

    Add ex 163 solution

--- htdp_2e_redo/ex_163/ex_163.rkt
+++ htdp_2e_redo/ex_163/ex_163.rkt
@@ -6,6 +6,9 @@ Exercise 163. Design convertFC. The function converts a list of measurements
 in Fahrenheit to a list of Celsius measurements.
 |#
 
+(define F_CONST1 32)
+(define F_CONST2 (/ 5 9))
+
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
 ; A List-of-F is one of:
@@ -25,5 +28,24 @@ in Fahrenheit to a list of Celsius measurements.
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
 
 ; List-of-F -> List-of-C
-;
-(define (converFC loF) '())
+; Converts a list of temperature readings in degrees Fahrenheit to a list
+; of temperature readings in degrees Celsius
+(check-expect (convertFC '()) '())
+(check-expect (convertFC (list 32)) (list 0))
+(check-expect (convertFC (list 500 320)) (list 260 160))
+
+(define (convertFC loF)
+  (cond
+    [(empty? loF) '()]
+    [else
+     (cons (FtoC (first loF))
+           (convertFC (rest loF)))]))
+
+; FTemp -> CTemp
+; Converts an FTemp to a CTemp
+(check-expect (FtoC 32) 0)
+(check-expect (FtoC 500) 260)
+
+(define (FtoC fah)
+  (* (- fah F_CONST1)
+     F_CONST2))
