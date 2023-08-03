d05bcc9f0c8923681ff9132a083871945c834666
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-04 05:09:32 +0800

    Add (sort>) revision

--- htdp_2e_redo/ex_187/ex_187.rkt
+++ htdp_2e_redo/ex_187/ex_187.rkt
@@ -25,7 +25,7 @@ Hint Formulate a function that compares two elements of GamePlayer.
 
 ; List-of-numbers -> Boolean
 ; Checks if list lon is sorted in descending order
-(check-error  (sorted>? '()) #true)
+(check-expect (sorted>? '()) #true)
 (check-expect (sorted>? (cons 1 '())) #true)
 (check-expect (sorted>? (cons 0 (cons 1 '()))) #false)
 (check-expect (sorted>? (cons 0 (cons 2 (cons 1 '())))) #false)
@@ -38,3 +38,35 @@ Hint Formulate a function that compares two elements of GamePlayer.
     [else
      (and (> (first lon) (first (rest lon)))
           (sorted>? (rest lon)))]))
+
+; List-of-gp -> List-of-gp
+; produces a sorted version of l
+(check-expect (sort> '()) '())
+(check-satisfied (sort> '()) sorted>?)
+(check-satisfied (sort> (list (make-gp "P1" 10)
+                              (make-gp "P2" 20)
+                              (make-gp "P3" 30)))
+                 sorted>?)
+(check-satisfied (sort> (list (make-gp "P1" 50)
+                              (make-gp "P2" 80)
+                              (make-gp "P3" 70)
+                              (make-gp "P4" 60)))
+                 sorted>?)
+(check-satisfied (sort> (list (make-gp "P1" 50)
+                              (make-gp "P2" 40)
+                              (make-gp "P3" 30)))
+                 sorted>?)
+
+(define (sort> l)
+  (cond
+    [(empty? l) '()]
+    [(cons? l) (insert (first l) (sort> (rest l)))]))
+
+; Number List-of-numbers -> List-of-numbers
+; inserts n into the sorted list of numbers l
+(define (insert n l)
+  (cond
+    [(empty? l) (cons n '())]
+    [else (if (>= n (first l))
+              (cons n l)
+              (cons (first l) (insert n (rest l))))]))
