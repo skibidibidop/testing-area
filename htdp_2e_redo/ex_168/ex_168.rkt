6b6a19e20fd15497c4b512c3177e0a3d241022c4
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-03 11:17:39 +0800

    Add ex 168 solution

--- htdp_2e_redo/ex_168/ex_168.rkt
+++ htdp_2e_redo/ex_168/ex_168.rkt
@@ -9,3 +9,21 @@ the movement of a point by a constant distance along a straight line is
 called a translation.
 |#
 
+; List-of-posn -> List-of-posn
+; Increases each y-coordinate in lop by 1
+(check-expect (translate '()) '())
+(check-expect (translate (list (make-posn 30 40)
+                               (make-posn 10 10)
+                               (make-posn 20 30)))
+              (list (make-posn 30 41)
+                    (make-posn 10 11)
+                    (make-posn 20 31)))
+
+(define (translate lop)
+  (cond
+    [(empty? lop) '()]
+    [else
+     (cons
+      (make-posn (posn-x (first lop))
+                 (+ (posn-y (first lop)) 1))
+      (translate (rest lop)))]))
