19f9443c194a4b97f11794cd76754c3a388db7db
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-05 09:06:52 +0800

    Add ex 234 description

---
+++ htdp_2e_redo/ex_234/ex_234.rkt
@@ -0,0 +1,16 @@
+#|
+Author: Mark Beltran
+Date: August 5, 2023
+
+Exercise 234. Create the function make-ranking, which consumes a
+list of ranked song titles and produces a list representation of an HTML
+table. Consider this example:
+
+(define one-list
+  '("Asia: Heat of the Moment"
+    "U2: One"
+    "The White Stripes: Seven Nation Army"))
+
+If you apply make-ranking to one-list and display the resulting web
+page in a browser, you see something like the screen shot in figure 85.
+|#
