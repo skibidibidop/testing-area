48dfd3783bfd7398a80e5abbbb766dc662c4faf2
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-05 10:14:15 +0800

    Add ex 236 solution

--- htdp_2e_redo/ex_236/ex_236.rkt
+++ htdp_2e_redo/ex_236/ex_236.rkt
@@ -40,7 +40,7 @@ revised definitions work properly. Finally, design a function that subtracts
       (+ (first l) 5)
       (plus5 (rest l)))]))
 
-; Number List -> List
+; Number List-of-number -> List-of-number
 ; Adds num n to all members of l
 (check-expect (add_num 1 '()) '())
 (check-expect (add_num 2 (list 0 1)) (list 2 3))
@@ -52,3 +52,42 @@ revised definitions work properly. Finally, design a function that subtracts
      (cons (+ n (first l))
            (add_num n (rest l)))]))
 
+; List-of-number -> List-of-number
+; Adds 1 to each item on l
+(check-expect (add_one '()) '())
+(check-expect (add_one (list 1 0)) (list 2 1))
+(check-expect (add_one (list 0 2 3 1)) (list 1 3 4 2))
+
+(define (add_one l)
+  (add_num 1 l))
+
+; List-of-number -> List-of-number
+; Adds 5 to each item on l
+(check-expect (add_5 '()) '())
+(check-expect (add_5 (list 1 0)) (list 6 5))
+(check-expect (add_5 (list 0 2 3 1)) (list 5 7 8 6))
+
+(define (add_5 l)
+  (add_num 5 l))
+
+; (Number -> Number) Number List-of-number -> List-of-number
+; Applies (op n) to all members of l
+(check-expect (operate + 3 '()) '())
+(check-expect (operate + 5 (list 4 3)) (list 9 8))
+(check-expect (operate * 2 (list 1 3)) (list 2 6))
+(check-expect (operate - 1 (list 1 0)) (list 0 -1))
+
+(define (operate op n l)
+  (cond
+    [(empty? l) '()]
+    [else
+     (cons
+      (op (first l) n)
+      (operate op n (rest l)))]))
+
+; List-of-number -> List-of-number
+(check-expect (sub_2 '()) '())
+(check-expect (sub_2 (list 1 2)) (list -1 0))
+
+(define (sub_2 l)
+  (operate - 2 l))
