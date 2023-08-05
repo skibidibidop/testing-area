17d6c748deaf2b4a1c8f9069a4eae75bd606299c
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-05 10:44:10 +0800

    Add list constants

--- htdp_2e_redo/ex_238/ex_238.rkt
+++ htdp_2e_redo/ex_238/ex_238.rkt
@@ -21,6 +21,11 @@ again, define inf-2 and sup-2, and test them with the same inputs again.
 Why are these versions so much faster?
 |#
 
+(define DESC_LIST
+  (list 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1))
+(define ASC_LIST
+  (list 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25))
+
 ; FUNCTIONS FROM FIGURE 89 /////////////////////////////////////////////////////
 
 ; Nelon -> Number
@@ -50,3 +55,4 @@ Why are these versions so much faster?
          (sup (rest l)))]))
 
 ; //////////////////////////////////////////////////////////////////////////////
+
