b53cd9af2c1c7b258e22d364aa3da5878cc0671d
skibidibidop <dev.mapb@gmail.com>
2023-08-08 07:43:18 +0800

    Add (function=at-1.2-3-and-5.775?) body

--- htdp_2e_redo/ex_245/ex_245.rkt
+++ htdp_2e_redo/ex_245/ex_245.rkt
@@ -18,7 +18,31 @@ not, explain why and consider the implication that you have encountered
 the first easily definable idea for which you cannot define a function.
 |#
 
+(define MOD 10)
+(define CONS_1 1.2)
+(define CONS_2 3)
+(define CONS_3 -5.775)
+
+(define (add10 x)
+  (+ x MOD))
+
+(define (sub10 x)
+  (- x MOD))
+
+(define (mult10 x)
+  (* x MOD))
+
 ; (Number -> Number) (Number -> Number) -> Boolean
 ; Determines if the 2 given functions return the same results given 1.2,
 ; 3, and -5.775.
-(define (function=at-1.2-3-and-5.775? fn_a fn_b) #false)
+(check-expect (function=at-1.2-3-and-5.775?
+               add10 sub10) #false)
+(check-expect (function=at-1.2-3-and-5.775?
+               sub10 mult10) #false)
+(check-expect (function=at-1.2-3-and-5.775?
+               add10 mult10) #false)
+
+(define (function=at-1.2-3-and-5.775? fn_a fn_b)
+  (and (= (fn_a CONS_1) (fn_b CONS_1))
+       (= (fn_a CONS_2) (fn_b CONS_2))
+       (= (fn_a CONS_3) (fn_b CONS_3))))
