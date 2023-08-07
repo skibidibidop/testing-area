edd789c31b0353844f78984696a57e9896ee6ac2
skibidibidop <dev.mapb@gmail.com>
2023-08-07 18:25:14 +0800

    Add ex 242 solution

--- htdp_2e_redo/ex_242/ex_242.rkt
+++ htdp_2e_redo/ex_242/ex_242.rkt
@@ -14,9 +14,28 @@ Exercise 242. Here is one more parametric data definition:
 Interpret these data definitions: [Maybe String], [Maybe [List-of String]],
 and [List-of [Maybe String]].
 
-What does the following function signature mean:
+Exercise continued from line 29
+|#
+
+; [Maybe String] is one of:
+; #false
+; String
+
+; [Maybe [List-of String]] is one of:
+; #false
+; [List-of String]
+
+; [List-of [Maybe String]] is one of:
+; '()
+; (cons [Maybe String] [List-of [Maybe String]])
+
+; What does the following function signature mean:
 
 ; String [List-of String] -> [Maybe [List-of String]]
+
+; The function consumes a String and a [List-of String] and
+; returns either #false or [List-of String]
+
 ; returns the remainder of los starting with s
 ; #false otherwise
 (check-expect (occurs "a" (list "b" "a" "d" "e"))
@@ -26,5 +45,5 @@ What does the following function signature mean:
 (define (occurs s los)
   los)
 
-Work through the remaining steps of the design recipe.
-|#
+; Work through the remaining steps of the design recipe.
+
