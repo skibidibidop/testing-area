42b0ba324e1597dbdc551729189f73db163d148e
skibidibidop <dev.mapb@gmail.com>
2023-08-08 07:20:41 +0800

    Add ex 244 description

---
+++ htdp_2e_redo/ex_244/ex_244.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex_244) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: August 8, 2023
+
+Exercise 244. Argue why the following sentences are now legal:
+
+1. (define (f x) (x 10))
+2. (define (f x) (x f))
+3. (define (f x y) (x 'a y 'b))
+
+Explain your reasoning.
+|#
+
