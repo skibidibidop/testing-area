115cc74e1f4ff2569aa1f9ce95c01a48b207d597
skibidibidop <dev.mapb@gmail.com>
2023-08-07 16:23:07 +0800

    Add ex 241 solution

--- htdp_2e_redo/ex_241/ex_241.rkt
+++ htdp_2e_redo/ex_241/ex_241.rkt
@@ -8,3 +8,22 @@ Date: August 7, 2023
 Exercise 241. Compare the definitions for NEList-of-temperatures and
 NEList-of-Booleans. Then formulate an abstract data definition NEList-of.
 |#
+
+; An NEList-of-temperatures is one of:
+; -- (cons CTemperature '())
+; -- (cons CTemperature NEList-of-temperatures)
+; interpretation non-empty lists of Celsius temperatures
+
+; A CTemperature is a Number greater than -272.
+
+; An NEList-of-Booleans is one of:
+; -- (cons Boolean '())
+; -- (cons Boolean NEList-of-Booleans)
+; interpretation non-empty lists of Booleans
+
+; Abstraction
+
+; An [NEList-of ITEM] is one of:
+; (cons ITEM '())
+; (cons ITEM [NEList-of ITEM])
+; Interp.: non-empty list of ITEM
