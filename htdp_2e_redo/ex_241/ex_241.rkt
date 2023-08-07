f7636b41c98253ac7dfdef0c8367ef684915eb5f
skibidibidop <dev.mapb@gmail.com>
2023-08-07 16:14:41 +0800

    Add ex 241 description

---
+++ htdp_2e_redo/ex_241/ex_241.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex_241) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: August 7, 2023
+
+Exercise 241. Compare the definitions for NEList-of-temperatures and
+NEList-of-Booleans. Then formulate an abstract data definition NEList-of.
+|#
