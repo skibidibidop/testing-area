17b911e159457aa6d3f2e9d03d8c604a399161ae
skibidibidop <dev.mapb@gmail.com>
2023-08-08 09:44:16 +0800

    Add ex 253 description

--- htdp_2e_redo/ex_253/ex_253.rkt
+++ htdp_2e_redo/ex_253/ex_253.rkt
@@ -1,3 +1,17 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex_253) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: August 8, 2023
+
+Exercise 253. Each of these signatures describes a class of functions:
+
+; [Number -> Boolean]
+; [Boolean String -> Boolean]
+; [Number Number Number -> Number]
+; [Number -> [List-of Number]]
+; [[List-of Number] -> Boolean]
+
+Describe these collections with at least one example from ISL.
+|#
