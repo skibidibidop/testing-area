9546c749f2d1bdaf53d5c7967c8d1b1f5972d8cd
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-09 06:19:25 +0800

    Add ex 261 solution

--- htdp_2e_redo/ex_261/ex_261.rkt
+++ htdp_2e_redo/ex_261/ex_261.rkt
@@ -9,6 +9,28 @@ this help increase the speed at which the function computes its result?
 Significantly? A little bit? Not at all?
 |#
 
+(define-struct ir [name price])
+; (make-ir String Number)
+; Interp.: (make-ir n p), represents an inventory report which contains an
+; item's name (n) and price (p)
+
+; An Inventory is one of:
+; '()
+; (cons IR Inventory)
+
+(define IR1 (list (make-ir "Item1" 30)
+                  (make-ir "Item2" 10)
+                  (make-ir "Item3" 40)
+                  (make-ir "Item4" 50)
+                  (make-ir "Item5" 0.5)
+                  (make-ir "Item6" 0.3)
+                  (make-ir "Item7" 10)
+                  (make-ir "Item8" 0.1)
+                  (make-ir "Item9" 0.2)
+                  (make-ir "Item10" 0.7)
+                  (make-ir "Item11" 20)))
+
+
 ; Function from figure 101:
 
 ; Inventory -> Inventory
@@ -23,3 +45,16 @@ Significantly? A little bit? Not at all?
         (cons (first an-inv) (extract1 (rest an-inv)))]
        [else (extract1 (rest an-inv))])]))
 
+(define (extract2 an-inv)
+  (cond
+    [(empty? an-inv) '()]
+    [else
+     (local ((define first-of-inv (first an-inv))
+             (define extracted (extract2 (rest an-inv))))
+       (cond
+         [(<= (ir-price first-of-inv) 1.0)
+          (cons first-of-inv extracted)]
+         [else extracted]))]))
+
+; Running (time (extract1 IR1)) and (time (extract2 IR1)) produce the
+; same result
