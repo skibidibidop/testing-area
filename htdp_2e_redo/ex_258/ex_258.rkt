d77f5ec51a6ff7b40fe944a11fda0c8b7f4bdce9
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-09 05:12:39 +0800

    Update ex_258.rkt

--- htdp_2e_redo/ex_258/ex_258.rkt
+++ htdp_2e_redo/ex_258/ex_258.rkt
@@ -5,5 +5,33 @@ Date: August 8, 2023
 Exercise 258. Use a local expression to organize the functions for
 drawing a polygon in figure 73. If a globally defined function is widely
 useful, do not make it local.
-|#
 
+Functions from figure 73:
+
+; Image Polygon -> Image
+; adds an image of p to MT
+(define (render-polygon img p)
+  (render-line (connect-dots img p) (first p) (last p)))
+
+; Image NELoP -> Image
+; connects the Posns in p in an image
+(define (connect-dots img p)
+  (cond
+    [(empty? (rest p)) MT]
+    [else (render-line (connect-dots img (rest p))
+                       (first p)
+                       (second p))]))
+
+; Image Posn Posn -> Image
+; draws a red line from Posn p to Posn q into im
+(define (render-line im p q)
+  (scene+line
+   im (posn-x p) (posn-y p) (posn-x q) (posn-y q) "red"))
+
+; Polygon -> Posn
+; extracts the last item from p
+(define (last p)
+  (cond
+    [(empty? (rest (rest (rest p)))) (third p)]
+    [else (last (rest p))]))
+|#
