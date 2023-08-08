562b81186d6fcdab8c3c30f608a7c6d4ca9a63a2
skibidibidop <dev.mapb@gmail.com>
2023-08-08 08:04:53 +0800

    Add ex 247 solution

---
+++ htdp_2e_redo/ex_247/ex_247.rkt
@@ -0,0 +1,24 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex_247) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: August 8, 2023
+
+Exercise 247. Evaluate (extract < (cons 8 (cons 4 '())) 5)
+with DrRacket’s stepper.
+|#
+
+(define (extract R l t)
+  (cond
+    [(empty? l) '()]
+    [else (cond
+            [(R (first l) t)
+             (cons (first l)
+                   (extract R (rest l) t))]
+            [else
+             (extract R (rest l) t)])]))
+
+(extract < (cons 8 (cons 4 '())) 5)
+
+; The result is (list 4)
