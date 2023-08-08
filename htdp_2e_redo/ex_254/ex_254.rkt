df14ca31e1ff5572e741ca64cafec3917a7c5ff5
skibidibidop <dev.mapb@gmail.com>
2023-08-08 09:52:28 +0800

    Add ex 254 description

---
+++ htdp_2e_redo/ex_254/ex_254.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex_254) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: August 8, 2023
+
+Exercise 254. Formulate signatures for the following functions:
+
+sort-n, which consumes a list of numbers and a function that con-
+sumes two numbers (from the list) and produces a Boolean; sort-n
+produces a sorted list of numbers.
+
+sort-s, which consumes a list of strings and a function that con-
+sumes two strings (from the list) and produces a Boolean; sort-s
+produces a sorted list of strings.
+
+Then abstract over the two signatures, following the above steps. Also
+show that the generalized signature can be instantiated to describe the sig-
+nature of a sort function for lists of IRs.
+|#
