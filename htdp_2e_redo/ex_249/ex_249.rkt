2e05995bbefc565c2a5b1666b62bafc73d0b22f4
skibidibidop <dev.mapb@gmail.com>
2023-08-08 08:34:16 +0800

    Add ex 249 description

---
+++ htdp_2e_redo/ex_249/ex_249.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex_249) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: August 8, 2023
+
+Exercise 249. Functions are values: arguments, results, items in lists.
+Place the following definitions and expressions into DrRacket’s definitions
+window and use the stepper to find out how running this program works:
+
+(define (f x) x)
+(cons f '())
+(f f)
+(cons f (cons 10 (cons (f 10) '())))
+
+The stepper displays functions as lambda expressions; see chapter 17.
+|#
