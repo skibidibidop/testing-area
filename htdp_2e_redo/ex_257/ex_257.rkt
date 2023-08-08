931b0c096327ef92289df2c0785ebfdf0104d2bb
skibidibidop <dev.mapb@gmail.com>
2023-08-08 16:13:59 +0800

    Add ex 257 description

--- htdp_2e_redo/ex_257/ex_257.rkt
+++ htdp_2e_redo/ex_257/ex_257.rkt
@@ -1,3 +1,28 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex_257) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: August 8, 2023
+
+Exercise 257. You can design build-list and foldl with the design
+recipes that you know, but they are not going to be like the ones that ISL
+provides. For example, the design of your own foldl function requires a
+use of the list reverse function:
+
+(see below)
+
+Design build-l*st, which works just like build-list. Hint Recall
+the add-at-end function from exercise 193. Note on Design Part VI cov-
+ers the concepts needed to design these functions from scratch.
+|#
+
+; [X Y] [X Y -> Y] Y [List-of X] -> Y
+; f*oldl works just like foldl
+(check-expect (f*oldl cons '() '(a b c))
+              (foldl cons '() '(a b c)))
+(check-expect (f*oldl / 1 '(6 3 2))
+              (foldl / 1 '(6 3 2)))
+
+(define (f*oldl f e l)
+  (foldr f e (reverse l)))
