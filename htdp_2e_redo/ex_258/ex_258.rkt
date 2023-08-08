fb021900ff85577b9e29ecfec4470b1fb32725ce
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-09 05:29:30 +0800

    Add ex 258 solution

--- htdp_2e_redo/ex_258/ex_258.rkt
+++ htdp_2e_redo/ex_258/ex_258.rkt
@@ -35,3 +35,48 @@ Functions from figure 73:
     [(empty? (rest (rest (rest p)))) (third p)]
     [else (last (rest p))]))
 |#
+
+(require 2htdp/image)
+
+(define MT (empty-scene 100 100))
+
+(define triangle-p (list (make-posn 20 10)
+                         (make-posn 20 20)
+                         (make-posn 30 20)))
+(define square-p (list (make-posn 10 10)
+                       (make-posn 20 10)
+                       (make-posn 20 20)
+                       (make-posn 10 20)))
+
+(define (make-polygon img p)
+  (local
+    (; Image NELoP -> Image
+     ; connects the Posns in p in an image
+     (define (connect-dots img p)
+       (cond
+         [(empty? (rest p)) MT]
+         [else (render-line (connect-dots img (rest p))
+                            (first p)
+                            (second p))]))
+
+     ; Image Posn Posn -> Image
+     ; draws a red line from Posn p to Posn q into im
+     (define (render-line im p q)
+       (scene+line
+        im (posn-x p) (posn-y p) (posn-x q) (posn-y q) "red"))
+
+     ; Polygon -> Posn
+     ; extracts the last item from p
+     (define (last p)
+       (cond
+         [(empty? (rest (rest (rest p)))) (third p)]
+         [else (last (rest p))]))
+
+     ; Image Polygon -> Image
+     ; adds an image of p to MT
+     (define (render-polygon img p)
+       (render-line (connect-dots img p) (first p) (last p))))
+    (render-polygon img p)))
+
+(make-polygon MT triangle-p)
+          
