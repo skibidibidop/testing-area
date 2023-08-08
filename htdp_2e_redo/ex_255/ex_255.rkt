befcb8c0ef9e8eef8328dbc2d05e48dcb3b55648
skibidibidop <dev.mapb@gmail.com>
2023-08-08 14:22:22 +0800

    Add ex 255 description

---
+++ htdp_2e_redo/ex_255/ex_255.rkt
@@ -0,0 +1,19 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex_255) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: August 8, 2023
+
+Exercise 255. Formulate signatures for the following functions:
+
+map-n, which consumes a list of numbers and a function from num-
+bers to numbers to produce a list of numbers
+
+map-s, which consumes a list of strings and a function from strings
+to strings and produces a list of strings.
+
+Then abstract over the two signatures, following the above steps. Also
+show that the generalized signature can be instantiated to describe the sig-
+nature of the map1 function above.
+|#
