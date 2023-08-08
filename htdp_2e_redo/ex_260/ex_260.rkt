b727c3e187962d9a63de4176adea2b05bd02c982
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-09 05:51:54 +0800

    Add ex 260 solution

--- htdp_2e_redo/ex_260/ex_260.rkt
+++ htdp_2e_redo/ex_260/ex_260.rkt
@@ -5,3 +5,24 @@ Date: August 8, 2023
 Exercise 260. Confirm the insight about the performance of inf.v2 by
 repeating the performance experiment of exercise 238.
 |#
+
+(define DESC_LIST
+  (list 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1))
+(define ASC_LIST
+  (list 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25))
+
+; Nelon -> Number
+; determines the smallest number on l
+(define (inf.v2 l)
+  (cond
+    [(empty? (rest l)) (first l)]
+    [else
+     (local ((define smallest-in-rest (inf.v2 (rest l))))
+       (if (< (first l) smallest-in-rest)
+           (first l)
+           smallest-in-rest))]))
+
+(inf.v2 DESC_LIST)
+(inf.v2 ASC_LIST)
+
+; wow so fast
