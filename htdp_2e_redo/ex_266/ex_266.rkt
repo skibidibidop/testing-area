10c79025c833d302129f0fb0c54d415d615fda7e
skibidibidop <dev.mapb@gmail.com>
2023-08-09 17:07:14 +0800

    Update ex 266

---
+++ htdp_2e_redo/ex_266/ex_266.rkt
@@ -0,0 +1,29 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex_266) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: August 9, 2023
+
+Exercise 266. Use DrRacket’s stepper to find out how ISL evaluates
+
+((local ((define (f x) (+ x 3))
+         (define (g x) (* x 4)))
+   (if (odd? (f (g 1)))
+       f
+       g))
+ 2)
+
+to 5.
+|#
+
+((local ((define (f x) (+ x 3))
+         (define (g x) (* x 4)))
+   (if (odd? (f (g 1)))
+       f
+       g))
+ 2)
+
+; wtf is this number doing here????
+; why the fuck is this example causing an error?????
+
