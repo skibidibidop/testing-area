8801d7bf99d18e0a04e2bad9585b195d9ed63e01
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-10 06:57:29 +0800

    Add ex 268 description

---
+++ htdp_2e_redo/ex_268/ex_268.rkt
@@ -0,0 +1,10 @@
+#|
+Author: Mark Beltran
+Date: August 10, 2023
+
+Exercise 268. An inventory record specifies the name of an item, a description,
+the acquisition price, and the recommended sales price.
+
+Define a function that sorts a list of inventory records by the difference
+between the two prices.
+|#
