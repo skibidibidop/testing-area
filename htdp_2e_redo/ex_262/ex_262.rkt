858899bc4fee56a13c1ecd8e84f881a1a60868c0
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-09 09:42:10 +0800

    Add ex 262 description

---
+++ htdp_2e_redo/ex_262/ex_262.rkt
@@ -0,0 +1,14 @@
+#|
+Author: Mark Beltran
+Date: August 9, 2023
+
+Exercise 262. Design the function identityM, which creates diagonal
+squares of 0s and 1s:
+
+> (identityM 1)
+(list (list 1))
+> (identityM 3)
+(list (list 1 0 0) (list 0 1 0) (list 0 0 1))
+
+Use the structural design recipe and exploit the power of local.
+|#
