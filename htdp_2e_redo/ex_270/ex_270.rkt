dc870f017f3838461c30dfcaf904a3dc1426d43b
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-10 10:56:55 +0800

    Add ex 270 description

---
+++ htdp_2e_redo/ex_270/ex_270.rkt
@@ -0,0 +1,14 @@
+#|
+Author: Mark Beltran
+Date: August 10, 2023
+
+Exercise 270. Use build-list to define a function that
+
+1. creates the list (list 0 ... (- n 1)) for any natural number n;
+2. creates the list (list 1 ... n) for any natural number n;
+3. creates the list (list 1 1/2 ... 1/n) for any natural number n;
+4. creates the list of the first n even numbers; and
+5. creates a diagonal square of 0s and 1s; see exercise 262.
+
+Finally, define tabulate from exercise 250 using build-list.
+|#
