5157c084b625f16c676854665c617c134aa7401e
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-11 04:59:51 +0800

    Add ex 273 description

---
+++ htdp_2e_redo/ex_273/ex_273.rkt
@@ -0,0 +1,8 @@
+#|
+Author: Mark Beltran
+Date: August 10, 2023
+
+Exercise 273. The fold functions are so powerful that you can define
+almost any list processing functions with them. Use fold to define map.
+|#
+
