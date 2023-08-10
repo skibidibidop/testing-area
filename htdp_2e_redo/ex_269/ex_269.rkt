6015b71093e143fb4d99482cfa5ac7ecb91d0673
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-10 09:22:24 +0800

    Add ex 269 description

---
+++ htdp_2e_redo/ex_269/ex_269.rkt
@@ -0,0 +1,16 @@
+#|
+Author: Mark Beltran
+Date: August 10, 2023
+
+Exercise 269. Define eliminate-expensive. The function consumes
+a number, ua, and a list of inventory records, and it produces a list of all
+those structures whose sales price is below ua.
+
+Then use filter to define recall, which consumes the name of an inventory item,
+called ty, and a list of inventory records and which produces a list of
+inventory records that do not use the name ty.
+
+In addition, define selection, which consumes two lists of names and
+selects all those from the second one that are also on the first.
+|#
+
