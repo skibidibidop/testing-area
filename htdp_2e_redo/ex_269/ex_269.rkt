7b21c1990db378c7788aa69214783b62bc80956a
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-10 09:32:46 +0800

    Add (eliminate-expensive)

--- htdp_2e_redo/ex_269/ex_269.rkt
+++ htdp_2e_redo/ex_269/ex_269.rkt
@@ -14,3 +14,33 @@ In addition, define selection, which consumes two lists of names and
 selects all those from the second one that are also on the first.
 |#
 
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+(define-struct ir [name desc buy sell])
+; (make-ir String String Number Number)
+; Interp.: (make-ir n d b s), an inventory record containing an item's name (n),
+; description (d), acquisition price (b), and suggested retail price (s)
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+(define IR_LIST
+  (list (make-ir "Item1" "#1" 10 20)
+        (make-ir "Item2" "#2" 40 200)
+        (make-ir "Item3" "#3" 3 10)
+        (make-ir "Item4" "#4" 50 70)))
+
+; Number [List-of Ir] -> [List-of Ir]
+; Creates a list of Irs whose sale price is below Number ua
+(check-expect (eliminate-expensive 3 IR_LIST) '())
+(check-expect (eliminate-expensive 100 '()) '())
+(check-expect (eliminate-expensive 60 IR_LIST)
+              (list (make-ir "Item1" "#1" 10 20)
+                    (make-ir "Item3" "#3" 3 10)))
+(check-expect (eliminate-expensive 300 IR_LIST) IR_LIST)
+
+(define (eliminate-expensive ua lir)
+  (local
+    [; Ir -> Boolean
+     (define (in_range? an-ir)
+       (< (ir-sell an-ir) ua))]
+    (filter in_range? lir)))
