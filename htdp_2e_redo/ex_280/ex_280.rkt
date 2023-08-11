74513e03ffc734ece638e462f7e1dcc60d0e724d
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-12 04:16:51 +0800

    Add ex 280 solution

--- htdp_2e_redo/ex_280/ex_280.rkt
+++ htdp_2e_redo/ex_280/ex_280.rkt
@@ -22,3 +22,24 @@ Exercise 280. Calculate the result of the following expressions:
 
 Check your results in DrRacket.
 |#
+
+(check-expect ((lambda (x y) (+ x (* x y)))
+               1 2)
+              3)
+
+(check-expect ((lambda (x y)
+                 (+ x
+                    (local ((define z (* y y)))
+                      (+ (* 3 z) (/ 1 x)))))
+               1 2)
+              14)
+
+(check-expect ((lambda (x y)
+                 (+ x
+                    ((lambda (z)
+                       (+ (* 3 z) (/ 1 z)))
+                     (* y y))))
+               1 2)
+              13.25)
+
+              
