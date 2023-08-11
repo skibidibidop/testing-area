b3dbbc50c38970076668013311caa466c74ed705
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-12 05:24:27 +0800

    Add ex 282 description

---
+++ htdp_2e_redo/ex_282/ex_282.rkt
@@ -0,0 +1,19 @@
+#|
+Author: Mark Beltran
+Date: August 12, 2023
+
+Exercise 282. Experiment with the above definitions in DrRacket.
+
+Also add
+
+; Number -> Boolean
+(define (compare x)
+  (= (f-plain x) (f-lambda x)))
+
+to the definitions area after renaming the left-hand f to f-plain and the
+right-hand one to f-lambda. Then run
+
+(compare (random 100000))
+
+a few times to make sure the two functions agree on all kinds of inputs.
+|#
