9028b56bc014371702f34a64c9a2aa98570de366
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-11 08:38:24 +0800

    Add (gen_prefixes) to wish list

--- htdp_2e_redo/ex_274/ex_274.rkt
+++ htdp_2e_redo/ex_274/ex_274.rkt
@@ -7,3 +7,17 @@ suffixes functions from exercise 190. Ensure that they pass the same
 tests as the original function.
 |#
 
+; [List-of 1String] -> [List-of [List-of 1String]]
+; Generatets a list of all prefixes of list l
+(check-expect (gen_prefixes '()) '())
+(check-expect (gen_prefixes (list "a"))
+              (list (list "a")))
+(check-expect (gen_prefixes (list "a" "b"))
+              (list (list "a")
+                    (list "a" "b")))
+(check-expect (gen_prefixes (list "a" "b" "c"))
+              (list (list "a")
+                    (list "a" "b")
+                    (list "a" "b" "c")))
+
+(define (gen_prefixes l) '())
