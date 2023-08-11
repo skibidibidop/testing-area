3fbcd25345e0735e8622ec2e7ce02a4ef0ae1c65
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-12 04:17:34 +0800

    Add ex 281 description

---
+++ htdp_2e_redo/ex_281/ex_281.rkt
@@ -0,0 +1,14 @@
+#|
+Author: Mark Beltran
+Date: August 12, 2023
+
+Exercise 281. Write down a lambda expression that
+
+1. consumes a number and decides whether it is less than 10;
+2. multiplies two given numbers and turns the result into a string;
+3. consumes a natural number and returns 0 for evens and 1 for odds;
+4. consumes two inventory records and compares them by price; and
+5. adds a red dot at a given Posn to a given Image.
+
+Demonstrate how to use these functions in the interactions area.
+|#
