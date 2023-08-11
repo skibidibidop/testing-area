aeac35c2e11ce51566ea079696fec6c6bb0a26a4
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-12 05:10:12 +0800

    Add ex 281 solution

--- htdp_2e_redo/ex_281/ex_281.rkt
+++ htdp_2e_redo/ex_281/ex_281.rkt
@@ -12,3 +12,41 @@ Exercise 281. Write down a lambda expression that
 
 Demonstrate how to use these functions in the interactions area.
 |#
+
+(require 2htdp/image)
+
+; 1. consumes a number and decides whether it is less than 10;
+((lambda (a) (< a 10))
+ 13)
+
+; 2. multiplies two given numbers and turns the result into a string;
+((lambda (a b)
+   (number->string (* a b)))
+ 3 4)
+
+; 3. consumes a natural number and returns 0 for evens and 1 for odds;
+((lambda (a)
+   (modulo a 2))
+ 1345)
+
+; 4. consumes two inventory records and compares them by price; and
+(define-struct ir [name price])
+
+; Ir Ir -> Boolean
+; #true if a's ir-price > b's ir-price, false otherwise
+((lambda (a b)
+   (> (ir-price a) (ir-price b)))
+ (make-ir "Item1" 100)
+ (make-ir "Item2" 200))
+
+; 5. adds a red dot at a given Posn to a given Image.
+(define RED_DOT (circle 10 "solid" "red"))
+(define BG (empty-scene 100 100))
+
+; Posn Image -> Image
+((lambda (coord bg)
+   (place-image RED_DOT
+                (posn-x coord) (posn-y coord)
+                bg))
+ (make-posn 30 40)
+ BG)
