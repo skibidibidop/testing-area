a6c42195f1a48a34958d1b5a07c9b94966b04fcc
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-11 08:11:44 +0800

    Add ex 273 solution

--- htdp_2e_redo/ex_273/ex_273.rkt
+++ htdp_2e_redo/ex_273/ex_273.rkt
@@ -6,3 +6,16 @@ Exercise 273. The fold functions are so powerful that you can define
 almost any list processing functions with them. Use fold to define map.
 |#
 
+; (X -> Y) [List-of X] -> [List-of Y]
+; Define the function map using fold
+(check-expect (own_map add1 '()) '())
+(check-expect (own_map add1 (list 0 1 2))
+              (list 1 2 3))
+
+(define (own_map f l)
+  (local
+    [(define (applicator i b)
+       (if (empty? i)
+           '()
+           (cons (f i) b)))]
+    (foldr applicator '() l)))
