c8855f968c78498615b671f87ac887a4a19ef111
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-12 07:08:37 +0800

    Add (convert_to_euro)

--- htdp_2e_redo/ex_285/ex_285.rkt
+++ htdp_2e_redo/ex_285/ex_285.rkt
@@ -12,3 +12,27 @@ measurements to a list of Celsius measurements.
 Finally, try your hand at translate, a function that translates a list of
 Posns into a list of lists of pairs of numbers.
 |#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; A USD is an amount in US Dollars
+
+; An EUR is an amount in Euros
+
+(define USD_TO_EUR 1.6)
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; [List-of USD] -> [List-of EUR]
+; Converts a list of USD amounts to a list of EUR amounts
+(check-expect (convert_to_euro '()) '())
+(check-expect (convert_to_euro (list 10 20 30))
+              (list (* USD_TO_EUR 10)
+                    (* USD_TO_EUR 20)
+                    (* USD_TO_EUR 30)))
+
+(define (convert_to_euro lusd)
+  (map
+   (lambda (a_usd)
+     (* USD_TO_EUR a_usd))
+   lusd))
