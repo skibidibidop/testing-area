859b6f0f71b186eea483f9884fb42e74a15993ba
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-12 06:43:13 +0800

    Add ex 285 description

---
+++ htdp_2e_redo/ex_285/ex_285.rkt
@@ -0,0 +1,14 @@
+#|
+Author: Mark Beltran
+Date: August 12, 2023
+
+Exercise 285. Use map to define the function convert-euro, which
+converts a list of US$ amounts into a list of EUR amounts based on an
+exchange rate of US$1.06 per EUR.
+
+Also use map to define convertFC, which converts a list of Fahrenheit
+measurements to a list of Celsius measurements.
+
+Finally, try your hand at translate, a function that translates a list of
+Posns into a list of lists of pairs of numbers.
+|#
