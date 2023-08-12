e715cab65a30f2748243a8bd6460a6abb30496ee
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-12 09:26:27 +0800

    Add (list_from_zero)

--- htdp_2e_redo/ex_288/ex_288.rkt
+++ htdp_2e_redo/ex_288/ex_288.rkt
@@ -12,3 +12,13 @@ Exercise 288. Use build-list and lambda to define a function that
 
 Also define tabulate with lambda.
 |#
+
+; 1. creates the list (list 0 ... (- n 1)) for any natural number n;
+(check-expect (list_from_zero 0) '())
+(check-expect (list_from_zero 3) (list 0 1 2))
+
+; Number -> [List-of Number]
+(define (list_from_zero n)
+  (build-list
+   n
+   (lambda (x) (identity x))))
