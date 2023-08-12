9d5368d678ee5b427ab8bf70b4f1bbf7ced5b249
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-12 09:11:16 +0800

    Add ex 288 description

---
+++ htdp_2e_redo/ex_288/ex_288.rkt
@@ -0,0 +1,14 @@
+#|
+Author: Mark Beltran
+Date: August 12, 2023
+
+Exercise 288. Use build-list and lambda to define a function that
+
+1. creates the list (list 0 ... (- n 1)) for any natural number n;
+2. creates the list (list 1 ... n) for any natural number n;
+3. creates the list (list 1 1/2 ... 1/n) for any natural number n;
+4. creates the list of the first n even numbers; and
+5. creates a diagonal square of 0s and 1s; see exercise 262.
+
+Also define tabulate with lambda.
+|#
