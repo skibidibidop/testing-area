ccff903a04e35964d2b4f07b8b7b060766f40dd3
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-12 08:52:53 +0800

    Add ex 287 solution

--- htdp_2e_redo/ex_287/ex_287.rkt
+++ htdp_2e_redo/ex_287/ex_287.rkt
@@ -8,3 +8,27 @@ and price), and it produces a list of all those structures whose acquisition
 price is below ua.
 |#
 
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+(define-struct ir [name price])
+; An InventoryRecord is a structure
+; (make-ir String Number)
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; Number [List-of InventoryRecord]
+; Creates a list of InventoryRecord of containing items with prices below ua
+(check-expect (eliminate-exp 10 '()) '())
+(check-expect (eliminate-exp 20 (list (make-ir "Item1" 100)
+                                      (make-ir "Item2" 10)
+                                      (make-ir "Item3" 50)
+                                      (make-ir "Item4" 20)
+                                      (make-ir "Item5" 5)))
+              (list (make-ir "Item2" 10)
+                    (make-ir "Item5" 5)))
+
+(define (eliminate-exp ua lir)
+  (filter
+   (lambda (an_ir)
+     (< (ir-price an_ir) ua))
+   lir))
