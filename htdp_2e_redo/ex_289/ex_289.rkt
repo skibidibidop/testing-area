f5f43e6ed12ff4d074c671f8f4febb588a0bf6a0
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-12 10:11:59 +0800

    Add (find-name)

--- htdp_2e_redo/ex_289/ex_289.rkt
+++ htdp_2e_redo/ex_289/ex_289.rkt
@@ -13,3 +13,16 @@ Should you use ormap or andmap to define a function that ensures that
 no name on some list exceeds some given width?
 |#
 
+; String [List-of String] -> Boolean
+; Checks if any of the names on the list of Strings provided are equal
+; to or an extension of n
+(check-expect (find-name "hi" '()) #false)
+(check-expect (find-name "hi" (list "hi there" "hey")) #true)
+(check-expect (find-name "hey" (list "hi" "hello" "there")) #false)
+
+(define (find-name n ln)
+  (ormap
+   (lambda (str_from_lstr)
+     (string-contains? n str_from_lstr))
+   ln))
+
