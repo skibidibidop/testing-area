248305937c780bcbc0092e4e9e7baee0981f50c3
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-12 09:56:54 +0800

    Add ex 289 description

---
+++ htdp_2e_redo/ex_289/ex_289.rkt
@@ -0,0 +1,15 @@
+#|
+Author: Mark Beltran
+Date: August 12, 2023
+
+Exercise 289. Use ormap to define find-name. The function consumes
+a name and a list of names. It determines whether any of the names on the
+latter are equal to or an extension of the former.
+
+With andmap you can define a function that checks all names on a list
+of names that start with the letter "a".
+
+Should you use ormap or andmap to define a function that ensures that
+no name on some list exceeds some given width?
+|#
+
