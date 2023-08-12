020e7a8c94ed2d88ed22c001e0da57c3d1c5f3ac
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-12 08:38:45 +0800

    Add ex 287 description

---
+++ htdp_2e_redo/ex_287/ex_287.rkt
@@ -0,0 +1,10 @@
+#|
+Author: Mark Beltran
+Date: August 12, 2023
+
+Exercise 287. Use filter to define eliminate-exp. The function
+consumes a number, ua, and a list of inventory records (containing name
+and price), and it produces a list of all those structures whose acquisition
+price is below ua.
+|#
+
