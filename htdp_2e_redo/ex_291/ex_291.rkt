8d5eac02ac8eb2cb7429b50842bb74ba2f9f0ed0
skibidibidop <dev.mapb@gmail.com>
2023-08-14 16:35:55 +0800

    Add ex 291 solution

--- htdp_2e_redo/ex_291/ex_291.rkt
+++ htdp_2e_redo/ex_291/ex_291.rkt
@@ -9,3 +9,19 @@ Exercise 291. The fold functions are so powerful that you can define
 almost any list-processing functions with them. Use fold to define map-
 via-fold, which simulates map.
 |#
+
+; (X -> Y) [List-of X] -> [List-of Y]
+; Define the function map using fold
+(check-expect (map-via-fold add1 '()) '())
+(check-expect (map-via-fold add1 (list 0 1)) (list 1 2))
+(check-expect (map-via-fold (lambda (n) (* n 3)) (list 0 1 2))
+              (list 0 3 6))
+
+(define (map-via-fold f l)
+  (foldr
+   (lambda (a_member b)
+     (if (empty? a_member)
+         '()
+         (cons (f a_member) b)))
+   '()
+   l))
