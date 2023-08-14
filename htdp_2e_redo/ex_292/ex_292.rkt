9c6027bf1b395140641e684d9cb2e9903534bb49
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-15 05:32:16 +0800

    Add ex 292 solution

--- htdp_2e_redo/ex_292/ex_292.rkt
+++ htdp_2e_redo/ex_292/ex_292.rkt
@@ -10,12 +10,17 @@ signature and purpose statement:
 The wish list even includes examples.
 |#
 
+(define ERR_MSG "Needs non-empty list.")
+
 ; [X X -> Boolean] [NEList-of X] -> Boolean
 ; determines whether l is sorted according to cmp
 (check-expect (sorted? < '(1 2 3)) #true)
 (check-expect (sorted? < '(2 1 3)) #false)
 
 (define (sorted? cmp l)
-  #false)
-
-
+ (cond
+   [(empty? l) (error ERR_MSG)]
+   [(empty? (rest l)) #true]
+   [else
+    (and (cmp (first l) (first (rest l)))
+         (sorted? cmp (rest l)))]))
