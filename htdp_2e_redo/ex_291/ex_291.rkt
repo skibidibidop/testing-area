9081d61ab63ee72cd302047d1ca00837f2b99dbf
skibidibidop <dev.mapb@gmail.com>
2023-08-14 13:49:10 +0800

    Add ex 291 description

---
+++ htdp_2e_redo/ex_291/ex_291.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex_291) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: August 14, 2023
+
+Exercise 291. The fold functions are so powerful that you can define
+almost any list-processing functions with them. Use fold to define map-
+via-fold, which simulates map.
+|#
