e402ae2d114842ca99b151df9443a1bc512c30c1
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-15 04:29:12 +0800

    Add ex 292 description

--- htdp_2e_redo/ex_292/ex_292.rkt
+++ htdp_2e_redo/ex_292/ex_292.rkt
@@ -1,3 +1,21 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex_292) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: August 15, 2023
+
+Exercise 292. Design the function sorted?, which comes with the following
+signature and purpose statement:
+
+(see below)
+
+The wish list even includes examples.
+|#
+
+; [X X -> Boolean] [NEList-of X] -> Boolean
+; determines whether l is sorted according to cmp
+(check-expect (sorted? < '(1 2 3)) #true)
+(check-expect (sorted? < '(2 1 3)) #false)
+
+(define (sorted? cmp l)
+  #false)
+
+
