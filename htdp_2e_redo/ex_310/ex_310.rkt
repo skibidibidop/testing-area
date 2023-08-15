8589bbd5e14e1d4b590147ac2ff8c7559b6692a2
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-15 08:57:37 +0800

    Add data definition

--- htdp_2e_redo/ex_310/ex_310.rkt
+++ htdp_2e_redo/ex_310/ex_310.rkt
@@ -6,3 +6,15 @@ Exercise 310. Develop count-persons. The function consumes a family tree and
 counts the child structures in the tree.
 |#
 
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+(define-struct no-parent [])
+
+(define NP (make-no-parent))
+
+(define-struct child [father mother name date eyes])
+; An FT (short for family tree) is one of:
+; -- NP
+; -- (make-child FT FT String N String)
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
