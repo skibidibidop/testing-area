526012d9e188005339314faaea43ff117b40916c
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-15 10:14:19 +0800

    Add ex 311 description

---
+++ htdp_2e_redo/ex_311/ex_311.rkt
@@ -0,0 +1,34 @@
+#|
+Author: Mark Beltran
+Date: August 15, 2023
+
+Exercise 311. Develop the function average-age. It consumes a family tree and
+the current year. It produces the average age of all child structures in
+the family tree.
+|#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+(define-struct no-parent [])
+
+(define NP (make-no-parent))
+
+(define-struct child [father mother name date eyes])
+; An FT (short for family tree) is one of:
+; -- NP
+; -- (make-child FT FT String N String)
+
+; Oldest Generation:
+(define Carl (make-child NP NP "Carl" 1926 "green"))
+(define Bettina (make-child NP NP "Bettina" 1926 "green"))
+
+; Middle Generation:
+(define Adam (make-child Carl Bettina "Adam" 1950 "hazel"))
+(define Dave (make-child Carl Bettina "Dave" 1955 "black"))
+(define Eva (make-child Carl Bettina "Eva" 1965 "blue"))
+(define Fred (make-child NP NP "Fred" 1966 "pink"))
+
+; Youngest Generation:
+(define Gustav (make-child Fred Eva "Gustav" 1988 "brown"))
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
