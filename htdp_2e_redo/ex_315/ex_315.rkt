30964de5dc1be73462ac1c2b6890dfafcaf4609a
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-16 05:06:00 +0800

    Add ex 315 description

---
+++ htdp_2e_redo/ex_315/ex_315.rkt
@@ -0,0 +1,17 @@
+#|
+Author: Mark Beltran
+Date: August 16, 2023
+
+Exercise 315. Design the function average-age. It consumes a family forest and
+a year (N). From this data, it produces the average age of all child instances
+in the forest.
+
+Note
+If the trees in this forest overlap, the result isn’t a true average because
+some people contribute more than others. For this exercise, act as if
+the trees don’t overlap.
+|#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
