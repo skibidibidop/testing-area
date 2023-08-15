05aebf78b4748f4d00c67dbae678cc3524177b98
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-15 10:13:29 +0800

    Add ex 310 solution

--- htdp_2e_redo/ex_310/ex_310.rkt
+++ htdp_2e_redo/ex_310/ex_310.rkt
@@ -17,4 +17,31 @@ counts the child structures in the tree.
 ; -- NP
 ; -- (make-child FT FT String N String)
 
+; Oldest Generation:
+(define Carl (make-child NP NP "Carl" 1926 "green"))
+(define Bettina (make-child NP NP "Bettina" 1926 "green"))
+
+; Middle Generation:
+(define Adam (make-child Carl Bettina "Adam" 1950 "hazel"))
+(define Dave (make-child Carl Bettina "Dave" 1955 "black"))
+(define Eva (make-child Carl Bettina "Eva" 1965 "blue"))
+(define Fred (make-child NP NP "Fred" 1966 "pink"))
+
+; Youngest Generation:
+(define Gustav (make-child Fred Eva "Gustav" 1988 "brown"))
+
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; FT -> Number
+; Counts the child structures in an-ftree
+(check-expect (count-persons Carl) 1)
+(check-expect (count-persons Fred) 1)
+(check-expect (count-persons Dave) 3)
+(check-expect (count-persons Gustav) 5)
+
+(define (count-persons an-ftree)
+  (cond
+    [(no-parent? an-ftree) 0]
+    [else
+     (+ (count-persons (child-father an-ftree))
+        (count-persons (child-mother an-ftree)) 1)]))
