f1db3c5a11f0d7b77dc2dbd7ad8dcc134ce0581a
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-16 04:52:38 +0800

    Add data def

--- htdp_2e_redo/ex_314/ex_314.rkt
+++ htdp_2e_redo/ex_314/ex_314.rkt
@@ -10,4 +10,28 @@ the list abstractions from the preceding chapter.
 
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
+; An FF (short for Family Forest) is a [List-of FT]
+
+(define-struct no-parent [])
+
+(define NP (make-no-parent))
+
+(define-struct child [father mother name date eyes])
+; An FT (short for Family Tree) is one of:
+; NP
+; (make-child FT FT String N String)
+
+; Oldest Generation:
+(define Carl (make-child NP NP "Carl" 1926 "green"))
+(define Bettina (make-child NP NP "Bettina" 1926 "green"))
+
+; Middle Generation:
+(define Adam (make-child Carl Bettina "Adam" 1950 "hazel"))
+(define Dave (make-child Carl Bettina "Dave" 1955 "black"))
+(define Eva (make-child Carl Bettina "Eva" 1965 "blue"))
+(define Fred (make-child NP NP "Fred" 1966 "pink"))
+
+; Youngest Generation:
+(define Gustav (make-child Fred Eva "Gustav" 1988 "brown"))
+
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
