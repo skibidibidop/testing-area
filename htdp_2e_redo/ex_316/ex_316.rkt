b4fa77bb84e2c93f48ca73fb64b0314d4f1b7625
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-16 10:10:56 +0800

    Add ex 316 solution

--- htdp_2e_redo/ex_316/ex_316.rkt
+++ htdp_2e_redo/ex_316/ex_316.rkt
@@ -7,4 +7,21 @@ Exercise 316. Define the atom? function.
 
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
+; An Atom is one of:
+; Number
+; String
+; Symbol
+
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; Any -> Boolean
+; Is a an Atom
+(check-expect (atom? "hi!") #true)
+(check-expect (atom? 1000) #true)
+(check-expect (atom? 'z) #true)
+(check-expect (atom? (make-posn 1 2)) #false)
+
+(define (atom? a)
+  (or (string? a)
+      (number? a)
+      (symbol? a)))
