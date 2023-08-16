42bc85604221010a550c9d625876eb9aa000fe63
skibidibidop <dev.mapb@gmail.com>
2023-08-16 15:41:37 +0800

    Add ex 317 description

---
+++ htdp_2e_redo/ex_317/ex_317.rkt
@@ -0,0 +1,22 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex_317) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: August 16, 2023
+
+Exercise 317. A program that consists of three connected functions
+ought to express this relationship with a local expression.
+
+Copy and reorganize the program from figure 117 into a single function
+using local. Validate the revised code with the test suite for count.
+
+The second argument to the local functions, sy, never changes. It is
+always the same as the original symbol. Hence you can eliminate it from
+the local function definitions to tell the reader that sy is a constant across
+the traversal process.
+|#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
