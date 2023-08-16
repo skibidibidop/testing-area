c83a88815e9a7d89eeb6523c5a32ca9f9757e0e2
skibidibidop <dev.mapb@gmail.com>
2023-08-16 16:00:18 +0800

    Add data def

--- htdp_2e_redo/ex_317/ex_317.rkt
+++ htdp_2e_redo/ex_317/ex_317.rkt
@@ -19,4 +19,30 @@ the traversal process.
 
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
+; An S-expr is one of:
+; -- Atom
+; -- SL
+
+; An Atom is one of:
+; -- Number
+; -- String
+; -- Symbol
+
+; An SL is one of:
+; -- '()
+; -- (cons S-expr SL)
+
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; Any -> Boolean
+; Is a an Atom
+(check-expect (atom? "hi!") #true)
+(check-expect (atom? 1000) #true)
+(check-expect (atom? 'z) #true)
+(check-expect (atom? (make-posn 1 2)) #false)
+
+(define (atom? a)
+  (or (string? a)
+      (number? a)
+      (symbol? a)))
+
