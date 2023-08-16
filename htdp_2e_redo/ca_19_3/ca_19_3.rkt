ea2f0431f9edb622cb7272eb3c15c1d4beb50fa7
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-16 10:50:08 +0800

    Add data def, wish list: (count)

--- htdp_2e_redo/ca_19_3/ca_19_3.rkt
+++ htdp_2e_redo/ca_19_3/ca_19_3.rkt
@@ -7,3 +7,24 @@ Design the function count, which determines
 how many times some symbol occurs in some S-expression.
 |#
 
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; An S-expr is one of:
+; -- Atom
+; -- SL
+
+; An Atom is one of:
+; -- Number
+; -- String
+; -- Symbol
+
+; An SL is one of:
+; -- '()
+; -- (cons S-expr SL)
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; S-expr Symbol -> N
+; Counts all occurrences of sy in sexp
+(define (count sexp sy)
+  0)
