ba8309b95f2a0ffa3df3a000c5bfa449008be78b
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-18 06:27:11 +0800

    Add ex 321 solution

--- htdp_2e_redo/ex_321/ex_321.rkt
+++ htdp_2e_redo/ex_321/ex_321.rkt
@@ -6,3 +6,27 @@ Exercise 321. Abstract the data definitions for S-expr and SL so that
 they abstract over the kinds of Atoms that may appear.
 |#
 
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; An S-expr is one of:
+; -- Atom
+; -- SL
+
+; An Atom is one of:
+; -- Number
+; -- String
+; -- Symbol
+
+; An SL is one of:
+; -- '()
+; -- (cons S-expr SL)
+
+; ABSTRACTION //////////////////////////////////////////////////////////////////
+
+; A [One-of S] is one of:
+; [One-of A]
+; [List-of [One-of S]]
+
+; A [List-of [One-of S]] is one of:
+; '()
+; (cons [One-of S] [List-of [One-of S]])
