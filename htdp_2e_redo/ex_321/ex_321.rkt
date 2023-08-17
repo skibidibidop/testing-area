a62ae25b73402f314fc6d22fd8476b078c8d3440
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-18 05:42:12 +0800

    Ad ex 321 description

---
+++ htdp_2e_redo/ex_321/ex_321.rkt
@@ -0,0 +1,8 @@
+#|
+Author: Mark Beltran
+Date: August 18, 2023
+
+Exercise 321. Abstract the data definitions for S-expr and SL so that
+they abstract over the kinds of Atoms that may appear.
+|#
+
