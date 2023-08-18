59c839e0cbf4ec4e5bf9b2f1c609755e482a115a
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-18 08:28:53 +0800

    Add (contains-bt?) to wish list

---
+++ htdp_2e_redo/ex_322/ex_322.rkt
@@ -0,0 +1,43 @@
+#|
+Author: Mark Beltran
+Date: August 18, 2023
+
+Exercise 322. Draw the above two trees in the manner of figure 119.
+Then design contains-bt?, which determines whether a given number
+occurs in some given BT.
+|#
+
+(define-struct no-info [])
+(define NONE (make-no-info))
+
+(define-struct node [ssn name left right])
+; A BT (short for BinaryTree) is one of:
+; NONE
+; (make-node Number Symbol BT BT)
+
+; Trees provided as examples:
+
+(define TREE_A
+  (make-node
+   15
+   'd
+   NONE
+   (make-node
+    24 'i NONE NONE)))
+
+(define TREE_B
+  (make-node
+   15
+   'd
+   (make-node
+    87 'h NONE NONE)
+   NONE))
+
+; Number BT -> Boolean
+; Is n in bt
+(check-expect (contains-bt? 12 NONE) #false)
+(check-expect (contains-bt? 15 TREE_A) #true)
+(check-expect (contains-bt? 24 TREE_A) #true)
+(check-expect (contains-bt? 30 TREE_A) #false)
+
+(define (contains-bt? n bt) #false)
