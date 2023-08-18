9eac8d760abd69eeff564842a94cec00586421b4
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-19 05:31:40 +0800

    Add ex 324 description

---
+++ htdp_2e_redo/ex_324/ex_324.rkt
@@ -0,0 +1,20 @@
+#|
+Author: Mark Beltran
+Date: August 19, 2023
+
+Exercise 324. Design the function inorder. It consumes a binary tree
+and produces the sequence of all the ssn numbers in the tree as they show
+up from left to right when looking at a tree drawing.
+
+Hint Use append, which concatenates lists like thus:
+
+(append (list 1 2 3) (list 4) (list 5 6 7))
+==
+(list 1 2 3 4 5 6 7)
+
+What does inorder produce for a binary search tree?
+|#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
