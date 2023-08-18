836cd4589e06e67b20a9ff10dd479cc4825ea82f
skibidibidop <dev.mapb@gmail.com>
2023-08-18 15:21:52 +0800

    Add (search-bt) tests

--- htdp_2e_redo/ex_323/ex_323.rkt
+++ htdp_2e_redo/ex_323/ex_323.rkt
@@ -1,3 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex_323) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 #|
 Author: Mark Beltran
 Date: August 18, 2023
@@ -7,7 +10,7 @@ n and a BT. If the tree contains a node structure whose ssn field is n, the
 function produces the value of the name field in that node. Otherwise, the
 function produces #false.
 
-Hint Consider using contains-bt? to check the entire tree first or
+Hint: Consider using contains-bt? to check the entire tree first or
 boolean? to check the result of the natural recursion at each stage.
 |#
 
@@ -25,19 +28,21 @@ boolean? to check the result of the natural recursion at each stage.
 
 (define TREE_A
   (make-node
-   15
-   'd
+   15 'd
    NONE
    (make-node
     24 'i NONE NONE)))
 
 (define TREE_B
   (make-node
-   15
-   'd
+   15 'd
    (make-node
     87 'h NONE NONE)
    NONE))
+
+; A MatchOrNot is one of:
+; #false
+; Symbol
 
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
 
@@ -55,3 +60,24 @@ boolean? to check the result of the natural recursion at each stage.
      (or (= n (node-ssn bt) n)
          (contains-bt? n (node-left bt))
          (contains-bt? n (node-right bt)))]))
+
+; Number BT -> MatchOrNot
+; Returns name of a node if its ssn equals n
+(check-expect (search-bt 30 NONE) #false)
+(check-expect (search-bt 15 TREE_A) 'd)
+(check-expect (search-bt 24 TREE_A) 'i)
+(check-expect (search-bt 16 TREE_A) #false)
+(check-expect (search-bt 3 (make-node
+                            2 'a
+                            (make-node
+                             3 'b
+                             (make-node
+                              3 'c
+                              NONE NONE)
+                             (make-node
+                              4 'd NONE NONE))
+                            (make-node
+                             3 'e NONE NONE)))
+              'b)
+
+(define (search-bt n bt)
