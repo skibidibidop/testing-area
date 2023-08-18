b1ac9de5cfd6e73ab4bcf41856fdb148c797194f
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-19 05:28:27 +0800

    Add ex 323 solution (but I used BST input)

--- htdp_2e_redo/ex_323/ex_323.rkt
+++ htdp_2e_redo/ex_323/ex_323.rkt
@@ -1,9 +1,6 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex_323) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 #|
 Author: Mark Beltran
-Date: August 18, 2023
+Date: August 19, 2023
 
 Exercise 323. Design search-bt. The function consumes a number
 n and a BT. If the tree contains a node structure whose ssn field is n, the
@@ -13,6 +10,9 @@ function produces #false.
 Hint: Consider using contains-bt? to check the entire tree first or
 boolean? to check the result of the natural recursion at each stage.
 |#
+
+; Can't think of a reasonable way to make this work without requiring the
+; input to be sorted from lowest to highest ssn
 
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
@@ -67,20 +67,10 @@ boolean? to check the result of the natural recursion at each stage.
 (check-expect (search-bt 15 TREE_A) 'd)
 (check-expect (search-bt 24 TREE_A) 'i)
 (check-expect (search-bt 16 TREE_A) #false)
-(check-expect (search-bt 3 (make-node
-                            2 'a
-                            (make-node
-                             3 'b
-                             (make-node
-                              3 'c
-                              NONE NONE)
-                             (make-node
-                              4 'd NONE NONE))
-                            (make-node
-                             3 'e NONE NONE)))
-              'b)
 
 (define (search-bt n bt)
   (cond
     [(no-info? bt) #false]
-    [
+    [(= n (node-ssn bt)) (node-name bt)]
+    [(< n (node-ssn bt)) (search-bt n (node-left bt))]
+    [(> n (node-ssn bt)) (search-bt n (node-right bt))]))
