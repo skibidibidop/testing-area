990d7fd6efb28301f997820c3b1ce1d45c14b952
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-19 08:59:16 +0800

    Add ex 325 description

---
+++ htdp_2e_redo/ex_325/ex_325.rkt
@@ -0,0 +1,14 @@
+#|
+Author: Mark Beltran
+Date: August 19, 2023
+
+Exercise 325. Design search-bst. The function consumes a number
+n and a BST. If the tree contains a node whose ssn field is n, the function
+produces the value of the name field in that node. Otherwise, the function
+produces NONE. The function organization must exploit the BST invariant
+so that the function performs as few comparisons as necessary.
+|#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
