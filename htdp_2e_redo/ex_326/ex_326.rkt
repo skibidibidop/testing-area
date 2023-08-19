3835af34e7e6f7a5331af2a7893a2db541f90977
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-19 10:14:44 +0800

    Add (create-bst) tests

--- htdp_2e_redo/ex_326/ex_326.rkt
+++ htdp_2e_redo/ex_326/ex_326.rkt
@@ -49,6 +49,71 @@ Once the design is completed, use the function on tree A from figure 119.
 
 ; BST Number Symbol -> BST
 ; Adds (make-node num sym NONE NONE) to bst
-(check-expect (create-bst ))
+(check-expect (create-bst TREE_A 0 'z)
+              (make-node 63 'a
+                         (make-node 29 'b
+                                    (make-node 15 'c
+                                               (make-node 10 'd
+                                                          (make-node 0 'z
+                                                                     NONE
+                                                                     NONE)
+                                                          NONE)
+                                               (make-node 24 'e
+                                                          NONE
+                                                          NONE))
+                                    NONE)
+                         (make-node 89 'f
+                                    (make-node 77 'g
+                                               NONE
+                                               NONE)
+                                    (make-node 95 'h
+                                               NONE
+                                               (make-node 99 'i
+                                                          NONE
+                                                          NONE)))))
+(check-expect (create-bst TREE_A 150 'y)
+              (make-node 63 'a
+                         (make-node 29 'b
+                                    (make-node 15 'c
+                                               (make-node 10 'd
+                                                          NONE
+                                                          NONE)
+                                               (make-node 24 'e
+                                                          NONE
+                                                          NONE))
+                                    NONE)
+                         (make-node 89 'f
+                                    (make-node 77 'g
+                                               NONE
+                                               NONE)
+                                    (make-node 95 'h
+                                               NONE
+                                               (make-node 99 'i
+                                                          NONE
+                                                          (make-node 150 'y
+                                                                     NONE
+                                                                     NONE))))))
+(check-expect (create-bst TREE_A 80 'x)
+              (make-node 63 'a
+                         (make-node 29 'b
+                                    (make-node 15 'c
+                                               (make-node 10 'd
+                                                          NONE
+                                                          NONE)
+                                               (make-node 24 'e
+                                                          NONE
+                                                          NONE))
+                                    NONE)
+                         (make-node 89 'f
+                                    (make-node 77 'g
+                                               NONE
+                                               (make-node 80 'x
+                                                          NONE
+                                                          NONE))
+                                    (make-node 95 'h
+                                               NONE
+                                               (make-node 99 'i
+                                                          NONE
+                                                          NONE)))))
 
 (define (create-bst bst num sym) NONE)
