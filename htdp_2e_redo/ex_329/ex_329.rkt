6579750ab73b0d38e701be95e7c927035124539d
skibidibidop <dev.mapb@gmail.com>
2023-08-22 15:14:34 +0800

    Add ex 329 description

---
+++ htdp_2e_redo/ex_329/ex_329.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex_329) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: August 22, 2023
+
+Exercise 329. How many times does a file name read! occur in the
+directory tree TS? Can you describe the path from the root directory to the
+occurrences? What is the total size of all the files in the tree? What is the
+total size of the directory if each directory node has size 1? How many
+levels of directories does it contain?
+|#
+
