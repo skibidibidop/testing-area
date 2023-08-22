d642dce2995e65443c8ea8fc8b492451646ed586
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-23 06:48:16 +0800

    Add ex 331 solution

--- htdp_2e_redo/ex_331/ex_331.rkt
+++ htdp_2e_redo/ex_331/ex_331.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex_331) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 #|
 Author: Mark Beltran
 Date: August 22, 2023
@@ -26,37 +23,41 @@ exercise 330 provides you with data examples.
 ; String
 ; Symbol
 
-(define DIR_TREE
-  (list 'TS
-        (list "read!")
-        (list 'Text
-              (list "part1"
-                    "part2"
-                    "part3"))
-        (list 'Libs
-              (list 'Code
-                    (list "hang"
-                          "draw"))
-              (list 'Docs
-                    (list "read!")))))
+#|
+Directory tree from Figure 123
+
+- TS (DIR)
+--- read! (10)
+--- Text (DIR)
+------ part1 (99)
+------ part2 (52)
+------ part3 (17)
+--- Libs (DIR)
+------ Code (DIR)
+---------- hang (8)
+---------- draw (2)
+------ Docs (DIR)
+---------- write! (19)
+|#
+
+(define Docs (list "write!"))
+(define Code (list "hang" "draw"))
+(define Libs (list Code Docs))
+(define Text (list "part1" "part2" "part3"))
+(define TS1   (list "read!" Text Libs))
 
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
 
 ; Dir.v1 -> N
 ; Returns the number of files in a given Dir.v1
 (check-expect (how-many '()) 0)
-(check-expect (how-many DIR_TREE) 7)
+(check-expect (how-many TS1) 7)
 
 (define (how-many dir)
-  (local
-    [(define (for_list l)
-       (how-many (first l)))]
-    
-    (cond
-      [(empty? dir) 0]
-      [(list? (first dir))
-       (for_list (first dir))]
-      [(string? (first dir))
-       (+ (how-many (rest dir)) 1)]
-      [else
-       (how-many (rest dir))])))
+  (cond
+    [(empty? dir) 0]
+    [(string? (first dir))
+     (+ 1 (how-many (rest dir)))]
+    [(list? dir)
+     (+ (how-many (first dir))
+        (how-many (rest dir)))]))
