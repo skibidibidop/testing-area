c85da61fd9f31fbd80ec58336773cae2a655163b
skibidibidop <dev.mapb@gmail.com>
2023-08-22 16:31:52 +0800

    Add ex 331 description

---
+++ htdp_2e_redo/ex_331/ex_331.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex_331) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: August 22, 2023
+
+Exercise 331. Design the function how-many, which determines how
+many files a given Dir.v1 contains. Remember to follow the design recipe;
+exercise 330 provides you with data examples.
+|#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
