b231e2ad3526570d2227d949daf310d40c3ce787
skibidibidop <dev.mapb@gmail.com>
2023-08-22 16:33:47 +0800

    Add (how-many) to wish list

--- htdp_2e_redo/ex_331/ex_331.rkt
+++ htdp_2e_redo/ex_331/ex_331.rkt
@@ -12,4 +12,17 @@ exercise 330 provides you with data examples.
 
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
+; A Dir.v1 is one of:
+; '()
+; (cons File.v1 Dir.v1)
+; (cons Dir.v1 Dir.v1)
+
+; A File.v1 is a String.
+
+; An N is a Natural Number
+
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; Dir.v1 -> N
+; Returns the number of files in a given Dir.v1
+(define (how-many dir) 0)
