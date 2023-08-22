1585c6aa4031f08ad7360e96cd1eba0d51407d62
skibidibidop <dev.mapb@gmail.com>
2023-08-22 15:19:03 +0800

    Add ex 329 solution

--- htdp_2e_redo/ex_329/ex_329.rkt
+++ htdp_2e_redo/ex_329/ex_329.rkt
@@ -12,3 +12,18 @@ total size of the directory if each directory node has size 1? How many
 levels of directories does it contain?
 |#
 
+; How many times does a file name read! occur in the directory tree TS?
+; It occurs twice, in different subdirectories.
+
+; Can you describe the path from the root directory to the occurrences?
+; The first file with the name read! occurs in the first directory (TS)
+; but the other file occurs in a subdirectory of a subdirectory of TS
+
+; What is the total size of all the files in the tree?
+; 207
+
+; What is the total size of the directory if each directory node has size 1?
+; 5
+
+; How many levels of directories does it contain?
+; 3
