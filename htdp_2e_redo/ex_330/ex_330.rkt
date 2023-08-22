1c3a567ef2b6ea11b2ecf9d07d54d8ab550588ef
skibidibidop <dev.mapb@gmail.com>
2023-08-22 15:37:25 +0800

    Add data def, dir tree from fig 123

--- htdp_2e_redo/ex_330/ex_330.rkt
+++ htdp_2e_redo/ex_330/ex_330.rkt
@@ -8,3 +8,30 @@ Date: August 22, 2023
 Exercise 330. Translate the directory tree in figure 123 into a data
 representation according to model 1.
 |#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; A Dir.v1 (short for directory) is one of:
+; '()
+; (cons File.v1 Dir.v1)
+; (cons Dir.v1 Dir.v1)
+
+; A File.v1 is a String
+
+#|
+Directory tree from Figure 123
+
+- TS (DIR)
+--- read! (10)
+--- Text (DIR)
+------ part1 (99)
+------ part2 (52)
+------ part3 (17)
+--- Libs (DIR)
+------ Code (DIR)
+---------- hang (8)
+---------- draw (2)
+------ Docs (DIR)
+---------- read! (19)
+
+|#
