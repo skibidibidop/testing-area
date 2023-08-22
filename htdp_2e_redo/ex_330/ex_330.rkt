07bdb6810f114e1ecf89269e3f188f7c10d6c39a
skibidibidop <dev.mapb@gmail.com>
2023-08-22 15:22:02 +0800

    Add ex 330 description

---
+++ htdp_2e_redo/ex_330/ex_330.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex_330) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: August 22, 2023
+
+Exercise 330. Translate the directory tree in figure 123 into a data
+representation according to model 1.
+|#
