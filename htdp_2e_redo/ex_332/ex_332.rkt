f96fbb69522fba7ab21f5bb235eb0779da87175d
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-23 06:53:34 +0800

    Add ex 332 description

---
+++ htdp_2e_redo/ex_332/ex_332.rkt
@@ -0,0 +1,38 @@
+#|
+Author: Mark Beltran
+Date: August 23, 2023
+
+Exercise 332. Translate the directory tree in figure 123 into a data
+representation according to model 2.
+
+Modified directory tree from Figure 123. I changed read! (19) to write! (19)
+to make it easier for me to debug.
+
+- TS (DIR)
+--- read! (10)
+--- Text (DIR)
+------ part1 (99)
+------ part2 (52)
+------ part3 (17)
+--- Libs (DIR)
+------ Code (DIR)
+---------- hang (8)
+---------- draw (2)
+------ Docs (DIR)
+---------- write! (19)
+|#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+(define-struct dir [name content])
+; A Dir.v2 is a structure:
+; (make-dir String LOFD)
+
+; An LOFD (short for list of files and directories) is one of:
+; '()
+; (cons File.v2 LOFD)
+; (cons Dir.v2 LOFD)
+
+; A File.v2 is a String.
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
