e3b1c6ef8352631a06bd39d86578f9a6c141b100
skibidibidop <dev.mapb@gmail.com>
2023-08-23 16:32:04 +0800

    Add ex 337 description

---
+++ htdp_2e_redo/ex_337/ex_337.rkt
@@ -0,0 +1,31 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex_337) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#|
+Author: Mark Beltran
+Date: August 23, 2023
+
+Exercise 337. Use List-of to simplify the data definition Dir.v3. Then
+use ISL+’s list-processing functions from figures 95 and 96 to simplify the
+function definition(s) for the solution of exercise 336.
+|#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+(define-struct file [name size content])
+; A File.v3 is a structure:
+; (make-file String N String)
+
+(define-struct dir.v3 [name dirs files])
+; A Dir.v3 is a structrue:
+; (make-dir.v3 String Dir* File*)
+
+; A Dir* is one of:
+; '()
+; (cons Dir.v3 Dir*)
+
+; A File* is one of:
+; '()
+; (cons File.v3 File*)
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
