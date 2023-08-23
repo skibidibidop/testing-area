9b7bd5b2344b446befbc8f8a0861a5ed27d51b39
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-24 05:26:23 +0800

    Add ex 338 description

---
+++ htdp_2e_redo/ex_338/ex_338.rkt
@@ -0,0 +1,13 @@
+#|
+Author: Mark Beltran
+Date: August 24, 023
+
+Exercise 338. Use create-dir to turn some of your directories into
+ISL+ data representations. Then use how-many from exercise 336 to count
+how many files they contain. Why are you confident that how-many produces
+correct results for these directories?
+|#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
