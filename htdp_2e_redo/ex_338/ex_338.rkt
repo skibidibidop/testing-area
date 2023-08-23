7ebdd9035219eb180095156841b792c93a8b4f6b
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-24 05:50:37 +0800

    Add ex 338 solution

--- htdp_2e_redo/ex_338/ex_338.rkt
+++ htdp_2e_redo/ex_338/ex_338.rkt
@@ -8,6 +8,34 @@ how many files they contain. Why are you confident that how-many produces
 correct results for these directories?
 |#
 
+(require htdp/dir)
+
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
+
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+(define (how-many dir)
+  (local
+    [; Dir.v3 -> N
+     (define (count_files d)
+       (length (dir-files d)))
+
+     ; Dir* -> N
+     (define (traverse dirs)
+       (cond
+         [(empty? dirs) 0]
+         [else
+          (+ (count_files (first dirs))
+             (traverse (dir-dirs (first dirs)))
+             (traverse (rest dirs)))]))]
+
+    (+ (count_files dir)
+       (traverse (dir-dirs dir)))))
+
+(define WIN_DIR_PATH
+  (create-dir "D:\\DrRacket\\Racket\\projects"))
+
+(how-many WIN_DIR_PATH)
+
+
