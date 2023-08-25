3e6d233c00ad08784f77584bacc461e22e9da3dc
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-26 04:16:09 +0800

    Add ex 340 solution

--- htdp_2e_redo/ex_340/ex_340.rkt
+++ htdp_2e_redo/ex_340/ex_340.rkt
@@ -12,55 +12,55 @@ and directories in a given Dir.
 
 (define DIR_TREE
   (make-dir "TS (DIR)"
-               (list
-                (make-dir "Text (DIR)"
-                             '()
-                             (list
-                              (make-file "part1" 99 "")
-                              (make-file "part2" 52 "")
-                              (make-file "part3" 17 "")))
-                (make-dir "Libs (DIR)"
-                             (list
-                              (make-dir "Code (DIR)"
-                                           '()
-                                           (list
-                                            (make-file "hang" 8 "")
-                                            (make-file "draw" 2 "")))
-                              (make-dir "Docs (DIR)"
-                                           '()
-                                           (list
-                                            (make-file "write!" 19 ""))))
-                             '()))
-               (list
-                (make-file "read!" 10 ""))))
+            (list
+             (make-dir "Text (DIR)"
+                       '()
+                       (list
+                        (make-file "part1" 99 "")
+                        (make-file "part2" 52 "")
+                        (make-file "part3" 17 "")))
+             (make-dir "Libs (DIR)"
+                       (list
+                        (make-dir "Code (DIR)"
+                                  '()
+                                  (list
+                                   (make-file "hang" 8 "")
+                                   (make-file "draw" 2 "")))
+                        (make-dir "Docs (DIR)"
+                                  '()
+                                  (list
+                                   (make-file "write!" 19 ""))))
+                       '()))
+            (list
+             (make-file "read!" 10 ""))))
 
 (define DIR_TREE2
   (make-dir
    "TS (DIR)"
    (list
     (make-dir "Text (DIR)"
-                 '()
-                 (list
-                  (make-file "part1" 99 "")
-                  (make-file "part2" 52 "")
-                  (make-file "part3" 17 "")))
+              '()
+              (list
+               (make-file "part1" 99 "")
+               (make-file "part2" 52 "")
+               (make-file "part3" 17 "")))
     (make-dir "Libs (DIR)"
-                 (list
-                  (make-dir "Code (DIR)"
-                               '()
-                               (list
-                                (make-file "hang" 8 "")
-                                (make-file "draw" 2 "")))
-                  (make-dir "Docs (DIR)"
-                               (list
-                                (make-dir "Extra (DIR)"
-                                             '()
-                                             (list
-                                              (make-file "hey" 3 "")
-                                              (make-file "there" 4 ""))))
-                               (list
-                                (make-file "write!" 19 ""))))
-                 '()))
+              (list
+               (make-dir "Code (DIR)"
+                         '()
+                         (list
+                          (make-file "hang" 8 "")
+                          (make-file "draw" 2 "")))
+               (make-dir "Docs (DIR)"
+                         (list
+                          (make-dir "Extra (DIR)"
+                                    '()
+                                    (list
+                                     (make-file "hey" 3 "")
+                                     (make-file "there" 4 ""))))
+                         (list
+                          (make-file "write!" 19 ""))))
+              '()))
    (list
     (make-file "read!" 10 ""))))
 
@@ -78,35 +78,35 @@ and directories in a given Dir.
 ; Dir -> [List-of File]
 ; Lists all file names in given directory
 (check-expect (ls DIR_TREE)
-              (list "TS (DIR)"
-                    (list "Text (DIR)"
-                          (list "part1"
-                                "part2"
-                                "part3"))
-                    (list "Libs (DIR)"
-                          (list "Code (DIR)"
-                                (list "hang"
-                                      "draw"))
-                          (list "Docs (DIR)"
-                                (list "write!")))
-                    (list "read!")))
+              (list
+               "TS (DIR)" "read!"
+               "Text (DIR)" "part1" "part2" "part3"
+               "Libs (DIR)"
+               "Code (DIR)" "hang" "draw"
+               "Docs (DIR)" "write!"))
 (check-expect (ls DIR_TREE2)
-              (list "TS (DIR)"
-                    (list "Text (DIR)"
-                          (list "part1"
-                                "part2"
-                                "part3"))
-                    (list "Libs (DIR)"
-                          (list "Code (DIR)"
-                                (list "hang"
-                                      "draw"))
-                          (list "Docs (DIR)"
-                                (list "Extra (DIR)"
-                                      (list "hey"
-                                            "there"))
-                                (list "write!")))
-                    (list "read!")))
-(check-expect (ls EMPTY_DIR_TREE) '())
-(check-expect (ls NO_DIRS) (list "f1" "f2"))
+              (list
+               "TS (DIR)" "read!"
+               "Text (DIR)" "part1" "part2" "part3"
+               "Libs (DIR)"
+               "Code (DIR)" "hang" "draw"
+               "Docs (DIR)" "write!"
+               "Extra (DIR)" "hey" "there"))
+(check-expect (ls EMPTY_DIR_TREE) (list "Empty (DIR)"))
+(check-expect (ls NO_DIRS) (list "No Dirs (DIR)" "f1" "f2"))
 
-(define (ls d) '())
+(define (ls d)
+  (local
+    [(define (fname_lister flist)
+       (foldr
+        (λ (an_f base)
+          (cons (file-name an_f) base))
+        '()
+        flist))]
+
+    (foldl
+     (λ (a_dir base)
+       (append base (ls a_dir)))
+     (append (list (dir-name d))
+             (fname_lister (dir-files d)))
+     (dir-dirs d))))
