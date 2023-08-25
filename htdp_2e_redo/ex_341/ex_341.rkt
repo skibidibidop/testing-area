af258f056ad81a21dff44e7fd8b6730860c6a0b3
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-26 04:34:14 +0800

    Add ex 341 description

---
+++ htdp_2e_redo/ex_341/ex_341.rkt
@@ -0,0 +1,18 @@
+#|
+Author: Mark Beltran
+Date: August 26, 2023
+
+Exercise 341. Design du, a function that consumes a Dir and computes
+the total size of all the files in the entire directory tree. Assume that
+storing a directory in a Dir structure costs 1 file storage unit. In the real
+world, a directory is basically a special file, and its size depends on how
+large its associated directory is.
+|#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+
