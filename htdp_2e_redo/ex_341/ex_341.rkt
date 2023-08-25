d77c57999e5fe8d263bcad4db329d5887ac6e7f6
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-26 04:49:16 +0800

    Add (du) to wish list

--- htdp_2e_redo/ex_341/ex_341.rkt
+++ htdp_2e_redo/ex_341/ex_341.rkt
@@ -9,10 +9,76 @@ world, a directory is basically a special file, and its size depends on how
 large its associated directory is.
 |#
 
+(require htdp/dir)
+
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
+(define DIR_TREE
+  (make-dir "TS (DIR)"
+            (list
+             (make-dir "Text (DIR)"
+                       '()
+                       (list
+                        (make-file "part1" 99 "")
+                        (make-file "part2" 52 "")
+                        (make-file "part3" 17 "")))
+             (make-dir "Libs (DIR)"
+                       (list
+                        (make-dir "Code (DIR)"
+                                  '()
+                                  (list
+                                   (make-file "hang" 8 "")
+                                   (make-file "draw" 2 "")))
+                        (make-dir "Docs (DIR)"
+                                  '()
+                                  (list
+                                   (make-file "write!" 19 ""))))
+                       '()))
+            (list
+             (make-file "read!" 10 ""))))
 
+(define DIR_TREE2
+  (make-dir
+   "TS (DIR)"
+   (list
+    (make-dir "Text (DIR)"
+              '()
+              (list
+               (make-file "part1" 99 "")
+               (make-file "part2" 52 "")
+               (make-file "part3" 17 "")))
+    (make-dir "Libs (DIR)"
+              (list
+               (make-dir "Code (DIR)"
+                         '()
+                         (list
+                          (make-file "hang" 8 "")
+                          (make-file "draw" 2 "")))
+               (make-dir "Docs (DIR)"
+                         (list
+                          (make-dir "Extra (DIR)"
+                                    '()
+                                    (list
+                                     (make-file "hey" 3 "")
+                                     (make-file "there" 4 ""))))
+                         (list
+                          (make-file "write!" 19 ""))))
+              '()))
+   (list
+    (make-file "read!" 10 ""))))
+
+(define EMPTY_DIR_TREE (make-dir "Empty (DIR)" '() '()))
+(define NO_DIRS
+  (make-dir
+   "No Dirs (DIR)"
+   '()
+   (list
+    (make-file "f1" 5 "")
+    (make-file "f2" 32 ""))))
 
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
 
-
+; Dir -> N
+; Returns the total size of all files in a directory tree
+; Sub-directories cost 1 file storage unit
+(define (du d) 0)
