2b922474f57a19747bf9ef8bd5cc1f896ab160d8
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-29 07:42:37 +0800

    Add ex 224 description

---
+++ htdp_2e_redo/ex_224/ex_224.rkt
@@ -0,0 +1,15 @@
+#|
+Author: Mark Beltran
+Date: August 29, 2023
+
+Exercise 224. Use the lessons learned from the preceding two sections and design
+the game extension slowly, adding one feature of the game after another. Always
+use the design recipe and rely on the guidelines for auxiliary functions. If you
+like the game, add other features: show a running text; equip the UFO with
+charges that can eliminate the tank; create an entire fleet of attacking UFOs;
+and above all, use your imagination.
+|#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
