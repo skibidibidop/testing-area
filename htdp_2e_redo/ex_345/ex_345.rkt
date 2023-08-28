d8371a593262318eec0a6babbfd7b7a3e7c7a510
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-29 04:51:11 +0800

    Add ex 345 solution

--- htdp_2e_redo/ex_345/ex_345.rkt
+++ htdp_2e_redo/ex_345/ex_345.rkt
@@ -19,4 +19,37 @@ Interpret the following data as expressions:
 
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
-; FUNCTIONS ////////////////////////////////////////////////////////////////////
+(define-struct add [left right])
+
+(define-struct mul [left right])
+
+; A BSL-expr is one of:
+; Number
+; Add
+; Mul
+
+; Translate the following expressions into data:
+; 1. (+ 10 -10)
+(make-add 10 -10)
+
+; 2. (+ (* 20 3) 33)
+(make-add
+ (make-mul 20 3)
+ 33)
+
+; 3. (+ (* 3.14 (* 2 3)) (* 3.14 (* -1 -9)))
+(make-add
+ (make-mul 3.14
+           (make-mul 2 3))
+ (make-mul 3.14
+           (make-mul -1 -9)))
+
+; Interpret the following data as expressions:
+; 1. (make-add -1 2)
+(+ -1 2)
+
+; 2. (make-add (make-mul -2 -3) 33)
+(+ (* -2 -3) 33)
+
+; 3. (make-mul (make-add 1 (make-mul 2 3)) 3.14)
+(* (+ 1 (* 2 3)) 3.14)
