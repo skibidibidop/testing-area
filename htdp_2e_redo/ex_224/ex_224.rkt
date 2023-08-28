ddd8a831550885e8205569bd7a400243c6033e21
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-29 07:43:42 +0800

    Update description

--- htdp_2e_redo/ex_224/ex_224.rkt
+++ htdp_2e_redo/ex_224/ex_224.rkt
@@ -2,6 +2,12 @@
 Author: Mark Beltran
 Date: August 29, 2023
 
+Full Space War: As always, a UFO is trying to land on Earth. The player’s task
+is to prevent the UFO from landing. To this end, the game comes with a tank
+that may fire an arbitrary number of shots. When one of these shots comes
+close enough to the UFO’s center of gravity, the game is over and the player
+won. If the UFO comes close enough to the ground, the player lost.
+
 Exercise 224. Use the lessons learned from the preceding two sections and design
 the game extension slowly, adding one feature of the game after another. Always
 use the design recipe and rely on the guidelines for auxiliary functions. If you
