0c0abfa005795cf92a13d316df894d5aa0b92392
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-30 08:10:05 +0800

    Add ex 225 description

---
+++ htdp_2e_redo/ex_225/ex_225.rkt
@@ -0,0 +1,26 @@
+#|
+Author: Mark Beltran
+Date: August 31, 2023
+
+Exercise 225. Design a fire-fighting game.
+
+The game is set in the western states where fires rage through vast forests. It
+simulates an airborne fire-fighting effort. Specifically, the player acts as the
+pilot of an airplane that drops loads of water on fires on the ground. The
+player controls the plane’s horizontal movements and the release of water loads.
+
+Your game software starts fires at random places on the ground. You
+may wish to limit the number of fires, making them a function of how many
+fires are currently burning or other factors. The purpose of the game is to
+extinguish all fires in a limited amount of time.
+
+To do:
+---
+|#
+
+; DATA DEFINITION //////////////////////////////////////////////////////////////
+
+; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; MAIN /////////////////////////////////////////////////////////////////////////
+
