9d520143f1d9fc2efc6a9ebbae429933a6da35ef
Mark Beltran <118040870+skibidibidop@users.noreply.github.com>
2023-08-30 10:42:23 +0800

    Add (render)

--- htdp_2e_redo/ex_225/ex_225.rkt
+++ htdp_2e_redo/ex_225/ex_225.rkt
@@ -23,12 +23,12 @@ To do:
 
 (define SCALER 3)
 (define SCN_WIDTH   (* SCALER 100))
-(define SCN_HEIGHT  (* SCALER 250))
+(define SCN_HEIGHT  (* SCALER 200))
 (define SCN_XCENTER (/ SCN_WIDTH 2))
 (define SCN_YCENTER (/ SCN_HEIGHT 2))
 
 (define FOREST_WIDTH  (* SCALER 95))
-(define FOREST_HEIGHT (* SCALER 245))
+(define FOREST_HEIGHT (* SCALER 195))
 
 (define FOREST
   (place-image
@@ -36,29 +36,123 @@ To do:
    SCN_XCENTER SCN_YCENTER
    (empty-scene SCN_WIDTH SCN_HEIGHT)))
 
+(define BORDER_XGAP
+  (- SCN_WIDTH FOREST_WIDTH))
+(define BORDER_YGAP
+  (- SCN_HEIGHT FOREST_HEIGHT))
+
 (define PLANE
-  (triangle (* SCALER 5) "solid" "Light Steel Blue"))
+  (triangle (* SCALER 8) "solid" "Light Steel Blue"))
 (define PLANE_HALF
   (/ (image-height PLANE) 2))
+(define PLANE_YPOS
+  (- SCN_HEIGHT
+     (+ PLANE_HALF BORDER_YGAP)))
 
 (define FIRE
   (square (* SCALER 7) "solid" "Tomato"))
 
 (define WATER
-  (square (* SCALER 4) "solid" "Deep Sky Blue"))
+  (square (* SCALER 7) "solid" "Deep Sky Blue"))
 (define WATER_HALF
   (/ (image-height WATER) 2))
 
-(define GAP (* SCALER 2))
+(define SHOT_GAP (* SCALER 2))
 
 (define MAX_WATER_DIST
-  (+ WATER_HALF GAP PLANE_HALF))
+  (+ WATER_HALF SHOT_GAP PLANE_HALF))
 
 ; DATA DEFINITION //////////////////////////////////////////////////////////////
 
+(define-struct ws [plane water lof])
+; (make-ws Plane Water [List-of Fire])
+; Interp.: represents the current state
 
+; A Plane is a Number
+; Interp.: represents the plane's x coordinate
+
+; A Water is one of:
+; #false
+; Posn
+; Interp.: represents the water shot's x and y coordinates
+
+; A Fire is a Posn
+; Interp.: represents the x and y coordinates of a fire in the forest
+
+(define START_STATE
+  (make-ws
+   SCN_XCENTER
+   #false
+   '()))
+(define FIREY_STATE
+  (make-ws
+   SCN_XCENTER
+   #false
+   (list (make-posn 40 50)
+         (make-posn 60 80))))
+(define SHOOTING_STATE
+  (make-ws
+   SCN_XCENTER
+   (make-posn SCN_XCENTER
+              (- PLANE_YPOS
+                 (+ PLANE_HALF WATER_HALF)))
+   (list (make-posn 40 50)
+         (make-posn 60 80))))
 
 ; FUNCTIONS ////////////////////////////////////////////////////////////////////
+
+; Ws -> Image
+; Renders an image based on the current state ws
+(check-expect (render START_STATE)
+              (place-image
+               PLANE SCN_XCENTER PLANE_YPOS FOREST))
+(check-expect (render FIREY_STATE)
+              (place-image
+               PLANE SCN_XCENTER PLANE_YPOS
+               (place-image
+                FIRE 40 50
+                (place-image
+                 FIRE 60 80 FOREST))))
+(check-expect (render SHOOTING_STATE)
+              (place-image
+               PLANE SCN_XCENTER PLANE_YPOS
+               (place-image
+                WATER
+                SCN_XCENTER
+                (- PLANE_YPOS
+                   (+ PLANE_HALF WATER_HALF))
+                (place-image
+                 FIRE 40 50
+                 (place-image
+                  FIRE 60 80 FOREST)))))
+
+(define (render ws)
+  (local
+    [; [List-of Fire] -> Image
+     (define (gen_fires lof)
+       (cond
+         [(empty? lof) FOREST]
+         [else
+          (place-image
+           FIRE (posn-x (first lof)) (posn-y (first lof))
+           (gen_fires (rest lof)))]))
+     
+     ; Water Image -> Image
+     (define (gen_water w bg)
+       (cond
+         [(boolean? w) bg]
+         [else
+          (place-image
+           WATER
+           (posn-x (ws-water ws))
+           (posn-y (ws-water ws))
+           bg)]))]
+    
+    (place-image
+     PLANE (ws-plane ws) PLANE_YPOS
+     (gen_water
+      (ws-water ws)
+      (gen_fires (ws-lof ws))))))
 
 ; MAIN /////////////////////////////////////////////////////////////////////////
 #|
